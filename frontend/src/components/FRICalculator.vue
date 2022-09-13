a8c54ba8104fd1bd31fcc3e419d8c5e4a0b58452
Johnathan Storey <61481459+jgstorey@users.noreply.github.com>
2022-09-12 21:53:04 -0700

    Updated FRICalculator. 1st draft towards getting UI inline with wireframe.

--- frontend/src/components/FRICalculator.vue
+++ frontend/src/components/FRICalculator.vue
@@ -1,131 +1,298 @@
+<!-- TODO by priority for phase 2/Sept 23rd:
+       1) Get sytles from Jennifer's wireframes and appy. Will consolidate styles into css later (step 7).
+       2) Update 'Optional Facility Search' card-title to one field as recently decided. The field will 'type-a-head' lookup
+          and open a staged pop-up search results (as updated wireframe illustrates).
+       3) Populate drop list fields. Hard code for now.
+       4) Stage UI to show iterative child cards interactlity based on the number provided for 'Total Number of Children'.
+       5) Ensure 'Download the Results' button invokes basic field validations.
+       6) Add the content under 'Results' 
+       7) Consolidate manual styles into resuable ccs
+-->
 <template>
   <v-container>
     <v-row>
-      <v-col cols="6" md="4">
-        <v-card flat="false">
-          <v-card-text class="col-sm">
-            <p class="text-h4 text--secondary">CCFRI Estimator</p>
-            <div class="text--secondary">
-              Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod
-              tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim
-              veniam, quis nostrum exercitationem ullam corporis suscipit
-              laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute
-              iure reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint obcaecat cupiditat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </div>
-          </v-card-text>
+      <v-col cols="10">
+        <v-card elevation="0">
+          <template>
+            <v-alert type="info">
+              The estimation provided in this service is not guarantee of payments. The estimation does not take into account all of your circumstances and should be used as a guide only.
+            </v-alert>
+          </template>
         </v-card>
       </v-col>
-      <v-col cols="12" sm="6" md="8">
-        <v-container>
-          <v-card outlined class="pa-4">
-          <v-row>
-            <v-col cols="12" md="7">
-              <v-text-field
-                v-model="form.firstName"
-                :rules="nameRules"
-                :counter="10"
-                label="Search Facility"
-                hint="Name, Address, Phone number, DBA, License #"
-                required
-              ></v-text-field>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col cols="12" sm="7">
-              <v-combobox
-                v-model="select"
-                :items="typeOfCareList"
-                label="Type of Care"
-              ></v-combobox>
-            </v-col>
-            <v-col cols="12" sm="5">
-      <v-menu
-        v-model="menu2"
-        :close-on-content-click="false"
-        :nudge-right="40"
-        transition="scale-transition"
-        offset-y
-        min-width="auto"
-      >
-        <template v-slot:activator="{ on, attrs }">
-          <v-text-field
-            v-model="date"
-            label="Month"
-            prepend-icon="mdi-calendar"
-            readonly
-            v-bind="attrs"
-            v-on="on"
-          ></v-text-field>
-        </template>
-        <v-date-picker
-          v-model="date"
-          @input="menu2 = false"
-        ></v-date-picker>
-      </v-menu>
-        </v-col>
-          </v-row>
-          <v-row>
-            <v-col cols="12" sm="7">
-              <v-combobox
-                v-model="select"
-                :items="typeOfCareList"
-                label="Age category"
-              ></v-combobox>
-            </v-col>
-            <v-col cols="12" sm="5">
-              <v-text-field
-                v-model="form.lastName"
-                :rules="nameRules"
-                :counter="10"
-                label="Parent fee"
-                required
-              ></v-text-field>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col cols="12" sm="7">
-              <v-text-field
-                v-model="form.lastName"
-                :rules="nameRules"
-                :counter="10"
-                label="Fee frequency"
-                required
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12" sm="5">
-              <v-text-field
-                v-model="form.lastName"
-                :rules="nameRules"
-                :counter="10"
-                label="Enrollment numbers"
-                required
-              ></v-text-field>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col cols="12" sm="7">
-              <v-combobox
-                v-model="select"
-                :items="typeOfCareList"
-                label="Number of children"
-                outlined
-              ></v-combobox>
-            </v-col>
-            <v-col cols="12" sm="5">
-              <v-text-field
-                v-model="form.lastName"
-                :rules="nameRules"
-                :counter="10"
-                label="Last name"
-                required
-              ></v-text-field>
-            </v-col>
-          </v-row>
+    </v-row>
 
-          </v-card>
-        </v-container>
+    <v-row>
+      <v-col cols="10">
+        <v-card elevation="4" class="pa-0" color="#D4EAFF" style="">
+          <v-row>
+            <v-col style="padding-top:0%;padding-bottom:0px;">
+              <v-card-title style="color:darkblue">Optional Facility Search:</v-card-title>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="2" style="padding-bottom:0px;">
+              <v-text-label style="padding-left:24px;color:darkblue">
+                Name
+              </v-text-label>
+            </v-col>
+            <v-col cols="8" style="padding-bottom:0px;">
+              <v-text-field
+                background-color="white"
+                dense
+                v-model="number"
+                v-on="on"
+                outlined
+                required>
+              </v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="2">
+              <v-text-label style="padding-left:24px;color:darkblue">
+                City
+              </v-text-label>
+            </v-col>
+            <v-col cols="8">
+              <v-text-field
+                background-color="white"
+                dense
+                v-model="number"
+                v-on="on"
+                outlined
+                required>
+              </v-text-field>
+            </v-col>
+            <v-col cols="2">
+              <v-btn
+                depressed
+                color="primary">Search</v-btn>
+            </v-col>
+        </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col></v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="10">
+      <v-card elevation="4" class="">
+        <v-row>
+          <v-col style="padding-top:0px;padding-bottom:0px;">
+            <v-card-title class="grey lighten-3" style="color:darkblue;">Facility Details</v-card-title>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="6" style="padding-bottom:0px;">
+            <v-text-label style="padding-left:24px;">
+              <template><span class="red--text"><strong>&nbsp;*</strong></span></template>
+              Type of Care
+            </v-text-label>
+          </v-col>
+          <v-col cols="4" style="padding-bottom:0px;">
+            <v-combobox
+                v-model="select"
+                :items="typeOfCareList"
+                outlined
+                required
+                dense>
+            </v-combobox>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col style="padding-top:0px;padding-bottom:0px;">
+            <v-divider></v-divider>
+          </v-col>
+        </v-row>
+        
+        <v-row>
+          <v-col cols="6" style="padding-bottom:0px;">
+            <v-text-label style="padding-left:24px">
+              <template><span class="red--text"><strong> *</strong></span></template>
+              Month
+            </v-text-label>
+          </v-col>
+          <v-col cols="4" style="padding-bottom:0px;">
+            <v-menu v-model="menu2"
+                    :close-on-content-click="false"
+                    :nudge-right="40"
+                    transition="scale-transition"
+                    offset-y
+                    min-width="auto">
+              <template v-slot:activator="{ on, attrs }">
+                <v-text-field
+                  v-model="date"
+                  readonly
+                  v-bind="attrs"
+                  v-on="on"
+                  outlined
+                  required
+                  dense>
+                </v-text-field>
+              </template>
+              <v-date-picker
+                v-model="date"
+                @input="menu2 = false">
+              </v-date-picker>
+            </v-menu>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col style="padding-top:0px;padding-bottom:0px;">
+            <v-divider></v-divider>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="6" style="padding-bottom:0px;">
+            <v-text-label style="padding-left:24px;">
+              <template><span class="red--text"><strong> *</strong></span></template>
+              Total Number of Children
+            </v-text-label>
+          </v-col>
+          <v-col cols="4" style="padding-bottom:0px;">
+            <v-text-field
+              v-model="number"
+              v-on="on"
+              outlined
+              required
+              dense>
+            </v-text-field>
+          </v-col>
+        </v-row>
+
+        <v-card-title class="grey lighten-3" style="color:darkblue;">Child 1</v-card-title>
+        <v-row>
+          <v-col cols="6">
+            <v-text-label style="padding-left:24px;">
+              <template><span class="red--text"><strong> *</strong></span></template>
+              Child's age category
+            </v-text-label>
+          </v-col>
+          <v-col cols="4">
+            <v-combobox
+                v-model="select"
+                :items="typeOfCareList"
+                outlined
+                dense>
+            </v-combobox>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col style="padding-top:0px;padding-bottom:0px;">
+            <v-divider></v-divider>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="5">
+            <v-text-label style="padding-left:24px;">
+              <template><span class="red--text"><strong> *</strong></span></template>
+              Parent Fee
+            </v-text-label>
+          </v-col>
+          <v-col cols="1">
+            <div style="align-content: center;">
+            <v-card class="blue darken-3" style="border-radius: 50%; height: 30px; width: 30px;text-align: center;">
+              <v-icon color="white">mdi-help</v-icon>
+            </v-card>
+            </div>
+          </v-col>
+          <v-col cols="4">
+            <v-text-field
+                v-model="form.lastName"
+                :rules="nameRules"
+                outlined
+                required
+                dense>
+            </v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col style="padding-top:0px;padding-bottom:0px;">
+            <v-divider></v-divider>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="5">
+            <v-text-label style="padding-left:24px;">
+              <template><span class="red--text"><strong> *</strong></span></template>
+              Parent Fee Frequency
+            </v-text-label>
+          </v-col>
+          <v-col cols="1">
+            <div style="align-content: center;">
+            <v-card class="blue darken-3" style="border-radius: 50%; height: 30px; width: 30px;text-align: center;">
+              <v-icon color="white">mdi-help</v-icon>
+            </v-card>
+            </div>
+          </v-col>
+          <v-col cols="4">
+            <v-text-field
+                v-model="form.lastName"
+                :rules="nameRules"
+                outlined
+                required
+                dense></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col style="padding-top:0px;padding-bottom:0px;">
+            <v-divider></v-divider>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="4">
+            <v-text-label style="padding-left:24px;">
+              <template><span class="red--text"><strong> *</strong></span></template>
+              Total number of days
+            </v-text-label>
+          </v-col>
+
+          <v-col cols="2" style="text-align:right;">
+            <v-text-label>
+              4 hours or less
+            </v-text-label>
+          </v-col>
+          <v-col cols="1" sm="">
+            <v-text-field
+                v-model="form.lastName"
+                :rules="nameRules"
+                outlined
+                required
+                dense></v-text-field>
+          </v-col>
+
+          <v-col cols="2" style="padding-left:0px;text-align:right;">
+            <v-text-label>
+              Over 4 hours
+            </v-text-label>
+          </v-col>
+          <v-col cols="2" style="padding-left:0px;">
+            <v-text-field style="width:124px"
+                v-model="form.lastName"
+                :rules="nameRules"
+                outlined
+                required
+                dense></v-text-field>
+          </v-col>
+        </v-row>
+
+        <v-card-title class="grey lighten-3" style="color:darkblue;">Results</v-card-title>
+        <v-row>
+          <v-col cols="12" class="text-center">
+            <v-btn
+              class="ma-2"
+              outlined
+              color="indigo">
+              Estimate the Benefit
+            </v-btn>
+          </v-col>
+        </v-row>
+
+
+      </v-card>
       </v-col>
     </v-row>
   </v-container>
@@ -368,4 +535,6 @@ export default {
     transform: scale(1);
   }
 }
+
+
 </style>
