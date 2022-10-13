06ee9ad42346f95da52f0bd8b4b11fb9155f0039
Malikov <alexey.malikov@cgi.com>
2022-10-13 12:45:09 -0600

    Update FacilityInformation.vue

--- frontend/src/components/groupProvider/FacilityInformation.vue
+++ frontend/src/components/groupProvider/FacilityInformation.vue
@@ -6,19 +6,16 @@
           <v-container>
             <v-row>
               <v-col>
-                <v-text-field outlined required v-model="facilityNameCommunityCare" :rules="rules.required"
-                  label="Facility Name (as it appears on the Community Care Assisted Living Act licence)" />
+                <v-text-field outlined required v-model="facilityNameCommunityCare" :rules="rules.required" label="Facility Name (as it appears on the Community Care Assisted Living Act licence)" />
               </v-col>
             </v-row>
 
             <v-row>
               <v-col cols="12" md="6">
-                <v-text-field outlined required v-model="yearBeginOperation" type="number" :rules="rules.required"
-                  label="Year Facility Began operation (YYYY)" />
+                <v-text-field outlined required v-model="yearBeginOperation" type="number" :rules="rules.required" label="Year Facility Began operation (YYYY)" />
               </v-col>
               <v-col cols="12" md="6">
-                <v-text-field outlined required v-model="facilityAddress" :rules="rules.required"
-                  label="Facility Street Address" />
+                <v-text-field outlined required v-model="facilityAddress" :rules="rules.required" label="Facility Street Address" />
               </v-col>
             </v-row>
 
@@ -27,15 +24,13 @@
                 <v-text-field outlined required v-model="city" :rules="rules.required" label="City/Town" />
               </v-col>
               <v-col cols="12" md="6">
-                <v-text-field outlined required v-model="postalCode" :rules="[...rules.required, ...rules.postalCode]"
-                  label="Postal Code" />
+                <v-text-field outlined required v-model="postalCode" :rules="[...rules.required, ...rules.postalCode]" label="Postal Code" />
               </v-col>
             </v-row>
 
             <v-row>
               <v-col cols="12" md="6">
-                <v-text-field outlined required v-model="contactName" :rules="rules.required"
-                  label="Facility Contact Name" />
+                <v-text-field outlined required v-model="contactName" :rules="rules.required" label="Facility Contact Name" />
               </v-col>
               <v-col cols="12" md="6">
                 <v-text-field outlined required v-model="position" :rules="rules.required" label="Position" />
@@ -47,22 +42,18 @@
                 <v-text-field outlined required v-model="phone" :rules="rules.required" label="Business Phone" />
               </v-col>
               <v-col cols="12" md="6">
-                <v-text-field outlined required v-model="email" :rules="[...rules.required, ...rules.email]"
-                  label="Organization Facility email" />
+                <v-text-field outlined required v-model="email" :rules="[...rules.required, ...rules.email]" label="Organization Facility email" />
               </v-col>
             </v-row>
 
             <v-row>
               <v-col cols="12" md="6">
-                <v-text-field outlined required type="number" v-model="licenseNumber" :rules="rules.required"
-                  label="Facility Licence Number" />
+                <v-text-field outlined required type="number" v-model="licenseNumber" :rules="rules.required" label="Facility Licence Number" />
               </v-col>
               <v-col cols="12" md="6">
-                <v-menu v-model="calendarMenu" :close-on-content-click="false" :nudge-right="40"
-                  transition="scale-transition" offset-y min-width="auto">
+                <v-menu v-model="calendarMenu" :close-on-content-click="false" :nudge-right="40" transition="scale-transition" offset-y min-width="auto">
                   <template v-slot:activator="{ on, attrs }">
-                    <v-text-field outlined required v-model="licenseEffectiveDate"
-                      label="Effective Date of Current Licence (YYYY-MM-DD)" readonly v-bind="attrs" v-on="on">
+                    <v-text-field outlined required v-model="licenseEffectiveDate" label="Effective Date of Current Licence (YYYY-MM-DD)" readonly v-bind="attrs" v-on="on">
                     </v-text-field>
                   </template>
                   <v-date-picker v-model="licenseEffectiveDate" @input="calendarMenu = false">
@@ -85,8 +76,7 @@
 
             <v-row v-show="hasReceivedFunding === 'yesFacility'">
               <v-col>
-                <v-text-field outlined required v-model="facilityName"
-                  :rules="hasReceivedFunding === 'yesFacility' ? rules.required : []" label="Facility Name" />
+                <v-text-field outlined required v-model="facilityName" :rules="hasReceivedFunding === 'yesFacility' ? rules.required : []" label="Facility Name" />
               </v-col>
             </v-row>
 
