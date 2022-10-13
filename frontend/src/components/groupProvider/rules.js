82cfc15dd97d0ff38311e34e477a37fe823534b1
Malikov <alexey.malikov@cgi.com>
2022-10-13 12:44:32 -0600

    fix indent and line wrap

--- frontend/src/components/groupProvider/rules.js
+++ frontend/src/components/groupProvider/rules.js
@@ -1,15 +1,15 @@
 const rules = {
-    email: [v => /.+@.+/.test(v) || 'A valid email is required'],
-    required: [function (v) {
-        if (v === 0) {
-            return true;
-        } else if (!v) {
-            return 'This field is requiered';
-        }
+  email: [v => /.+@.+/.test(v) || 'A valid email is required'],
+  required: [function (v) {
+    if (v === 0) {
+      return true;
+    } else if (!v) {
+      return 'This field is requiered';
+    }
 
-        return true;
-    }],
-    postalCode: [v => /^[ABCEGHJ-NPRSTVXY]\d[ABCEGHJ-NPRSTV-Z][ -]?\d[ABCEGHJ-NPRSTV-Z]\d$/i.test(v) || 'A valid postal code is required']
+    return true;
+  }],
+  postalCode: [v => /^[ABCEGHJ-NPRSTVXY]\d[ABCEGHJ-NPRSTV-Z][ -]?\d[ABCEGHJ-NPRSTV-Z]\d$/i.test(v) || 'A valid postal code is required']
 };
 
 export default rules;
