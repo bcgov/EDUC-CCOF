e8e47480557ba10a1a800e4b713e91d103036a27
Malikov <alexey.malikov@cgi.com>
2022-11-01 11:07:56 -0600

    refactoring

--- frontend/src/store/modules/organization.js
+++ frontend/src/store/modules/organization.js
@@ -57,65 +57,52 @@ export default {
   },
   actions: {
     async saveOrganization({ state, commit }) {
-      return new Promise((resolve, reject) => {
-        if (!localStorage.getItem('jwtToken')) { // DONT Call api if there is no token.
-          console.log('unable to save because you are not logged in');
-          reject('unable to save because you are not logged in');
-        }
-        const payload = {
-          legalName: state.legalName,
-          address1: state.address1,
-          city1: state.city1,
-          postalCode1: state.postalCode1,
-          address2: state.address2,
-          city2: state.city2,
-          postalCode2: state.postalCode2,
-          contactName: state.contactName,
-          position: state.position,
-          phone: state.phone,
-          businessId: state.businessId,
-          email: state.email,
-          incNumber: '' + state.incNumber, //Make sure it's a string
-          organizationType: state.organizationType,
-        };
-        if (state.organizationId) { // has an orgaization ID, so update the data
-          ApiService.apiAxios.put(ApiRoutes.ORGANIZATION + '/' + state.organizationId, payload)
-            .then((response) => {
-              commit('setOrganizationId', response.data?.organizationId);
-              commit('setLegalName', response.data?.legalName);
-              commit('setAddress1', response.data?.address1);
-              commit('setCity1', response.data?.city1);
-              commit('setPostalCode1', response.data?.postalCode1);
-              commit('setAddress2', response.data?.address2);
-              commit('setCity2', response.data?.city2);
-              commit('setPostalCode2', response.data?.postalCode2);
-              commit('setContactName', response.data?.contactName);
-              commit('setPosition', response.data?.position);
-              commit('setPhone', response.data?.phone);
-              // don't update business Id just yet
-              // commit('setBusinessId', response.data?.businessId); 
-              commit('setEmail', response.data?.email);
-              commit('setIncNumber', response.data?.incNumber);
-              commit('setOrganizationType', response.data?.organizationType);
-              resolve(response);
-            })
-            .catch((e) => {
-              console.log(`Failed to update existing Organization - ${e}`);
-              reject(e);
-            });
 
-        } else {
-          ApiService.apiAxios.post(ApiRoutes.ORGANIZATION, payload)
-            .then((response) => {
-              commit('setOrganizationId', response.data?.organizationId);
-              resolve(response);
-            })
-            .catch((e) => {
-              console.log(`Failed to save new Organization - ${e}`);
-              reject(e);
-            });
+      if (!localStorage.getItem('jwtToken')) { // DONT Call api if there is no token.
+        console.log('unable to save because you are not logged in');
+        throw 'unable to save because you are not logged in';
+      }
+
+      let payload = JSON.parse(JSON.stringify(state));
+      console.log('payload', payload);
+
+      if (state.organizationId) {
+        // has an orgaization ID, so update the data
+        try {
+          let response = await ApiService.apiAxios.put(ApiRoutes.ORGANIZATION + '/' + state.organizationId, payload);
+
+          commit('setOrganizationId', response.data?.organizationId);
+          commit('setLegalName', response.data?.legalName);
+          commit('setAddress1', response.data?.address1);
+          commit('setCity1', response.data?.city1);
+          commit('setPostalCode1', response.data?.postalCode1);
+          commit('setAddress2', response.data?.address2);
+          commit('setCity2', response.data?.city2);
+          commit('setPostalCode2', response.data?.postalCode2);
+          commit('setContactName', response.data?.contactName);
+          commit('setPosition', response.data?.position);
+          commit('setPhone', response.data?.phone);
+          // don't update business Id just yet
+          // commit('setBusinessId', response.data?.businessId); 
+          commit('setEmail', response.data?.email);
+          commit('setIncNumber', response.data?.incNumber);
+          commit('setOrganizationType', response.data?.organizationType);
+
+          return response;
+        } catch (error) {
+          console.log(`Failed to update existing Organization - ${error}`);
+          throw error;
         }
-      });
+      } else {
+        try {
+          let response = await ApiService.apiAxios.post(ApiRoutes.ORGANIZATION, payload);
+          commit('setOrganizationId', response.data?.organizationId);
+          return response;
+        } catch (error) {
+          console.log(`Failed to save new Organization - ${error}`);
+          throw error;
+        }
+      }
     }
   },
 };
