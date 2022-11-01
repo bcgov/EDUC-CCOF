a3e5fde310ee5dc23d62643f7164b60c3e754ad9
Johnathan Storey <61481459+jgstorey@users.noreply.github.com>
2022-11-01 11:32:41 -0700

    FacilitySearch component integrated with dynamics endpoints and the estimator screen.

--- backend/src/components/public.js
+++ backend/src/components/public.js
@@ -3,7 +3,7 @@
 const axios = require('axios');
 const log = require('./logger');
 const config = require('../config/index');
-const { errorResponse, minify, HttpStatus, getHttpHeader} = require('./utils');
+const { errorResponse, minify, HttpStatus, getHttpHeader, getOperation } = require('./utils');
 const {ApiError} = require('./error');
 const { CHILD_AGE_CATEGORY_TYPES } = require('../util/constants');
 
@@ -14,9 +14,8 @@ async function getFacilities(req, res) {
     let payLoad = await searchFacility(req.query.criteria);
     // Iterate through the payload to colect what we need, lighten, and return a condensed payload...
     for (let x in payLoad) {
-      log.info(x + ' : ' + payLoad[x]);
       results.push({facilityId: payLoad[x]['@search.objectid'],
-        accountNumber: payLoad[x].accountNumber,
+        accountNumber: payLoad[x].accountnumber,
         facilityName: payLoad[x].name,
         city: payLoad[x].address1_city});
     }
@@ -32,51 +31,36 @@ async function getFacility(req, res) {
   try {
     let results = {};
     let approvedFeesByChildAgeCategory = [];
-
-    // TODO: temp hardcoded payload. Remove once endpoint is complete.
-    let payLoad = simulatedPayloadFacility;
-
-    /* TODO: acutal endpoint call. Uncomment and test once endpoint is implemented.
-    let payLoad = (await axios.get('/api/operations?statement=accounts('+req.query.facilityId+')?'+
-                                   '$select=accountid,address1_city,accountnumber,'+
-                                   'name&$expand=ccof_account_ccof_parent_fees_Facility($select=ccof_parent_feesid,ccof_apr,ccof_aug,_ccof_childcarecategory_value,ccof_dec,_ccof_facility_value,ccof_feb,ccof_jan,ccof_jul,ccof_jun,ccof_mar,ccof_may,ccof_nov,ccof_oct,_ccof_programyear_value,ccof_sep),'+
-                                   'ccof_facility_licenses_Facility_account($select=ccof_facility_licensesid,_ccof_facility_value,_ccof_licensecategory_value')).data;
-    */
+    
+    let operation = 'accounts('+req.params.facilityId+')?$select=accountid,address1_city,accountnumber,name&$expand=ccof_account_ccof_parent_fees_Facility($select=ccof_parent_feesid,ccof_apr,ccof_aug,_ccof_childcarecategory_value,ccof_dec,_ccof_facility_value,ccof_feb,ccof_jan,ccof_jul,ccof_jun,ccof_mar,ccof_may,ccof_nov,ccof_oct,_ccof_programyear_value,ccof_sep),ccof_facility_licenses_Facility_account($select=ccof_facility_licensesid,_ccof_facility_value,_ccof_licensecategory_value)';
+    let payLoad = await getOperation(operation);
+  
+    results.facilityId = payLoad.accountnumber;
+    results.name = payLoad.name;
+    results.city = payLoad.address1_composite;
+    results.approvedFeesByChildAgeCategory = [];
+    approvedFeesByChildAgeCategory = payLoad.ccof_account_ccof_parent_fees_Facility;
 
     // Iterate through the payload to colect what we need, lighten, and return a condensed payload...
-    for (let x in payLoad) {
-      // TODO: if statement blow is only required for hardcoded payload. Remove when no longer needed remove from the for loop it surrounds.
-      if (payLoad[x].accountid == req.params.facilityId) {
-        // Get the header record info from the payload. NOTE: we are dong this because we are transforming the payload to into something lighter
-        // to return than original payload...
-        results.facilityId = payLoad[x].accountid;
-        results.name = payLoad[x].name;
-        results.city = payLoad[x].address1_composite;
-        results.approvedFeesByChildAgeCategory = [];
-        approvedFeesByChildAgeCategory = payLoad[x].ccof_account_ccof_parent_fees_Facility;
- 
-        // For each approvedFeesByChildAgeCategory payload rec, transform the payload data into less verbose json...
-        let rec;
-        for (let y in approvedFeesByChildAgeCategory) {
-          rec = {
-            childCareCategory: CHILD_AGE_CATEGORY_TYPES.get(approvedFeesByChildAgeCategory[y]['_ccof_childcarecategory_value@OData.Community.Display.V1.FormattedValue']),
-            programYear: approvedFeesByChildAgeCategory[y]['_ccof_programyear_value@OData.Community.Display.V1.FormattedValue'],
-            approvedFeeApr: approvedFeesByChildAgeCategory[y].ccof_apr,
-            approvedFeeAug: approvedFeesByChildAgeCategory[y].ccof_aug,
-            approvedFeeDec: approvedFeesByChildAgeCategory[y].ccof_dec,
-            approvedFeeFeb: approvedFeesByChildAgeCategory[y].ccof_feb,
-            approvedFeeJan: approvedFeesByChildAgeCategory[y].ccof_jan,
-            approvedFeeJul: approvedFeesByChildAgeCategory[y].ccof_jul,
-            approvedFeeJun: approvedFeesByChildAgeCategory[y].ccof_jun,
-            approvedFeeMar: approvedFeesByChildAgeCategory[y].ccof_mar,
-            approvedFeeMay: approvedFeesByChildAgeCategory[y].ccof_may,
-            approvedFeeNov: approvedFeesByChildAgeCategory[y].ccof_nov,
-            approvedFeeOct: approvedFeesByChildAgeCategory[y].ccof_oct,
-            approvedFeeSep: approvedFeesByChildAgeCategory[y].ccof_sep
-          };
-          results.approvedFeesByChildAgeCategory.push(rec);
-        }
-      }
+    let rec;
+    for (let y in approvedFeesByChildAgeCategory) {
+      rec = {
+        childCareCategory: CHILD_AGE_CATEGORY_TYPES.get(approvedFeesByChildAgeCategory[y]['_ccof_childcarecategory_value@OData.Community.Display.V1.FormattedValue']),
+        programYear: approvedFeesByChildAgeCategory[y]['_ccof_programyear_value@OData.Community.Display.V1.FormattedValue'],
+        approvedFeeApr: approvedFeesByChildAgeCategory[y].ccof_apr,
+        approvedFeeAug: approvedFeesByChildAgeCategory[y].ccof_aug,
+        approvedFeeDec: approvedFeesByChildAgeCategory[y].ccof_dec,
+        approvedFeeFeb: approvedFeesByChildAgeCategory[y].ccof_feb,
+        approvedFeeJan: approvedFeesByChildAgeCategory[y].ccof_jan,
+        approvedFeeJul: approvedFeesByChildAgeCategory[y].ccof_jul,
+        approvedFeeJun: approvedFeesByChildAgeCategory[y].ccof_jun,
+        approvedFeeMar: approvedFeesByChildAgeCategory[y].ccof_mar,
+        approvedFeeMay: approvedFeesByChildAgeCategory[y].ccof_may,
+        approvedFeeNov: approvedFeesByChildAgeCategory[y].ccof_nov,
+        approvedFeeOct: approvedFeesByChildAgeCategory[y].ccof_oct,
+        approvedFeeSep: approvedFeesByChildAgeCategory[y].ccof_sep
+      };
+      results.approvedFeesByChildAgeCategory.push(rec);
     }
     return res.status(200).json(results);
   } catch (e) {
