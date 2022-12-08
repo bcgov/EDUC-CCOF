71428cfb74956efa69808feb3994daef40678bde
Rob Lo <rob.lo@cgi.com>
2022-12-08 15:06:20 -0800

    fixed a bug where we can't create a user when the user has no last name in SSO

--- backend/src/components/user.js
+++ backend/src/components/user.js
@@ -185,17 +185,22 @@ async function getDynamicsUserByEmail(req) {
 
 async function creatUser(req) {
   log.info('No user found, creating BCeID User: ', getUserName(req));
-  let firstname = req.session.passport.user._json.given_name;
-  let lastname = req.session.passport.user._json.family_name;
+  let given_name = req.session.passport.user._json.given_name; 
+  let family_name = req.session.passport.user._json.family_name;
+  let firstname = undefined;
+  let lastname = undefined;
   try {
-    if (!lastname && firstname && firstname.split(' ').length > 1) {
+    if (!family_name && given_name && given_name.split(' ').length > 1) {
       //If for some reason we don't have a last name from SSO, see if firstname has 2 words
-      firstname = firstname.split(' ').slice(0, -1).join(' ');
-      lastname = firstname.split(' ').slice(-1).join(' ');
-    } else if (!firstname && lastname && lastname.split(' ').length > 1) {
+      firstname = given_name.split(' ').slice(0, -1).join(' ');
+      lastname = given_name.split(' ').slice(-1).join(' ');
+    } else if (!given_name && family_name && family_name.split(' ').length > 1) {
       //If for some reason we don't have a firstname name from SSO, see if lastname has 2 words
-      firstname = lastname.split(' ').slice(0, -1).join(' ');
-      lastname = lastname.split(' ').slice(-1).join(' ');
+      firstname = family_name.split(' ').slice(0, -1).join(' ');
+      lastname = family_name.split(' ').slice(-1).join(' ');
+    } else {
+      firstname = given_name;
+      lastname = family_name;
     }
 
     let payload = {
