b3003de600169911586597112e6e016b1740e534
Wang <yiyi.wang@cgi.com>
2023-01-04 12:11:49 -0800

    Update the license upload script

--- test/src/testCases/upload-license.js
+++ test/src/testCases/upload-license.js
@@ -26,13 +26,43 @@ test('Upload License', async t => {
   await t.click(upload.licenseUploadButton);
   await t.click(upload.noButton);
   await t.expect(upload.header.exists).ok({timeout: 5000});
-//   await t.click(upload.saveButton);
-  await t.click(Selector('div').withExactText('Select your file').nth(0));
-  await upload.uploadFiles(t, Selector('input').withAttribute('placeholder', 'Select your file').nth(0), 'Community Care Licence.png');
+  const facilities = Selector('input').withAttribute('placeholder', 'Select your file');
+  const length = await facilities.count;
+  let index = 0;
+  for(index; index < length; index++){
+    await t.click(Selector('div').withExactText('Select your file').nth(index));
+    await upload.uploadFiles(t, facilities.nth(index), 'Community Care Licence.png');
+  }
   await t.click(upload.saveButton);
   await t.takeScreenshot({fullPage: true});
-  await t.expect(upload.nextButton.hasAttribute('disabled')).ok(); // This will fail if the next button is disable
+  await t.expect(upload.nextButton.hasAttribute('disabled')).notOk(); // This will fail if the next button is disable
 
   await t.takeScreenshot({fullPage: true});
   await t.expect(Selector('.v-system-bar').exists).ok({ timeout: 5000 });
 });
+
+
+test('Delete License', async t => {
+    await login.bceIdLogin(t);
+    await t
+      .click(landing.continueButton)
+      .wait(2000);
+    await t.click(upload.licenseUploadButton);
+    await t.click(upload.noButton);
+    await t.expect(upload.header.exists).ok({timeout: 5000});
+    
+    const length = await Selector('.mdi-delete').count;
+    let index = 0;
+    for(index; index < length; index++){
+      await t.click(Selector('.mdi-delete').nth(0));
+      const facilities = Selector('input').withAttribute('placeholder', 'Select your file');
+      const length = await facilities.count;
+      await t.expect(length).eql(index + 1);
+    }
+    await t.click(upload.saveButton);
+    await t.takeScreenshot({fullPage: true});
+    await t.expect(upload.nextButton.hasAttribute('disabled')).ok(); // This will fail if the next button is not disable
+  
+    await t.takeScreenshot({fullPage: true});
+    await t.expect(Selector('.v-system-bar').exists).ok({ timeout: 5000 });
+  });
