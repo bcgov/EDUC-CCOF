39e17dca2f0ceeea98736c4a163293d421220742
Wang <yiyi.wang@cgi.com>
2023-01-04 11:19:19 -0800

    Refactoring the selectors and update organization

--- test/src/testCases/update-funding.js
+++ test/src/testCases/update-funding.js
@@ -17,23 +17,34 @@ const funding = new PageFunding();
 fixture `Funding Tests`
   .page(`${config.get('url')}/internal`)
   .beforeEach(async t => {
+    await t.maximizeWindow();
     await t.setTestSpeed(1);
   });
 
-test('Update Organization', async t => {
+test('Update Organization - Completed info', async t => {
   await login.idirLogin(t);
   await impersonate.loadUser(t, config.get('bceid_credentials').username);
   await t
     .click(landing.continueButton)
     .wait(2000);
-  await t.click(organization.nextButton)
-    .wait(2000)
-  
-  await t.click(organization.nextButton)
-    .wait(2000)
-  await funding.loadFieldsFromFile(t, 'test1-funding.txt');
+  await organization.updateField(t, 'Mailing Address', '111 sutter street');
+  await organization.loadFieldsFromFile(t, 'organization-completed.txt');
   await t.takeScreenshot({fullPage: true});
-  // await t.click(organization.nextButton);
+  await t.expect(organization.nextButton.hasAttribute('disabled')).notOk();
+  await t.click(organization.nextButton);
+  //Have to test something or the test will fail.
+  await t.expect(Selector('.v-system-bar').exists).ok({ timeout: 5000 });
+});
+
+test('Update Organization - Incompleted info', async t => {
+  await login.idirLogin(t);
+  await impersonate.loadUser(t, config.get('bceid_credentials').username);
+  await t
+    .click(landing.continueButton)
+    .wait(2000);
+  await organization.updateField(t, 'Mailing Address', '111 sutter street');
+  await organization.loadFieldsFromFile(t, 'organization-incompleted.txt');
+  await t.takeScreenshot({fullPage: true});
   await t.expect(organization.nextButton.hasAttribute('disabled')).ok();
   //Have to test something or the test will fail.
   await t.expect(Selector('.v-system-bar').exists).ok({ timeout: 5000 });
