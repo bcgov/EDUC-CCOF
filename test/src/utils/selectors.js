133680e6b0bc9ce3aacc3bdbd230578103db2764
Wang <yiyi.wang@cgi.com>
2023-01-12 20:57:45 -0800

    Update CCFRI Parent Fee Page

--- test/src/utils/selectors.js
+++ test/src/utils/selectors.js
@@ -24,6 +24,80 @@ function getRadioOption(labelName, selectedName) {
   return Selector('legend').withExactText(labelName).nextSibling().find('label').withExactText(selectedName);
 }
 
+
+function getRadioTextField(fieldName){
+  return Selector('div.v-input--radio-group').parent().parent().nextSibling().find('label').withExactText(fieldName).nextSibling();
+}
+
+function getErrorMessage(element, message){
+  return element.parent().parent().nextSibling().find('div').withExactText(message);
+}
+
+async function removeContent(t, element){
+  await t.typeText(element, 'a', { replace: true, speed:0.5 })
+          .pressKey('backspace');
+}
+
+function convertToMonth(date_month){
+  let month = ""
+  switch (date_month) {
+    case "01":
+      month = "Jan";
+      break;
+    case "02":
+      month = "Feb";
+      break;
+    case "03":
+      month = "Mar";
+      break;
+    case "04":
+      month = "Apr";
+      break;
+    case "05":
+      month = "May";
+      break;
+    case "06":
+      month = "Jun";
+      break;
+    case "07":
+      month = "Jul";
+      break;
+    case "08":
+      month = "Aug";
+      break;
+    case "09":
+      month = "Sep";
+      break;
+    case "10":
+      month = "Oct";
+      break;
+    case "11":
+      month = "Nov";
+      break;
+    case "12":
+      month = "Dec"
+      break;
+  }
+  return month;
+}
+
+async function selectDate(t, date_data){
+  const date_arr = date_data.trim().split('-');
+  const date_year = date_arr[0];
+  const date_month = date_arr[1];
+  const date_day = date_arr[2]; 
+
+  const year_title = Selector('div.v-date-picker-title__year').filterVisible();
+  await t.click(year_title);
+  const year_option = Selector('ul.v-date-picker-years').find('li').withText(date_year).filterVisible();
+  await t.click(year_option);
+  const month_option = Selector('div.v-date-picker-table--month').find('div').withText(convertToMonth(date_month)).filterVisible();
+  await t.click(month_option);
+  const day_option = Selector('div.v-date-picker-table--date').find('div').withText(date_day.replace(/^0+/, '')).filterVisible(); //remove the leading zero
+  await t.click(day_option);
+} 
+
+
 function getSelectOption(labelName, selectedName) {
   return Selector('label').withText(labelName).parent().parent().nextSibling().find('label').withText(selectedName).prevSibling();
 }
@@ -74,7 +148,8 @@ const selectors = {
   mapFieldsFromFile,
   getRadioOption,
   getErrorMessage,
-  removeContent
+  removeContent,
+  selectDate
 };
 
 module.exports = selectors;
