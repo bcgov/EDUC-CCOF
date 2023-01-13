6ceb2995972540b3baaeebc71ce4787f61a639d8
Wang <yiyi.wang@cgi.com>
2023-01-12 23:29:08 -0800

    ECEWE pages

--- test/src/testCases/updateECEWEEligibility.js
+++ test/src/testCases/updateECEWEEligibility.js
@@ -1,17 +1,27 @@
 import PageLogin from '../pageObjects/PageLogin';
 import PageLanding from '../pageObjects/PageLanding';
+<<<<<<< HEAD
 import PageECEWEEligibility from '../pageObjects/pageECEWEEligibility';
 import PageAlert from '../pageObjects/PageAlert';
 import PageECEWEfacility from '../pageObjects/pageECEWEfacility';
+=======
+import pageECEWEEligibility from '../pageObjects/pageECEWEEligibility';
+import PageAlert from '../pageObjects/PageAlert';
+>>>>>>> 5d1df4d (ECEWE pages)
 
 const config = require('../utils/configLoader');
 
 
 const login = new PageLogin();
 const landing = new PageLanding();
+<<<<<<< HEAD
 const eligibility = new PageECEWEEligibility();
 const alert = new PageAlert();
 const opt = new PageECEWEfacility();
+=======
+const eligibility = new pageECEWEEligibility();
+const alert = new PageAlert();
+>>>>>>> 5d1df4d (ECEWE pages)
 
 fixture `ECE-WE Eligibility Tests`
   .page(`${config.get('url')}/login`)
@@ -27,10 +37,18 @@ test('Update ECE-WE Eligibility', async t => {
     .wait(2000);
   await t.click(eligibility.ECEWEButton);
   await t.click(eligibility.eligibilityButton);
+<<<<<<< HEAD
   await eligibility.updateOptionFromFile(t, 'ee2eTest-ecewe-eligibility.txt');
   await eligibility.clickSaveAndNextButton(t);
   await opt.updateOptFromFile(t, 'e2eTest-ecewe-facility.txt');
   await opt.clickSaveAndNextButton(t);
+=======
+  await eligibility.updateOptionFromFile(t, 'ecewe-eligibility.txt');
+  await t.expect(eligibility.nextButton.hasAttribute('disabled')).notOk();
+  await t.click(eligibility.saveButton).wait(3000);
+  await t.expect(alert.success.exists).ok();
+  await t.click(eligibility.nextButton);
+>>>>>>> 5d1df4d (ECEWE pages)
 
 });
 
