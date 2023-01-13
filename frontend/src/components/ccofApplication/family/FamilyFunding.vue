13ffa31d1c5d20a3f1851561cdf8496d180b60d3
Malikov <alexey.malikov@cgi.com>
2023-01-13 00:10:40 -0700

    change read only fields into disabled

--- frontend/src/components/ccofApplication/family/FamilyFunding.vue
+++ frontend/src/components/ccofApplication/family/FamilyFunding.vue
@@ -7,7 +7,7 @@
           <v-container>
             <v-row>
               <v-col>
-                <v-radio-group :readonly="isLocked" v-model="model.familyLicenseType" label="Liscence type">
+                <v-radio-group :disabled="isLocked" v-model="model.familyLicenseType" label="Liscence type">
                   <v-radio label="Family child care" value="7" />
                   <v-radio label="In-Home Multi-Age Child Care" value="6" />
                   <v-radio label="Multi-Age Care" value="5" />
@@ -21,16 +21,16 @@
           <v-container>
             <v-row>
               <v-col cols="12" md="6">
-                <v-text-field :readonly="isLocked" type="number" outlined required :rules="rules.required" v-model.number="model.maxDaysPerWeek" label="Maximum number of days per week you provide child care" />
+                <v-text-field :disabled="isLocked" type="number" outlined required :rules="rules.required" v-model.number="model.maxDaysPerWeek" label="Maximum number of days per week you provide child care" />
               </v-col>
               <v-col cols="12" md="6">
-                <v-text-field :readonly="isLocked" type="number" outlined required :rules="rules.required" v-model.number="model.maxWeeksPerYear" label="Maximum of weeks per year you provide child care" />
+                <v-text-field :disabled="isLocked" type="number" outlined required :rules="rules.required" v-model.number="model.maxWeeksPerYear" label="Maximum of weeks per year you provide child care" />
               </v-col>
             </v-row>
 
             <v-row>
               <v-col>
-                <v-radio-group :readonly="isLocked" row v-model="model.hasClosedMonth" label="Are there months when ALL of the programs at this facility are closed for the entire month?">
+                <v-radio-group :disabled="isLocked" row v-model="model.hasClosedMonth" label="Are there months when ALL of the programs at this facility are closed for the entire month?">
                   <v-radio label="Yes" value="yes" />
                   <v-radio label="No" value="no" />
                 </v-radio-group>
@@ -45,43 +45,43 @@
 
             <v-row v-show="model.hasClosedMonth === 'yes'">
               <v-col>
-                <v-checkbox :readonly="isLocked" v-model="model.closedIn1" label="Jan" />
+                <v-checkbox :disabled="isLocked" v-model="model.closedIn1" label="Jan" />
               </v-col>
               <v-col>
-                <v-checkbox :readonly="isLocked" v-model="model.closedIn2" label="Feb" />
+                <v-checkbox :disabled="isLocked" v-model="model.closedIn2" label="Feb" />
               </v-col>
               <v-col>
-                <v-checkbox :readonly="isLocked" v-model="model.closedIn3" label="Mar" />
+                <v-checkbox :disabled="isLocked" v-model="model.closedIn3" label="Mar" />
               </v-col>
               <v-col>
-                <v-checkbox :readonly="isLocked" v-model="model.closedIn4" label="Apr" />
+                <v-checkbox :disabled="isLocked" v-model="model.closedIn4" label="Apr" />
               </v-col>
               <v-col>
-                <v-checkbox :readonly="isLocked" v-model="model.closedIn5" label="May" />
+                <v-checkbox :disabled="isLocked" v-model="model.closedIn5" label="May" />
               </v-col>
               <v-col>
-                <v-checkbox :readonly="isLocked" v-model="model.closedIn6" label="Jun" />
+                <v-checkbox :disabled="isLocked" v-model="model.closedIn6" label="Jun" />
               </v-col>
             </v-row>
 
             <v-row v-show="model.hasClosedMonth === 'yes'">
               <v-col>
-                <v-checkbox :readonly="isLocked" v-model="model.closedIn7" label="Jul" />
+                <v-checkbox :disabled="isLocked" v-model="model.closedIn7" label="Jul" />
               </v-col>
               <v-col>
-                <v-checkbox :readonly="isLocked" v-model="model.closedIn8" label="Aug" />
+                <v-checkbox :disabled="isLocked" v-model="model.closedIn8" label="Aug" />
               </v-col>
               <v-col>
-                <v-checkbox :readonly="isLocked" v-model="model.closedIn9" label="Sep" />
+                <v-checkbox :disabled="isLocked" v-model="model.closedIn9" label="Sep" />
               </v-col>
               <v-col>
-                <v-checkbox :readonly="isLocked" v-model="model.closedIn10" label="Oct" />
+                <v-checkbox :disabled="isLocked" v-model="model.closedIn10" label="Oct" />
               </v-col>
               <v-col>
-                <v-checkbox :readonly="isLocked" v-model="model.closedIn11" label="Nov" />
+                <v-checkbox :disabled="isLocked" v-model="model.closedIn11" label="Nov" />
               </v-col>
               <v-col>
-                <v-checkbox :readonly="isLocked" v-model="model.closedIn12" label="Dec" />
+                <v-checkbox :disabled="isLocked" v-model="model.closedIn12" label="Dec" />
               </v-col>
             </v-row>
 
@@ -94,7 +94,7 @@
                   <v-time-picker v-if="model.menu1" v-model="model.hoursFrom" full-width @click:minute="$refs.menu1.save(model.hoursFrom); model.hoursFrom12hr = formatTime(model.hoursFrom)" :allowed-minutes="allowedStep" />
                 </v-menu>
 
-                <v-text-field v-if="isLocked" readonly outlined required v-model="model.hoursFrom12hr" label="Facility hours of operation From" />
+                <v-text-field v-if="isLocked" disabled outlined required v-model="model.hoursFrom12hr" label="Facility hours of operation From" />
               </v-col>
 
               <v-col cols="12" md="6">
@@ -104,7 +104,7 @@
                   </template>
                   <v-time-picker v-if="model.menu2" v-model="model.hoursTo" full-width @click:minute="$refs.menu2.save(model.hoursTo); model.hoursTo12hr = formatTime(model.hoursTo)" :allowed-minutes="allowedStep" />
 
-                  <v-text-field v-if="isLocked" readonly outlined required v-model="model.hoursTo12hr" label="Facility hours of operation To" />
+                  <v-text-field v-if="isLocked" disabled outlined required v-model="model.hoursTo12hr" label="Facility hours of operation To" />
                 </v-menu>
               </v-col>
             </v-row>
@@ -115,11 +115,11 @@
           <v-container>
             <v-row>
               <v-col cols="12" md="6">
-                <v-text-field :readonly="isLocked" type="number" outlined v-model.number="model.maxSpaces" label="Maximum number of child care spaces you offer" />
+                <v-text-field :disabled="isLocked" type="number" outlined v-model.number="model.maxSpaces" label="Maximum number of child care spaces you offer" />
                 <label>Note: DO NOT include any children living in your home, under the age of 12.</label>
               </v-col>
               <v-col cols="12" md="6">
-                <v-text-field :readonly="isLocked" type="number" outlined required :rules="rules.required" v-model.number="model.maxLicensesCapacity" label="Maximum licensed capacity" />
+                <v-text-field :disabled="isLocked" type="number" outlined required :rules="rules.required" v-model.number="model.maxLicensesCapacity" label="Maximum licensed capacity" />
                 <label>(as indicated on your Community care and assisted Living Act License)</label>
               </v-col>
             </v-row>
@@ -130,7 +130,7 @@
           <v-container>
             <v-row>
               <v-col>
-                <v-radio-group :readonly="isLocked" row v-model="model.isExtendedHours" label="Do you regularly offer extended daily hours of child care (before 6 am, after 7pm or overnight)?">
+                <v-radio-group :disabled="isLocked" row v-model="model.isExtendedHours" label="Do you regularly offer extended daily hours of child care (before 6 am, after 7pm or overnight)?">
                   <v-radio label="Yes" value="yes" />
                   <v-radio label="No" value="no" />
                 </v-radio-group>
@@ -139,19 +139,19 @@
 
             <v-row v-show="model.isExtendedHours === 'yes'">
               <v-col>
-                <v-text-field :readonly="isLocked" type="number" outlined required :rules="model.isExtendedHours === 'yes' ? rules.required : []" v-model.number="model.maxCapacityExtended" label="Maximum number of spaces you offer extended hours of child care?" />
+                <v-text-field :disabled="isLocked" type="number" outlined required :rules="model.isExtendedHours === 'yes' ? rules.required : []" v-model.number="model.maxCapacityExtended" label="Maximum number of spaces you offer extended hours of child care?" />
               </v-col>
             </v-row>
 
             <v-row v-show="model.isExtendedHours === 'yes'">
               <v-col>
-                <v-text-field :readonly="isLocked" type="number" outlined required :rules="model.isExtendedHours === 'yes' ? rules.required : []" v-model.number="model.maxDaysPerWeekExtended" label="Maximum number of days per week you offer extended hours of child care?" />
+                <v-text-field :disabled="isLocked" type="number" outlined required :rules="model.isExtendedHours === 'yes' ? rules.required : []" v-model.number="model.maxDaysPerWeekExtended" label="Maximum number of days per week you offer extended hours of child care?" />
               </v-col>
             </v-row>
 
             <v-row v-show="model.isExtendedHours === 'yes'">
               <v-col>
-                <v-text-field :readonly="isLocked" type="number" outlined required :rules="model.isExtendedHours === 'yes' ? rules.required : []" v-model.number="model.maxWeeksPerYearExtended" label="Maximum number of weeks per year you offer extended hours of child care?" />
+                <v-text-field :disabled="isLocked" type="number" outlined required :rules="model.isExtendedHours === 'yes' ? rules.required : []" v-model.number="model.maxWeeksPerYearExtended" label="Maximum number of weeks per year you offer extended hours of child care?" />
               </v-col>
             </v-row>
 
