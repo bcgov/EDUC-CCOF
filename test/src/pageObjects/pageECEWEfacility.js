26d0c3f99baffdadf43e269d395ad695c0aaa611
Wang <yiyi.wang@cgi.com>
2023-01-12 23:29:08 -0800

    ECEWE pages

--- test/src/pageObjects/pageECEWEfacility.js
+++ test/src/pageObjects/pageECEWEfacility.js
@@ -1,88 +1,30 @@
-import {findSourceMap} from 'module';
-import {Selector} from 'testcafe';
-import log from 'npmlog';
-
+import { Selector } from 'testcafe';
 const {getButton} = require('../utils/selectors');
 const fs = require('fs');
 const path = require('path');
 
-class PageECEWEfacility {
+class pageECEWEfacility {
 
-  constructor() {
-    this.ECEWEButton = Selector('div').withExactText('ECE-WE');
-    this.facilityButton = Selector('div').withExactText('Facility');
-    this.backButton = getButton('Back');
-    this.nextButton = getButton('Next');
-    this.saveButton = getButton('Save');
-  }
-
-  async updateOptFromFile(t, fileName) {
-    const updateButtonCount = await Selector('button').child('span').withText('Update').count;
-    log.info('updateButtonCount: ' + updateButtonCount);
-    let data = fs.readFileSync(path.join(__dirname, '..', 'data', `${fileName}`), 'utf-8');
-    let lines = data.split('\n');
-    const length = lines.length;
-    log.info('length: ' + length);
-    if(updateButtonCount === length){
-      for (let index = 0; index < length; index++) {
-        await t.click(getButton('Update').nth(0)).wait(1000);
-        await t.click(Selector('label').withText(lines[index].trim()).nth(index));
-      }
-    }else{
-      for (let index = 0; index < length; index++) {
-        await t.click(this.getRadioTextField(lines[index].trim()));
-       /* if(getButton('Update').exists){
-          log.info('Update button exists');
-          await t.click(getButton('Update').nth(0)).wait(1000);
-          await t.click(this.getRadioTextField(lines[index].trim()));
-          await t.click(Selector('label').withText(lines[index].trim()).nth(0));
-        }else{
-          log.info('Update button does not exist');
-          await t.click(this.getRadioTextField(lines[index].trim()));
-          //await t.click(Selector('label').withText(lines[index].trim()).nth(index));
-        }*/
-      }
+    constructor() {
+      this.ECEWEButton = Selector('div').withExactText('ECE-WE');
+      this.facilityButton = Selector('div').withExactText('Facility');
+      this.backButton = getButton('Back');
+      this.nextButton = getButton('Next');
+      this.saveButton = getButton('Save');
     }
+  
+    async updateOptFromFile(t, fileName){
+        let data = fs.readFileSync(path.join(__dirname, '..', 'data', `${fileName}`), 'utf-8');
+        let lines = data.split('\n');
+        const length = lines.length;
+        for(let index = 0; index < length; index++){
+          if(await Selector('button').child('span').withText('Update').exists){
+            console.log("---------test---------");
+            await t.click(getButton('Update').nth(0)).wait(1000);
+          }
+            console.log("---------test222222---------");
+            // await t.click(Selector('label').withText(lines[index].trim()).nth(index));
+        }
+      }
   }
-
-  async clickSaveButton(t) {
-    await t.click(this.saveButton).wait(3000);
-    log.info('Save button clicked');
-
-  }
-
-  async clickBackButton(t) {
-    await t.click(this.backButton);
-    log.info('Back button clicked');
-  }
-
-  async clickNextButton(t) {
-    await t.click(this.nextButton);
-    log.info('Next button clicked');
-  }
-
-  async clickSaveAndNextButton(t) {
-    await this.clickSaveButton(t);
-    await this.clickNextButton(t);
-  }
-
-  async nextButtonIsDisabled(t) {
-    await t.expect(this.nextButton.hasAttribute('disabled')).ok();
-    log.info('Next button is disabled');
-  }
-
-  async nextButtonIsEnabled(t) {
-    await t.expect(this.nextButton.hasAttribute('disabled')).notOk();
-    log.info('Next button is enabled');
-  }
-  async getUpdateButton(index) {
-   return Selector('button', {timeout: 10000}).child('span').withText('Update').nth(index).parent();
-  }
-
-  async getRadioTextField(fieldName){
-    return Selector('legend').withExactText(fieldName).nextSibling().find('label').withExactText(fieldName);
-  }
-
-}
-
-export default PageECEWEfacility;
+  export default pageECEWEfacility;
--- test/src/testCases/updateECEWEEligibility.js
+++ test/src/testCases/updateECEWEEligibility.js
@@ -1,18 +1,15 @@
 import PageLogin from '../pageObjects/PageLogin';
 import PageLanding from '../pageObjects/PageLanding';
-import PageECEWEEligibility from '../pageObjects/pageECEWEEligibility';
+import pageECEWEEligibility from '../pageObjects/pageECEWEEligibility';
 import PageAlert from '../pageObjects/PageAlert';
-import PageECEWEfacility from '../pageObjects/pageECEWEfacility';
 
 const config = require('../utils/configLoader');
 
 
 const login = new PageLogin();
 const landing = new PageLanding();
-
-const eligibility = new PageECEWEEligibility();
+const eligibility = new pageECEWEEligibility();
 const alert = new PageAlert();
-const opt = new PageECEWEfacility();
 
 fixture `ECE-WE Eligibility Tests`
   .page(`${config.get('url')}/login`)
@@ -28,10 +25,11 @@ test('Update ECE-WE Eligibility', async t => {
     .wait(2000);
   await t.click(eligibility.ECEWEButton);
   await t.click(eligibility.eligibilityButton);
-  await eligibility.updateOptionFromFile(t, 'ee2eTest-ecewe-eligibility.txt');
-  await eligibility.clickSaveAndNextButton(t);
-  await opt.updateOptFromFile(t, 'e2eTest-ecewe-facility.txt');
-  await opt.clickSaveAndNextButton(t);
+  await eligibility.updateOptionFromFile(t, 'ecewe-eligibility.txt');
+  await t.expect(eligibility.nextButton.hasAttribute('disabled')).notOk();
+  await t.click(eligibility.saveButton).wait(3000);
+  await t.expect(alert.success.exists).ok();
+  await t.click(eligibility.nextButton);
 
 });
 
