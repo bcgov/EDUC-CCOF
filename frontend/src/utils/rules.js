04c506c0b0b1bb79612d7d341d9a1e6cc248fb2c
Malikov <alexey.malikov@cgi.com>
2023-01-16 15:33:39 -0700

    CCFRI-821 email validation

--- frontend/src/utils/rules.js
+++ frontend/src/utils/rules.js
@@ -1,5 +1,5 @@
 const rules = {
-  email: [v => /.+@.+/.test(v) || 'A valid email is required'],
+  email: [v => /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(v) || 'A valid email is required'], // https://emailregex.com/
   required: [function (v) {
     if (v === 0) {
       return true;
