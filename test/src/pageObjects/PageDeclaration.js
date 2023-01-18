bf5fc9d744e3c6bd39aaffd91f6565063b2be6d7
Wang <yiyi.wang@cgi.com>
2023-01-17 21:36:24 -0800

    Finishing the new application process

--- test/src/pageObjects/PageDeclaration.js
+++ test/src/pageObjects/PageDeclaration.js
@@ -1,5 +1,4 @@
 import { Selector } from 'testcafe';
-import log from 'npmlog';
 const { getButton, getTextField } = require('../utils/selectors');
 
 
@@ -13,38 +12,5 @@ class PageDeclaration {
     this.header = Selector('span').withText('Declaration');
     this.checkbox = Selector('input').withAttribute('role', 'checkbox');
   }
-
-  async clickBackButton(t) {
-    await t.click(this.backButton);
-    log.info('Back button clicked');
-  }
-
-  async clickDeclarationCheckBox(t) {
-    await t.click(this.checkbox);
-    log.info('Declaration Checkbox clicked');
-
-  }
-
-  async singDeclaration(t,signatureText) {
-    await t.typeText(this.signField, signatureText);
-    log.info('Declaration Signature Entered');
-  }
-
-  async clickSubmitButton(t) {
-    await t.click(this.submitButton).wait(2000);
-    log.info('Declaration Submit Button Clicked');
-  }
-
-  async submitButtonIsDisabled(t) {
-    await t.expect(this.submitButton.hasAttribute('disabled')).ok();
-    log.info('Next button is disabled');
-  }
-
-  async submitButtonIsEnabled(t) {
-    await t.expect(this.submitButton.hasAttribute('disabled')).notOk();
-    log.info('Next button is enabled');
-  }
-
-
 }
 export default PageDeclaration;
