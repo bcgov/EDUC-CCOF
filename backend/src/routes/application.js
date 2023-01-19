462abe2fbf09087f366cea647d178950a59d6158
Rob Lo <rob.lo@cgi.com>
2023-01-18 22:20:49 -0800

    updated dependencies and cleaned up code

--- backend/src/routes/application.js
+++ backend/src/routes/application.js
@@ -7,48 +7,15 @@ const { getRFIMedian, getRFIApplication, createRFIApplication, updateRFIApplicat
 const { upsertParentFees, updateCCFRIApplication, renewCCOFApplication } = require('../components/application');
 const { getECEWEApplication, updateECEWEApplication, updateECEWEFacilityApplication, getCCFRIApplication, getDeclaration, submitApplication} = require('../components/application');
 const { getNMFApplication, updateNMFApplication, createNMFApplication } = require('../components/nmfApplication');
-const { param, validationResult, checkSchema} = require('express-validator');
-const { log } = require('../components/logger');
-
+const { param, validationResult } = require('express-validator');
 
 router.post('/renew-ccof', passport.authenticate('jwt', {session: false}),isValidBackendToken, [],  (req, res) => {
-  //validationResult(req).throw();
-  //console.log(req.bpdy);
   return renewCCOFApplication(req, res);
 });
-
-// const facilitySchema = {
-//   facilityName: { in: ['body'],
-//     exists: { errorMessage: '[facilityName] is required', },
-//     isLength: { options: { max: 160 }, errorMessage: '[legalName] has a max length of 160'}},
-//   facilityAddress: { in: ['body'],
-//     exists: { errorMessage: '[address1] is required', },
-//     isLength: { options: { max: 250 }, errorMessage: '[address1] has a max length of 250'}},
-//   city: { in: ['body'],
-//     exists: { errorMessage: '[city] is required', },
-//     isLength: { options: { max: 80 }, errorMessage: '[city1] has a max length of 80'}},
-//   organizationId: { in: ['body'],
-//     exists: { errorMessage: '[organizationId] is required', },
-//     isBase64: { errorMessage: '[organizationId] must be a GUID'}},
-//   yearBeganOperation: { in: ['body'],
-//     exists: { errorMessage: '[yearBeganOperation] is required', },
-//     isDate: { errorMessage: '[yearBeganOperation] must be a date'}}
-// };
-
-
-/* CREATE a NEW CCFRI application */
-//post does not work rn, so leaving this commented out - JB
-
-// router.post('/ccfri', passport.authenticate('jwt', {session: false}),isValidBackendToken, [], (req, res) => {
-//   //validationResult(req).throw();
-//   return createCCFRIApplication(req, res);
-// });
-
 
 /* CREATE or UPDATE an existing CCFRI application for opt-in and out
   CCOF application guid and facility guid are defined in the payload
 */
-
 router.get('/ccfri/:ccfriId', passport.authenticate('jwt', {session: false}),isValidBackendToken,
   [param('ccfriId', 'URL param: [ccfriId] is required').not().isEmpty()], (req, res) => {
     validationResult(req).throw();
