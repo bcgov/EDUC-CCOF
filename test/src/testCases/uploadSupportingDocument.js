a0188cfdb48b426dfcf858ef466666b937e75600
Wang <yiyi.wang@cgi.com>
2023-01-23 14:34:22 -0800

    update

--- test/src/testCases/uploadSupportingDocument.js
+++ test/src/testCases/uploadSupportingDocument.js
@@ -12,7 +12,7 @@ const landing = new PageLanding();
 const upload = new PageSupportingDocument();
 const alert = new PageAlert();
 const acceptFile = ["jpg", "jpeg", "png", "pdf", "docx", "doc", "xls", "xlsx", "heic"];
-const facilityName = "test2";
+const facilityName = "Test DEV 07 Next Next Fac";
 
 
 fixture `Upload Support Document`
@@ -22,35 +22,35 @@ fixture `Upload Support Document`
     await t.setTestSpeed(1);
   });
 
-  // test('Upload Support Document', async t => {
-  //   await login.bceIdLogin(t);
-  //   await t
-  //     .click(landing.continueButton)
-  //     .wait(2000);
-  //   await t.click(upload.supportingDocumentButton).wait(1000);
+  test('Upload Support Document', async t => {
+    await login.bceIdLogin(t);
+    await t
+      .click(landing.continueButton)
+      .wait(2000);
+    await t.click(upload.supportingDocumentButton).wait(1000);
 
-  //   for(let i = 0; i < acceptFile.length; i++){
-  //     const fileName = `sample_${acceptFile[i]}.${acceptFile[i]}`;
-  //     await upload.uploadFile(t, facilityName, fileName, true);
-  //     await t.takeScreenshot({fullPage: true});
-  //     await t.click(upload.saveButton).wait(2000);
-  //     await t.takeScreenshot({fullPage: true});
-  //   }
-  //   await t.expect(Selector('.v-system-bar').exists).ok({ timeout: 5000 });
-  // });
-
-
-  test('Delete Support Document', async t => {
-      await login.bceIdLogin(t);
-      await t
-        .click(landing.continueButton)
-        .wait(2000);
-      
-      await t.click(upload.supportingDocumentButton).wait(1000);
-      await upload.deleteFile(t, 'test2', 'sample_jpeg');
-      await t.click(upload.saveButton);
+    for(let i = 0; i < acceptFile.length; i++){
+      const fileName = `sample_${acceptFile[i]}.${acceptFile[i]}`;
+      await upload.uploadFile(t, facilityName, fileName, true);
       await t.takeScreenshot({fullPage: true});
-
+      await t.click(upload.saveButton).wait(2000);
       await t.takeScreenshot({fullPage: true});
-      await t.expect(Selector('.v-system-bar').exists).ok({ timeout: 5000 });
+    }
+    await t.expect(Selector('.v-system-bar').exists).ok({ timeout: 5000 });
   });
+
+
+  // test('Delete Support Document', async t => {
+  //     await login.bceIdLogin(t);
+  //     await t
+  //       .click(landing.continueButton)
+  //       .wait(2000);
+      
+  //     await t.click(upload.supportingDocumentButton).wait(1000);
+  //     await upload.deleteFile(t, 'test2', 'sample_jpeg');
+  //     await t.click(upload.saveButton);
+  //     await t.takeScreenshot({fullPage: true});
+
+  //     await t.takeScreenshot({fullPage: true});
+  //     await t.expect(Selector('.v-system-bar').exists).ok({ timeout: 5000 });
+  // });
