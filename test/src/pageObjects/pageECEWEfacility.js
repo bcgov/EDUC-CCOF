79bfde79f19e424608241adf7a1c291461eb8ab0
sukanya-rath <98050194+sukanya-rath@users.noreply.github.com>
2023-02-06 09:56:41 -0800

    E2E changes

--- test/src/pageObjects/pageECEWEfacility.js
+++ test/src/pageObjects/pageECEWEfacility.js
@@ -1,31 +1,65 @@
-import { findSourceMap } from 'module';
-import { Selector } from 'testcafe';
+import {findSourceMap} from 'module';
+import {Selector} from 'testcafe';
+import log from 'npmlog';
+
 const {getButton} = require('../utils/selectors');
 const fs = require('fs');
 const path = require('path');
 
-class pageECEWEfacility {
+class PageECEWEfacility {
 
-    constructor() {
-      this.ECEWEButton = Selector('div').withExactText('ECE-WE');
-      this.facilityButton = Selector('div').withExactText('Facility');
-      this.backButton = getButton('Back');
-      this.nextButton = getButton('Next');
-      this.saveButton = getButton('Save');
-    }
-  
-    async updateOptFromFile(t, fileName){
-        let data = fs.readFileSync(path.join(__dirname, '..', 'data', `${fileName}`), 'utf-8');
-        let lines = data.split('\n');
-        const length = lines.length;
-        for(let index = 0; index < length; index++){
-          if(await getButton('Update').exists){
-            await t.click(Selector('button', {timeout: 10000}).child('span').withText('Update').nth(index).parent()).wait(1000);
-            await t.click(Selector('label').withText(lines[index].trim()).nth(0)).wait(1000);
-          }else{
-            await t.click(Selector('label').withText(lines[index].trim()).nth(index));
-          }
-        }
-      }
+  constructor() {
+    this.ECEWEButton = Selector('div').withExactText('ECE-WE');
+    this.facilityButton = Selector('div').withExactText('Facility');
+    this.backButton = getButton('Back');
+    this.nextButton = getButton('Next');
+    this.saveButton = getButton('Save');
   }
-  export default pageECEWEfacility;
+
+  async updateOptFromFile(t, fileName) {
+    let data = fs.readFileSync(path.join(__dirname, '..', 'data', `${fileName}`), 'utf-8');
+    let lines = data.split('\n');
+    const length = lines.length;
+    for (let index = 0; index < length; index++) {
+      if (await getButton('Update').exists) {
+        await t.click(Selector('button', {timeout: 10000}).child('span').withText('Update').nth(index).parent()).wait(1000);
+        await t.click(Selector('label').withText(lines[index].trim()).nth(0)).wait(1000);
+      } else {
+        await t.click(Selector('label').withText(lines[index].trim()).nth(index));
+      }
+    }
+  }
+
+  async clickSaveButton(t) {
+    await t.click(this.saveButton).wait(3000);
+    log.info('Save button clicked');
+
+  }
+
+  async clickBackButton(t) {
+    await t.click(this.backButton);
+    log.info('Back button clicked');
+  }
+
+  async clickNextButton(t) {
+    await t.click(this.nextButton);
+    log.info('Next button clicked');
+  }
+
+  async clickSaveAndNextButton(t) {
+    this.clickSaveButton(t);
+    this.clickNextButton(t);
+  }
+
+  async nextButtonIsDisabled(t) {
+    await t.expect(this.nextButton.hasAttribute('disabled')).ok();
+    log.info('Next button is disabled');
+  }
+
+  async nextButtonIsEnabled(t) {
+    await t.expect(this.nextButton.hasAttribute('disabled')).notOk();
+    log.info('Next button is enabled');
+  }
+}
+
+export default PageECEWEfacility;
