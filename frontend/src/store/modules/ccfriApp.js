4ed365d247e163cff493f2419a75aa29ea32c63d
roblo-cgi <rob.lo@cgi.com>
2023-04-05 10:40:34 -0700

    updates to when missing previous ccfriid

--- frontend/src/store/modules/ccfriApp.js
+++ frontend/src/store/modules/ccfriApp.js
@@ -183,6 +183,8 @@ export default {
         let careTypes = [];
         const currProgramYear = getProgramYear(ccofProgramYearId, programYearList);
         const prevProgramYear = getProgramYear(currProgramYear.previousYearId, programYearList);
+        const prevCcfriApp = await state.ccfriStore[state.CCFRIFacilityModel.previousCcfriId];
+
         console.log('currProgramYear', currProgramYear);
 
         //Always show the current year fee cards
@@ -201,10 +203,8 @@ export default {
           }
         });
 
-
-
-        //only display ALL previous year fee cards if it's the first time CCFRI application  -- OR prev fees are incorrect
-        if (!rootState.app.isRenewal || state.CCFRIFacilityModel.existingFeesCorrect != 100000000){
+        //display ALL previous year fee cards if it's the first time CCFRI application OR prev fees are incorrect OR if prev CCFRI is not found
+        if (!rootState.app.isRenewal || state.CCFRIFacilityModel.existingFeesCorrect != 100000000 || !prevCcfriApp){
           response.data.forEach(item => {
 
             //check for undefined here!
@@ -227,13 +227,11 @@ export default {
           });
         }
 
-
         /*
           first check if we are missing fee cards from last year. This can happen when a user has a new license for this year.
           Then check if we have any cards that don't belong (for example user selects NO fees are not correct, then goes back and selects YES)
         */
-        else if (rootState.app.isRenewal  && state.CCFRIFacilityModel.existingFeesCorrect == 100000000){
-          const prevCcfriApp = state.ccfriStore[state.CCFRIFacilityModel.previousCcfriId];
+        else if (rootState.app.isRenewal && state.CCFRIFacilityModel.existingFeesCorrect == 100000000 && prevCcfriApp){
           console.log('prevCCFRI IS:' , prevCcfriApp);
           response.data.forEach(item => {
 
