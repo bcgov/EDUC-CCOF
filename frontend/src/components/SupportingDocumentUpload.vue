d7b5f393a3ce5a9d513e92bcc9fad6248673cf62
Le <viet.le@cgi.com>
2023-04-11 15:11:29 -0700

    NavButton - update processing status

--- frontend/src/components/SupportingDocumentUpload.vue
+++ frontend/src/components/SupportingDocumentUpload.vue
@@ -116,7 +116,7 @@
         </v-card>
       </v-row>
       <NavButton :isNextDisplayed="true" :isSaveDisplayed="true"
-        :isSaveDisabled="!isSaveDisabled || isLocked" :isNextDisabled="!isNextEnabled" :isProcessing="processing" 
+        :isSaveDisabled="!isSaveDisabled || isLocked" :isNextDisabled="!isNextEnabled" :isProcessing="isProcessing" 
         @previous="previous" @next="next" @validateForm="validateForm()" @save="save(true)"></NavButton>
     </v-container>
   </v-form>
