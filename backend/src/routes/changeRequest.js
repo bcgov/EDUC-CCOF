1426d8446073ca273b0938102761154c64bcb710
roblo-cgi <rob.lo@cgi.com>
2023-04-19 15:16:42 -0700

    updates to change request api

--- backend/src/routes/changeRequest.js
+++ backend/src/routes/changeRequest.js
@@ -2,17 +2,25 @@ const express = require('express');
 const passport = require('passport');
 const router = express.Router();
 const auth = require('../components/auth');
-const isValidBackendToken= auth.isValidBackendToken();
-const { getChangeRequest, createChangeRequest } = require('../components/changeRequest');
-const { param, validationResult, checkSchema} = require('express-validator');
+const isValidBackendToken = auth.isValidBackendToken();
+const { getChangeRequest, createChangeRequest, CHANGE_REQUEST_TYPES } = require('../components/changeRequest');
+const { param, validationResult, checkSchema } = require('express-validator');
 
 module.exports = router;
 
 const changeRequestSchema = {
-  applicationId: { in: ['body'],
-    exists: { errorMessage: '[applicationId] is required', }},
-  programYearId: { in: ['body'],
-    exists: { errorMessage: '[programYearId] is required', }}
+  applicationId: {
+    in: ['body'],
+    exists: { errorMessage: '[applicationId] is required', }
+  },
+  programYearId: {
+    in: ['body'],
+    exists: { errorMessage: '[programYearId] is required', }
+  },
+  providerType: {
+    in: ['body'],
+    exists: { errorMessage: '[providerType] is required', }
+  }
 };
 
 /**
@@ -27,10 +35,18 @@ router.get('/:changeRequestId', //passport.authenticate('jwt', {session: false})
 /**
  * Create the change Request
  */
-router.post('/', //passport.authenticate('jwt', {session: false}),isValidBackendToken,
+router.post('/newFacility', //passport.authenticate('jwt', {session: false}),isValidBackendToken,
   [checkSchema(changeRequestSchema)], (req, res) => {
-  validationResult(req).throw();
-  return createChangeRequest(req, res);
-});
+    validationResult(req).throw();
+    return createChangeRequest(req, res, CHANGE_REQUEST_TYPES.NEW_FACILITY);
+  });
 
+/**
+ * Create the change request upload document
+ */
+router.post('/documents', //passport.authenticate('jwt', {session: false}),isValidBackendToken,
+  [checkSchema(changeRequestSchema)], (req, res) => {
+    validationResult(req).throw();
+    return createChangeRequest(req, res, CHANGE_REQUEST_TYPES.PDF_CHANGE);
+  });
 module.exports = router;
