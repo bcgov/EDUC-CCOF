eb60e08fb529bd82671550d427c03c7f8b92af35
roblo-cgi <rob.lo@cgi.com>
2023-04-21 10:39:37 -0700

    added add new facility change request

--- backend/src/components/changeRequest.js
+++ backend/src/components/changeRequest.js
@@ -3,6 +3,8 @@
 const log = require('./logger');
 const { MappableObjectForFront, MappableObjectForBack } = require('../util/mapping/MappableObject');
 const { ChangeRequestMappings } = require('../util/mapping/ChangeRequestMappings');
+const { mapFacilityObjectForBack } = require('./facility');
+const { ACCOUNT_TYPE, APPLICATION_STATUS_CODES, ORGANIZATION_PROVIDER_TYPES } = require('../util/constants');
 
 const HttpStatus = require('http-status-codes');
 
@@ -15,7 +17,7 @@ const CHANGE_REQUEST_TYPES = Object.freeze({
 });
 
 
-function mapOrganizationForBack(data, changeType) {
+function mapChangeRequestForBack(data, changeType) {
   let changeRequestForBack = new MappableObjectForBack(data, ChangeRequestMappings).toJSON();
   changeRequestForBack['ccof_program_year@odata.bind'] = `/ccof_program_years(${data.programYearId})`
   delete changeRequestForBack._ccof_program_year_value;
@@ -23,59 +25,98 @@ function mapOrganizationForBack(data, changeType) {
   changeRequestForBack['ccof_Application@odata.bind'] = `/ccof_applications(${data.applicationId})`
   delete changeRequestForBack._ccof_application_value;
 
-
   changeRequestForBack['ccof_change_action_change_request'] = [
     {
       ccof_changetype: changeType === CHANGE_REQUEST_TYPES.PDF_CHANGE ? 100000013 : 100000005
     }
   ];
+  if (changeType === CHANGE_REQUEST_TYPES.NEW_FACILITY) {
+    changeRequestForBack.ccof_provider_type = 100000000; //New facilities are only available for GROUP provider types
+  }
   return changeRequestForBack;
+}
 
+function mapOrganizationObjectForFront(data) {
+  return new MappableObjectForFront(data, ChangeRequestMappings).toJSON();
+}
 
-  function mapOrganizationObjectForFront(data) {
-    return new MappableObjectForFront(data, ChangeRequestMappings).toJSON();
+// get Change Request
+async function getChangeRequest(req, res) {
+  log.info('get changeRequest called');
+
+  try {
+    let changeRequest = await getOperationWithObjectId('ccof_change_requests', req.params.changeRequestId);
+    changeRequest = mapOrganizationObjectForFront(changeRequest);
+
+    return res.status(HttpStatus.OK).json(changeRequest);
+  } catch (e) {
+    console.log('e', e);
+    return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(e.data ? e.data : e?.status);
   }
+}
 
-  // get Change Request
-  async function getChangeRequest(req, res) {
-    log.info('get changeRequest called');
-
-    try {
-      let changeRequest = await getOperationWithObjectId('ccof_change_requests', req.params.changeRequestId);
-      changeRequest = mapOrganizationObjectForFront(changeRequest);
-
-      return res.status(HttpStatus.OK).json(changeRequest);
-    } catch (e) {
-      console.log('e', e);
-      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(e.data ? e.data : e?.status);
+// create Change Request
+async function createChangeRequest(req, res, changeType) {
+  log.info('createChangeRequest called');
+  try {
+    let changeRequest = req.body;
+    changeRequest = mapChangeRequestForBack(changeRequest, changeType);
+    const changeRequestId = await postOperation('ccof_change_requests', changeRequest);
+    let operation = `ccof_change_requests(${changeRequestId})?$select=ccof_change_requestid&$expand=ccof_change_action_change_request($select=ccof_change_actionid,statuscode)`;
+    const payload = await getOperation(operation);
+    let changeActionId = undefined;
+    if (payload && payload.ccof_change_action_change_request?.length > 0) {
+      changeActionId = payload.ccof_change_action_change_request[0].ccof_change_actionid;
     }
-  }
+    if (changeType === CHANGE_REQUEST_TYPES.NEW_FACILITY) {
 
-  // create Change Request
-  async function createChangeRequest(req, res, changeType) {
-    log.info('createChangeRequest called');
-    try {
-      let changeRequest = req.body;
-      changeRequest = mapOrganizationForBack(changeRequest, changeType);
-      const changeRequestId = await postOperation('ccof_change_requests', changeRequest);
-      let operation = `ccof_change_requests(${changeRequestId})?$select=ccof_change_requestid&$expand=ccof_change_action_change_request($select=ccof_change_actionid,statuscode)`;
-      const payload = await getOperation(operation);
-      let changeActionId = undefined;
-      if (payload && payload.ccof_change_action_change_request?.length > 0) {
-        changeActionId = payload.ccof_change_action_change_request[0].ccof_change_actionid;
-      }
-      return res.status(HttpStatus.CREATED).json({
-        changeRequestId: changeRequestId,
-        changeActionId: changeActionId,
-      });
-    } catch (e) {
-      log.error('error', e);
-      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(e.data ? e.data : e?.status);
     }
+    return res.status(HttpStatus.CREATED).json({
+      changeRequestId: changeRequestId,
+      changeActionId: changeActionId,
+    });
+  } catch (e) {
+    log.error('error', e);
+    return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(e.data ? e.data : e?.status);
   }
+}
 
-  module.exports = {
-    getChangeRequest,
-    createChangeRequest,
-    CHANGE_REQUEST_TYPES,
-  };
+function buildNewFacilityPayload(req) {
+  let facility = req.body;
+
+  facility = mapFacilityObjectForBack(facility);
+  facility['ccof_accounttype'] = ACCOUNT_TYPE.FACILITY;
+  facility['parentaccountid@odata.bind'] = `/accounts(${req.body.organizationId})`,
+  facility['ccof_ccof_change_request_new_facility_facility'] = [
+    {
+      'ccof_change_action@odata.bind': `/ccof_change_actions(${req.params.changeActionId})`,
+    }
+  ];
+
+  return facility;
+}
+
+
+async function createChangeRequestFacility(req, res) {
+  let facility = buildNewFacilityPayload(req);
+  try {
+    const facilityGuid = await postOperation('accounts', facility);
+    //After the 'ChangeActionNewFacility' entity is created, grab the guid
+    let operation = 'accounts(' + facilityGuid + ')?$select=accountid&$expand=ccof_ccof_change_request_new_facility_facility($select=ccof_change_request_new_facilityid,statuscode)';
+    let payload = await getOperation(operation);
+    let changeRequestNewFacilityId = undefined;
+    if ( payload?.ccof_ccof_change_request_new_facility_facility?.length > 0) {
+      changeRequestNewFacilityId = payload.ccof_ccof_change_request_new_facility_facility[0].ccof_change_request_new_facilityid;
+    }
+    return res.status(HttpStatus.CREATED).json({facilityId: facilityGuid, changeRequestNewFacilityId: changeRequestNewFacilityId});
+  } catch (e) {
+    return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(e.data? e.data : e?.status );
+  }
+}
+
+module.exports = {
+  getChangeRequest,
+  createChangeRequest,
+  createChangeRequestFacility,
+  CHANGE_REQUEST_TYPES,
+};
--- backend/src/routes/changeRequest.js
+++ backend/src/routes/changeRequest.js
@@ -3,12 +3,12 @@ const passport = require('passport');
 const router = express.Router();
 const auth = require('../components/auth');
 const isValidBackendToken = auth.isValidBackendToken();
-const { getChangeRequest, createChangeRequest, CHANGE_REQUEST_TYPES } = require('../components/changeRequest');
+const { getChangeRequest, createChangeRequest, createChangeRequestFacility, CHANGE_REQUEST_TYPES } = require('../components/changeRequest');
 const { param, validationResult, checkSchema } = require('express-validator');
 
 module.exports = router;
 
-const changeRequestSchema = {
+const newFacilityChangeRequestSchema = {
   applicationId: {
     in: ['body'],
     exists: { errorMessage: '[applicationId] is required', }
@@ -17,6 +17,10 @@ const changeRequestSchema = {
     in: ['body'],
     exists: { errorMessage: '[programYearId] is required', }
   },
+};
+
+const documentChangeRequestSchema = {
+  ...newFacilityChangeRequestSchema,
   providerType: {
     in: ['body'],
     exists: { errorMessage: '[providerType] is required', }
@@ -36,16 +40,26 @@ router.get('/:changeRequestId', //passport.authenticate('jwt', {session: false})
  * Create the change Request
  */
 router.post('/newFacility', //passport.authenticate('jwt', {session: false}),isValidBackendToken,
-  [checkSchema(changeRequestSchema)], (req, res) => {
+  [checkSchema(newFacilityChangeRequestSchema)], (req, res) => {
     validationResult(req).throw();
     return createChangeRequest(req, res, CHANGE_REQUEST_TYPES.NEW_FACILITY);
   });
 
 /**
+ * Create the change Request
+ */
+router.post('/newFacility/:changeActionId', //passport.authenticate('jwt', {session: false}),isValidBackendToken,
+  [param('changeActionId', 'URL param: [changeActionId] is required').not().isEmpty()], (req, res) => {
+    validationResult(req).throw();
+    return createChangeRequestFacility(req, res);
+  });
+
+
+/**
  * Create the change request upload document
  */
 router.post('/documents', //passport.authenticate('jwt', {session: false}),isValidBackendToken,
-  [checkSchema(changeRequestSchema)], (req, res) => {
+  [checkSchema(documentChangeRequestSchema)], (req, res) => {
     validationResult(req).throw();
     return createChangeRequest(req, res, CHANGE_REQUEST_TYPES.PDF_CHANGE);
   });
