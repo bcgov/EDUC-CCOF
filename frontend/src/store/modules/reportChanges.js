f98fd1feb757ebac2bbc3083d9ffd4c4e038243b
Jenavieve Beckett <jen.beckett@cgi.com>
2023-05-02 12:18:37 -0700

    WIP change request

--- frontend/src/store/modules/reportChanges.js
+++ frontend/src/store/modules/reportChanges.js
@@ -1,20 +1,24 @@
 import ApiService from '@/common/apiService';
 import {ApiRoutes} from '@/utils/constants';
 import {checkSession} from '@/utils/session';
+import {isEmpty} from 'lodash';
+import {deepCloneObject} from '@/utils/common';
 
 
+//TODO : remove unneeded state vars after restructure
 export default {
   namespaced: true,
   state: {
-    changeActionId: 123,
     changeRequestId: undefined,
     unsubmittedDocuments: [],
     model: {},
+    changeActionStore : {},
 
 
   },
   getters: {
-    changeActionId: state => state.changeActionId,
+    changeActionStore: state => state.changeActionStore,
+    changeActions: state => state.changeActions,
     changeRequestId: state => state.changeRequestId,
     unsubmittedDocuments: state => state.unsubmittedDocuments,
   },
@@ -22,8 +26,10 @@ export default {
     model(state, value) {
       state.model = value;
     },
-    setChangeActionId: (state, changeActionId) => {
-      state.changeActionId = changeActionId;
+    addChangeActionToStore: (state, {changeActionId, model} ) => {
+      if (changeActionId) {
+        state.changeActionStore[changeActionId] = model;
+      }
     },
     setChangeRequestId: (state, changeRequestId) => {
       state.changeRequestId = changeRequestId;
@@ -36,6 +42,37 @@ export default {
     },
   },
   actions: {
+    async loadChangeRequest({getters, commit}, applicationId) {
+      console.log('loading change req for: ', applicationId);
+
+      checkSession();
+
+      try {
+        console.log('');
+        let response = await ApiService.apiAxios.get(ApiRoutes.APPLICATION_CHANGE_REQUEST + '/' + applicationId);
+        console.log(response);
+        if (!isEmpty(response.data)) {
+
+          response.data.forEach(element => {
+            commit('addChangeActionToStore', {changeActionId: element.changeRequestId, model: element});
+          });
+
+
+          //commit('setLoadedModel', deepCloneObject(response.data));
+        }
+        // else {
+
+        //   commit('addRfiToStore', {ccfriId: ccfriId, model: rfi});
+        //   commit('setRfiModel', rfi);
+        //   commit('setLoadedModel', deepCloneObject(rfi));
+        // }
+      } catch(e) {
+        console.log(`Failed to get load change reqz with error - ${e}`);
+        throw e;
+      }
+    },
+
+
     async createChangeRequest({ state, commit, rootState }) {
 
       checkSession();
