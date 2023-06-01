3782e993f45ac1f7821abcba31658125085abfd9
Greg Pascucci <greg.pascucci@gcgi.com>
2023-06-01 13:50:47 -0700

    initial commit of estimation testing pages

---
+++ test/src/testCases/childcareEstimator.js
@@ -0,0 +1,28 @@
+import log from "npmlog";
+const config = require('../utils/configLoader');
+const validation = require('../utils/validation');
+import PageEstimator from '../pageObjects/PageEstimator';
+
+const pageEstimator = new PageEstimator();
+
+fixture `Child Care Estimator Test`
+  .page(`${config.get('url')}/ccfri-estimator`).after(async t => {
+});
+
+  test ('Childcare Estimator Test', async t => {
+    await pageEstimator.parentSelect(t);
+    log.info('Parent Selected.');
+
+    let totChildren = 2;
+    await pageEstimator.addChildren(t, totChildren);
+    log.info(totChildren, ' children added.');
+
+    let typeOfCare = 'Licensed Group';
+    await pageEstimator.typeOfCare(t, typeOfCare);
+    log.info(typeOfCare, ' selected');
+
+    let feeFrequency = 'Daily';
+    await pageEstimator.parentFeeFrequency(t, feeFrequency);
+    log.info(feeFrequency, ' selected.');
+
+  });
