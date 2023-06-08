dcca4fb4d164466b8ed3ab17884d2a3e5682f7de
Greg Pascucci <greg.pascucci@gcgi.com>
2023-06-08 08:32:09 -0700

    initial successful testcafe suite for estimates

--- test/src/pageObjects/PageEstimator.js
+++ test/src/pageObjects/PageEstimator.js
@@ -1,102 +1,100 @@
 import log from "npmlog";
 import { Selector } from 'testcafe';
 const config = require('../utils/configLoader');
-const { getTextFieldById, mapFieldsFromFile, getButton } = require('../utils/selectors');
+const { getTextFieldById, getButton } = require('../utils/selectors');
 
 class PageEstimator {
-    constructor() {
-        this.parentBtn = getButton('Parent');
-        this.totNumberOfChildren = getTextFieldById('#totNumberOfChildren');
-        this.typeOfCareSelector = getTextFieldById('#typeOfCare');
-        this.childAgeCategorySelector = getTextFieldById('#childAgeCategory');
-        this.parentFeeFrequencySelector = getTextFieldById('#parentFeeFrequency');
-        this.fullTimeParentFeeField = getTextFieldById('#approvedFee');
-        this.partTimeFeeField = getTextFieldById('#partTimeFee');
-        this.estimateYourSavingsBtn = getButton('Estimate your savings');
-      }
+  constructor() {
+    this.parentBtn = getButton('Parent');
+    this.totNumberOfChildren = getTextFieldById('#totNumberOfChildren');
+    this.typeOfCareSelector = getTextFieldById('#typeOfCare');
+    this.childAgeCategorySelector = getTextFieldById('#childAgeCategory');
+    this.parentFeeFrequencySelector = getTextFieldById('#parentFeeFrequency');
+    this.fullTimeParentFeeField = getTextFieldById('#approvedFee');
+    this.partTimeFeeField = getTextFieldById('#partTimeFee');
+    this.estimateYourSavingsBtn = getButton('Estimate your savings');
+    this.fourHoursOrLessToSelectList = Selector('div').withAttribute('role', 'option').withExactText('4 hours or less');
+    this.moreThanFourToSelectList = Selector('div').withAttribute('role', 'option').withExactText('More than 4 hours');
+  }
 
-      async parentSelect (t) {
-        await t.click(this.parentBtn)
-        .expect(Selector('#totNumberOfChildren').exists).ok({ timeout: 1000 });
-      }
+  async parentSelect(t) {
+    await t.click(this.parentBtn)
+      .expect(Selector('#totNumberOfChildren').exists).ok({ timeout: 1000 });
+  }
 
-      async addChildren (t, value) {
-        await t.typeText(this.totNumberOfChildren, value.toString(), {replace: true})
-        .expect(this.totNumberOfChildren.value).eql(value.toString());
-      }
+  async addChildren(t, value) {
+    await t.typeText(this.totNumberOfChildren, value.toString(), { replace: true })
+      .expect(this.totNumberOfChildren.value).eql(value.toString());
+  }
 
-      async typeOfCare (t, option) {
-        const typeOfCareSelect = Selector('#list-39');
-        const optionToSelect = Selector('div').withAttribute('role', 'option').withExactText(option)
-        await t.click(this.typeOfCareSelector)
-        .expect(typeOfCareSelect.exists).ok()
-        .expect(optionToSelect.exists).ok()
-        .click(optionToSelect);
-      }
-      
-      async childAgeCategory (t, option) {
-        const childAgeCategorySelect = Selector('#list-62');
-        const optionToSelect = Selector('div').withAttribute('role', 'option').withExactText(option)
-        await t.click(this.childAgeCategorySelector)
-        .expect(childAgeCategorySelect.exists).ok()
-        .expect(optionToSelect.exists).ok()
-        .click(optionToSelect.withText(option));
-      }
+  async typeOfCare(t, option) {
+    const typeOfCareSelect = Selector('div').withAttribute('tabindex', '-1');
+    const optionToSelect = Selector('div').withAttribute('role', 'option').withExactText(option)
+    await t.click(this.typeOfCareSelector)
+      .expect(typeOfCareSelect.exists).ok()
+      .expect(optionToSelect.exists).ok()
+      .click(optionToSelect);
+  }
 
-      async careSchedule (t, noCare, fourOrLess, moreThanFour) {
-        const calendarTable = Selector('div').withAttribute('role', 'list');
-        const noCareToSelectList = Selector('div').withAttribute('role', 'option').withExactText('No care');
-        const fourHoursOrLessToSelectList = Selector('div').withAttribute('role', 'option').withExactText('4 hours or less');
-        const moreThanFourToSelectList = Selector('div').withAttribute('role', 'option').withExactText('More than 4 hours');
-        await t.expect(calendarTable.count).eql(7)
-              .expect(noCareToSelectList.count).eql(7);
+  async childAgeCategory(t, option) {
+    const childAgeCategorySelect = Selector('div').withAttribute('tabindex', '-1');
+    const optionToSelect = Selector('div').withAttribute('role', 'option').withExactText(option)
+    await t.click(this.childAgeCategorySelector)
+      .expect(childAgeCategorySelect.exists).ok()
+      .expect(optionToSelect.exists).ok()
+      .click(optionToSelect.withText(option));
+  }
 
-        const noCareColumns = noCare;
-        for (let i = 0; i < noCareColumns; i++) {
-          log.info(i, ' number of passes for no care.')
-          const noCareToSelect = noCareToSelectList.nth(i);
-          await t.click(noCareToSelect);
-        }
+  async careSchedule(t, fourOrLess, moreThanFour) {
+    const noCareToSelectList = Selector('div').withAttribute('role', 'option').withExactText('No care');
+    for (let i = 0; i < 7; i++) {
+      const noCareToSelect = noCareToSelectList.nth(i);
+      await t.click(noCareToSelect);
+    }
+    for (let i = 0; i < 7; i++) {
+      const noCareToSelect = noCareToSelectList.nth(i);
+      await t.click(noCareToSelect);
+    }
 
-        const addPartTimeColumns = noCare+fourOrLess;
-        for (let i = noCare; i < addPartTimeColumns; i++) {
-          log.info(i, ' number of passes for less than 4');
-          const fourHoursOrLessToSelect = fourHoursOrLessToSelectList.nth(i);
-          await t.click(fourHoursOrLessToSelect);
-        }
+    const partTimeColumns = fourOrLess;
+    for (let i = 0; i < partTimeColumns; i++) {
+      const fourHoursOrLessToSelect = this.fourHoursOrLessToSelectList.nth(i);
+      await t.click(fourHoursOrLessToSelect);
+    }
 
-        const addMoreThanFourColumns = addPartTimeColumns+moreThanFour;
-        for (let i = addPartTimeColumns ; i < addMoreThanFourColumns; i++) {
-          log.info(i, ' number of passes for More than 4.');
-          const moreThanFourToSelect = moreThanFourToSelectList.nth(i);
-          await t.click(moreThanFourToSelect);
-        }
-      }
+    const moreThanFourColumns = partTimeColumns + moreThanFour;
+    for (let i = partTimeColumns; i < moreThanFourColumns; i++) {
+      const moreThanFourToSelect = this.moreThanFourToSelectList.nth(i);
+      await t.click(moreThanFourToSelect);
+    }
+  }
 
-      async parentFeeFrequency (t, option) {
-        const parentFeeFrequencySelect = Selector('#list-138');
-        const optionToSelect = Selector('div').withAttribute('role', 'option').withExactText(option)
-        await t.click(this.parentFeeFrequencySelector)
-              .expect(parentFeeFrequencySelect.exists).ok()
-              .expect(optionToSelect.exists).ok()
-              .click(optionToSelect.withText(option));
-      }
+  async parentFeeFrequency(t, option) {
+    const parentFeeFrequencySelect = Selector('div').withAttribute('tabindex', '-1');
+    const optionToSelect = Selector('div').withAttribute('role', 'option').withExactText(option)
+    await t.click(this.parentFeeFrequencySelector)
+      .expect(parentFeeFrequencySelect.exists).ok()
+      .expect(optionToSelect.exists).ok()
+      .click(optionToSelect.withText(option));
+  }
 
-      async fullTimeParentFee (t, value) {
-        await t.typeText(this.fullTimeParentFeeField, value.toString(), {replace: true})
-        .expect(this.fullTimeParentFeeField.value).eql(value.toString());
-      }
+  async fullTimeParentFee(t, value) {
+    await t.typeText(this.fullTimeParentFeeField, value.toString(), { replace: true })
+      .expect(this.fullTimeParentFeeField.value).eql(value.toString());
+  }
 
-      async partTimeFee (t, value) {
-        await t.typeText(this.partTimeFeeField, value.toString(), {replace: true})
-        .expect(this.partTimeFeeField.value).eql(value.toString());
-      }
+  async partTimeFee(t, value) {
+    await t.typeText(this.partTimeFeeField, value.toString(), { replace: true })
+      .expect(this.partTimeFeeField.value).eql(value.toString());
+  }
 
-      async estiamteSavings (t, childCareSavings, parentFeeReduction) {
-        await t.click(this.estimateYourSavingsBtn)
-        .expect(Selector('div').withExactText(childCareSavings)).ok({ timeout: 1000 })
-        .expect(Selector('div').withExactText(parentFeeReduction)).ok({ timeout: 1000 });
-      }
+  async estimateSavings(t, childCareSavings, parentFeeReduction) {
+    log.info('Expected values: ', childCareSavings, parentFeeReduction);
+    await t.click(this.estimateYourSavingsBtn)
+      .expect(Selector('div').withText(childCareSavings).exists).ok()
+      .expect(Selector('div').withText(parentFeeReduction).exists).ok();
+  }
+
 }
 
 export default PageEstimator;
--- test/src/testCases/childcareEstimator.js
+++ test/src/testCases/childcareEstimator.js
@@ -1,103 +1,79 @@
 import log from "npmlog";
-const fs = require('fs');
-const path = require('path');
-
-const config = require('../utils/configLoader');
-const validation = require('../utils/validation');
 import PageEstimator from '../pageObjects/PageEstimator';
 
+const fs = require('fs');
+const path = require('path');
+const config = require('../utils/configLoader');
+const validation = require('../utils/validation');
 const pageEstimator = new PageEstimator();
+const data = loadFile('estimatorData.csv');
 
-fixture `Child Care Estimator Test`
+fixture`Child Care Estimator Test`
   .page(`${config.get('url')}/ccfri-estimator`).after(async t => {
-});
-
-
-  test ('Childcare Estimator Test (Daily)', async t => {
-    const lines = await loadFile('estimatorData.csv'); 
-    log.info(lines.length, ' is the the number of lines.');
-    
-    for (let i = 0 ; i < lines.length; i ++) {
-      let values = lines[i].split(',');
-
-      for (let fd = 0; fd <= 7; fd ++) {
-        let childAgeCategory = String(values[1]).trim();
-        let isPreschool = childAgeCategory == 'Preschool';
-        let columnOffset = 6;
-        if (isPreschool && fd > 0) {
-          continue;
-        }
-        for (let hd = 0; (hd + fd) <= 7; hd ++) {
-          let correctCell = values[columnOffset ++];
-          if (fd === 0 && hd === 0) { // in the spreadsheet there is no fd=0, hd=-, so test the FT rate.
-            continue;
-          }
-          let correctValues = correctCell.split(' ');
-
-          log.info(correctValues[0], ' is the expected reduction.')
-          log.info(correctValues[1], ' is the expected parent fee.')
-
-          //await pageEstimator.parentSelect(t);
-          log.info('Parent Selected.');
-
-          let totChildren = 1;
-          //await pageEstimator.addChildren(t, totChildren);
-          log.info(totChildren, ' children added.');
-
-          let typeOfCare = String(values[0]).trim();;
-          //await pageEstimator.typeOfCare(t, typeOfCare);
-          log.info(typeOfCare, ' selected');
-          
-          //await pageEstimator.childAgeCategory(childAgeCategory);
-          log.info(childAgeCategory, ' selected');
-
-          let noCare = 0 ;
-          let fourOrLess = hd;
-          let moreThanFour = fd;
-          //await pageEstimator.careSchedule(t, noCare, fourOrLess, moreThanFour);
-          log.info(noCare, ' is the number of No care.')
-          log.info(fourOrLess, ' is the number of Four hours or less.')
-          log.info(moreThanFour, ' is the number of Four hours or more.')
-
-          let feeFrequency = 'Daily';
-          //await pageEstimator.parentFeeFrequency(t, feeFrequency);
-          log.info(feeFrequency, ' selected.');
-
-          let approvedFee = values[3];
-          //await pageEstimator.fullTimeParentFee(t, approvedFee);
-          log.info(approvedFee, ' dollars.')
-
-          let actualFee = values[5];
-          //await pageEstimator.partTimeFee(t, actualFee);
-          log.info(actualFee, ' dollars.')
-
-          // * number of days of care * 4 (number of weeks in a month)
-          let dailychildCareSavings = correctValues[0]/20;
-          let monthlyChildCareSavingsHD = dailychildCareSavings * hd * 4;
-          let monthlyChildCareSavingsFD = dailychildCareSavings * fd * 4;
-          let totalSavings = monthlyChildCareSavingsFD + monthlyChildCareSavingsHD;
-          let childCareSavings = '$'+dailychildCareSavings+'/day ($'+ totalSavings +'/month)';
-
-          let estimatedParentFee = correctValues[1]/20;
-          let monthlysEtimatedParentFeeHD = estimatedParentFee * hd * 4;
-          let monthlyEtimatedParentFeeFD = estimatedParentFee * fd * 4;
-          let totalEstimatedFees = monthlysEtimatedParentFeeHD + monthlyEtimatedParentFeeFD;
-          let parentFeeReduction = '$'+estimatedParentFee+'/day ($'+totalEstimatedFees+'/month)';
-          //await pageEstimator.estiamteSavings(t, childCareSavings, parentFeeReduction);
-          log.info(childCareSavings, ' is your Estimated child care savings.');
-          log.info(parentFeeReduction, ' is your Estimated parent fee after reduction.');
-
-        
-        }
-      }
-    }
   });
 
-  async function loadFile(fileName) {
-    let data = fs.readFileSync(path.join(__dirname, '..', 'data', `${fileName}`), { encoding: "utf8" });
-    let lines = data.split('\n');
-    lines.shift(); // remove the first 2 lines which are headers
-    lines.shift();
+data.forEach(line => {
+  let values = line.split(',');
+  let columnOffset = 6;
+  test('Childcare Estimator Test (Monthly)', async t => {
+    for (let fd = 0; fd <= 7; fd++) {
+      let childAgeCareCategory = String(values[1]).trim();
+      let isPreschool = childAgeCareCategory === 'Preschool' ? true : false;
+      if (isPreschool && fd > 0) {
+        continue;
+      }
 
-    return lines;
-  }
+      for (let hd = 0; (hd + fd) <= 7; hd++) {
+        log.info(`full time days = ${fd} and part time days = ${hd}`);
+        let correctCell = values[columnOffset++];
+        let correctValues = correctCell.split(' ');
+        if (fd === 0 && hd === 0) { // in the spreadsheet there is no fd=0, hd=-, so test the FT rate.
+          continue;
+        }
+
+        await pageEstimator.parentSelect(t);
+
+        let totChildren = 1;
+        await pageEstimator.addChildren(t, totChildren);
+
+        let typeOfCare = String(values[0]).trim();;
+        await pageEstimator.typeOfCare(t, typeOfCare);
+
+        await pageEstimator.childAgeCategory(t, childAgeCareCategory);
+
+        await pageEstimator.careSchedule(t, hd, fd);
+
+        await pageEstimator.parentFeeFrequency(t, 'Monthly');
+
+        let approvedFee = values[2];
+        await pageEstimator.fullTimeParentFee(t, approvedFee);
+
+        let actualFee = values[4];
+        await pageEstimator.partTimeFee(t, actualFee);
+
+        // * number of days of care * 4 (number of weeks in a month)
+        let dailychildCareSavings = correctValues[0];
+        // let monthlyChildCareSavingsHD = dailychildCareSavings * hd * 4;
+        // let monthlyChildCareSavingsFD = dailychildCareSavings * fd * 4;
+        // let totalSavings = monthlyChildCareSavingsFD + monthlyChildCareSavingsHD;
+        // let childCareSavings = '$' + dailychildCareSavings + '/day ($' + totalSavings + '/month)';
+
+        let estimatedParentFee = correctValues[1];
+        // let monthlysEtimatedParentFeeHD = estimatedParentFee * hd * 4;
+        // let monthlyEtimatedParentFeeFD = estimatedParentFee * fd * 4;
+        // let totalEstimatedFees = monthlysEtimatedParentFeeHD + monthlyEtimatedParentFeeFD;
+        // let parentFeeReduction = '$' + estimatedParentFee + '/day ($' + totalEstimatedFees + '/month)';
+        await pageEstimator.estimateSavings(t, dailychildCareSavings, estimatedParentFee);
+      }
+    }
+  })
+});
+
+function loadFile(fileName) {
+  let file = fs.readFileSync(path.join(__dirname, '..', 'data', `${fileName}`), { encoding: "utf8" });
+  let lines = file.split('\n');
+  lines.shift(); // remove the first 2 lines which are headers
+  lines.shift();
+  log.info(lines.length, ' is the the number of lines.');
+  return lines;
+}
