7a9347226ab63935b3312232a1f4ddbf9a7223dd
roblo-cgi <rob.lo@cgi.com>
2023-06-12 13:40:58 -0700

    updates to TTL for redis

--- backend/src/util/redis/cache-helper.js
+++ backend/src/util/redis/cache-helper.js
@@ -39,7 +39,7 @@ const cacheHelper = {
     const redisClient = Redis.getRedisClient();
     if (redisClient) {
       const facilityTTL = config.get('redis:facilityTTL');
-      console.verbose('facility TTL: ', facilityTTL);
+      log.verbose('facility TTL: ', facilityTTL);
       if (facilityTTL > 0) {
         await redisClient.expire(FACILITY_D, facilityTTL, 'NX');
       }
