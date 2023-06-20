8615dc6a5db4ffd54dc3b5c5ecda219681016af1
Greg Pascucci <greg.pascucci@gcgi.com>
2023-06-20 15:59:30 -0700

    formatting fixes

--- frontend/src/components/summary/group/CCFRISummary.vue
+++ frontend/src/components/summary/group/CCFRISummary.vue
@@ -2,124 +2,106 @@
   <v-row no-gutters class="d-flex flex-column">
     <v-form ref="ccfriSummaryForm" v-model="isValidForm">
       <v-expansion-panel-header>
-        <h4 style="color:#003466;">Child Care Fee Reduction Initiative (CCFRI)
-          <v-icon v-if="isValidForm" color="green" large>mdi-check-circle-outline</v-icon>
-          <v-icon v-if="!isValidForm" color="#ff5252" large>mdi-alert-circle-outline</v-icon>
-          <span v-if="!isValidForm" style="color:#ff5252;">Your form is missing required information. Click here to view.
-          </span>
-        </h4>
-      </v-expansion-panel-header>
-      <v-expansion-panel-content eager>
-        <v-row v-if="!ccfri">
-          <v-col cols="12">
-            <span cols="6" class="summary-label">CCFRI Opt-In/Opt-Out Status:</span>
-            <v-text-field cols="6" placeholder="Required" class="summary-value" dense flat solo hide-details readonly
-              :rules="rules.required"></v-text-field>
-          </v-col>
-        </v-row>
-        <v-row v-else-if="ccfri.ccfriOptInStatus != 0" no-gutters class="d-flex flex-column">
-          <div v-for=" (ccType, index) in ccfriChildCareTypes" :key="index">
-            <v-row class="d-flex justify-start">
-              <v-col cols="12" lg="12" class="pb-1 pt-1 ml-2">
-                <v-row no-gutters class="d-flex justify-start">
-                  <v-col cols="12" class="d-flex justify-start">
-                    <span class="summary-label pt-3" v-if="!!ccType.programYear && !!ccType.childCareCategory">Parent Fees
-                      {{ ccType.programYear }}: {{ ccType.childCareCategory }}: </span>
-                    <v-text-field placeholder="Required" v-else
-                      :value="generateProgYearText(ccType.programYear, ccType.childCareCategory)" class="summary-label"
-                      dense flat solo hide-details readonly :rules="rules.required" prefix="Parent Fees"></v-text-field>
-                    <v-text-field placeholder="Required" :value="ccType.feeFrequency" class="summary-value" dense flat
-                      solo hide-details readonly :rules="rules.required"></v-text-field>
-                  </v-col>
-                  <v-col cols="12" class="d-flex justify-start">
-                    <span v-if="!!ccType.feeFrequency" class="summary-label">Your <b>highest {{
-                      ccType.feeFrequency.toLowerCase() }} parent fee before CCFRI is applied</b> in every month below.
-                    </span>
-                    <v-text-field placeholder="Required" v-else :value="ccType.feeFrequency" class="summary-value" dense
-                      flat solo hide-details readonly :rules="rules.required"></v-text-field>
-                  </v-col>
-                  <v-col cols="2" class="d-flex justify-start">
-                    <span class="summary-label pt-3">Apr:</span>
-                    <v-text-field placeholder="Required" :value="ccType.approvedFeeApr" class="summary-value" dense flat
-                      solo hide-details readonly :rules="rules.required" prefix="$"></v-text-field>
-                  </v-col>
-                  <v-col cols="2" class="d-flex justify-start">
-                    <span class="summary-label pt-3">May:</span>
-                    <v-text-field placeholder="Required" :value="ccType.approvedFeeMay" class="summary-value" dense flat
-                      solo hide-details readonly :rules="rules.required" prefix="$"></v-text-field>
-                  </v-col>
-                  <v-col cols="2" class="d-flex justify-start">
-                    <span class="summary-label pt-3">Jun:</span>
-                    <v-text-field placeholder="Required" :value="ccType.approvedFeeJun" class="summary-value" dense flat
-                      solo hide-details readonly :rules="rules.required" prefix="$"></v-text-field>
-                  </v-col>
-                  <v-col cols="2" class="d-flex justify-start">
-                    <span class="summary-label pt-3">Jul:</span>
-                    <v-text-field placeholder="Required" :value="ccType.approvedFeeJul" class="summary-value" dense flat
-                      solo hide-details readonly :rules="rules.required" prefix="$"></v-text-field>
-                  </v-col>
-                  <v-col cols="2" class="d-flex justify-start">
-                    <span class="summary-label pt-3">Aug:</span>
-                    <v-text-field placeholder="Required" :value="ccType.approvedFeeAug" class="summary-value" dense flat
-                      solo hide-details readonly :rules="rules.required" prefix="$"></v-text-field>
-                  </v-col>
-                  <v-col cols="2" class="d-flex justify-start">
-                    <span class="summary-label pt-3">Sep:</span>
-                    <v-text-field placeholder="Required" :value="ccType.approvedFeeSep" class="summary-value" dense flat
-                      solo hide-details readonly :rules="rules.required" prefix="$"></v-text-field>
-                  </v-col>
-                  <v-col cols="2" class="d-flex justify-start">
-                    <span class="summary-label pt-3">Oct:</span>
-                    <v-text-field placeholder="Required" :value="ccType.approvedFeeOct" class="summary-value" dense flat
-                      solo hide-details readonly :rules="rules.required" prefix="$"></v-text-field>
-                  </v-col>
-                  <v-col cols="2" class="d-flex justify-start">
-                    <span class="summary-label pt-3">Nov:</span>
-                    <v-text-field placeholder="Required" :value="ccType.approvedFeeNov" class="summary-value" dense flat
-                      solo hide-details readonly :rules="rules.required" prefix="$"></v-text-field>
-                  </v-col>
-                  <v-col cols="2" class="d-flex justify-start">
-                    <span class="summary-label pt-3">Dec:</span>
-                    <v-text-field placeholder="Required" :value="ccType.approvedFeeDec" class="summary-value" dense flat
-                      solo hide-details readonly :rules="rules.required" prefix="$"></v-text-field>
-                  </v-col>
-                  <v-col cols="2" class="d-flex justify-start">
-                    <span class="summary-label pt-3">Jan:</span>
-                    <v-text-field placeholder="Required" :value="ccType.approvedFeeJan" class="summary-value" dense flat
-                      solo hide-details readonly :rules="rules.required" prefix="$"></v-text-field>
-                  </v-col>
-                  <v-col cols="2" class="d-flex justify-start">
-                    <span class="summary-label pt-3">Feb:</span>
-                    <v-text-field placeholder="Required" :value="ccType.approvedFeeFeb" class="summary-value" dense flat
-                      solo hide-details readonly :rules="rules.required" prefix="$"></v-text-field>
-                  </v-col>
-                  <v-col cols="2" class="d-flex justify-start">
-                    <span class="summary-label pt-3">Mar:</span>
-                    <v-text-field placeholder="Required" :value="ccType.approvedFeeMar" class="summary-value" dense flat
-                      solo hide-details readonly :rules="rules.required" prefix="$"></v-text-field>
-                  </v-col>
-                </v-row>
-              </v-col>
-            </v-row>
-          </div>
-          <v-row class="d-flex justify-start ml-0">
-            <v-col cols="12" lg="612" class="pb-2 pt-2">
-              <v-row no-gutters class="d-flex justify-start">
-                <v-col cols="12" class="d-flex justify-start">
-                  <span class="summary-label">Do you charge parent fees at this facility to any closures on business days
-                    (other than statutory holidays)</span>
-                  <v-text-field placeholder="Required" :value="this.getClosureFees(this.ccfri.hasClosureFees)"
-                    class="summary-value" dense flat solo hide-details readonly :rules="rules.required"></v-text-field>
-                </v-col>
-              </v-row>
+      <h4 style="color:#003466;">Child Care Fee Reduction Initiative (CCFRI)
+        <v-icon v-if="isValidForm" color="green" large>mdi-check-circle-outline</v-icon>
+        <v-icon v-if="!isValidForm" color="#ff5252" large>mdi-alert-circle-outline</v-icon>
+        <span v-if="!isValidForm" style="color:#ff5252;">Your form is missing required information. Click here to view. </span>
+      </h4>
+    </v-expansion-panel-header>
+    <v-expansion-panel-content eager  >
+    <v-row v-if="!ccfri" >
+      <v-col cols="12" >
+          <span  cols="6" class="summary-label">CCFRI Opt-In/Opt-Out Status:</span>
+          <v-text-field  cols="6" placeholder="Required" class="summary-value" dense flat solo hide-details readonly :rules="rules.required" ></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row v-else-if="ccfri.ccfriOptInStatus != 0" no-gutters class="d-flex flex-column">
+      <div v-for=" (ccType, index) in ccfriChildCareTypes" :key="index">
+      <v-row class="d-flex justify-start">
+        <v-col cols="6" lg="6" class="pb-1 pt-1 ml-2">
+          <v-row no-gutters class="d-flex justify-start">
+            <v-col cols="12" class="d-flex justify-start">
+              <span class="summary-label pt-3" v-if="!!ccType.programYear && !!ccType.childCareCategory">Parent Fees {{ ccType.programYear }}: {{ ccType.childCareCategory }}: </span>
+              <v-text-field placeholder="Required"  v-else :value="generateProgYearText(ccType.programYear,ccType.childCareCategory)" class="summary-label" dense flat solo hide-details readonly :rules="rules.required" prefix="Parent Fees" ></v-text-field>
+              <v-text-field placeholder="Required"  :value="ccType.feeFrequency" class="summary-value" dense flat solo hide-details readonly :rules="rules.required" ></v-text-field>
+            </v-col>
+            <v-col cols="12" class="d-flex justify-start">
+              <span v-if="!!ccType.feeFrequency" class="summary-label">Your <b>highest {{ ccType.feeFrequency.toLowerCase() }} parent fee before CCFRI is applied</b> in every month below. </span>
+              <v-text-field placeholder="Required"  v-else :value="ccType.feeFrequency" class="summary-value" dense flat solo hide-details readonly :rules="rules.required"></v-text-field>
+            </v-col>
+            <v-col  cols="2" class="d-flex justify-start">
+              <span class="summary-label pt-3">Apr:</span>
+              <v-text-field placeholder="Required"  :value="ccType.approvedFeeApr" class="summary-value" dense flat solo hide-details readonly :rules="rules.required" prefix="$"></v-text-field>
+            </v-col>
+            <v-col cols="2" class="d-flex justify-start">
+              <span class="summary-label pt-3">May:</span>
+              <v-text-field placeholder="Required"  :value="ccType.approvedFeeMay" class="summary-value" dense flat solo hide-details readonly :rules="rules.required" prefix="$"></v-text-field>
+            </v-col>
+            <v-col  cols="2" class="d-flex justify-start">
+              <span class="summary-label pt-3">Jun:</span>
+              <v-text-field placeholder="Required"  :value="ccType.approvedFeeJun" class="summary-value" dense flat solo hide-details readonly :rules="rules.required" prefix="$"></v-text-field>
+            </v-col>
+            <v-col  cols="2" class="d-flex justify-start">
+              <span class="summary-label pt-3">Jul:</span>
+              <v-text-field placeholder="Required"  :value="ccType.approvedFeeJul" class="summary-value" dense flat solo hide-details readonly :rules="rules.required" prefix="$"></v-text-field>
+            </v-col>
+            <v-col  cols="2" class="d-flex justify-start">
+              <span class="summary-label pt-3">Aug:</span>
+              <v-text-field placeholder="Required"  :value="ccType.approvedFeeAug" class="summary-value" dense flat solo hide-details readonly :rules="rules.required" prefix="$"></v-text-field>
+            </v-col>
+            <v-col  cols="2" class="d-flex justify-start">
+              <span class="summary-label pt-3">Sep:</span>
+              <v-text-field placeholder="Required"  :value="ccType.approvedFeeSep" class="summary-value" dense flat solo hide-details readonly :rules="rules.required" prefix="$"></v-text-field>
+            </v-col>
+            <v-col  cols="2" class="d-flex justify-start">
+              <span class="summary-label pt-3">Oct:</span>
+              <v-text-field placeholder="Required"  :value="ccType.approvedFeeOct" class="summary-value" dense flat solo hide-details readonly :rules="rules.required" prefix="$"></v-text-field>
+            </v-col>
+            <v-col cols="2" class="d-flex justify-start">
+              <span class="summary-label pt-3">Nov:</span>
+              <v-text-field placeholder="Required"  :value="ccType.approvedFeeNov" class="summary-value" dense flat solo hide-details readonly :rules="rules.required" prefix="$"></v-text-field>
+            </v-col>
+            <v-col  cols="2" class="d-flex justify-start">
+              <span class="summary-label pt-3">Dec:</span>
+              <v-text-field placeholder="Required"  :value="ccType.approvedFeeDec" class="summary-value" dense flat solo hide-details readonly :rules="rules.required" prefix="$"></v-text-field>
+            </v-col>
+            <v-col  cols="2" class="d-flex justify-start">
+              <span class="summary-label pt-3">Jan:</span>
+              <v-text-field placeholder="Required"  :value="ccType.approvedFeeJan" class="summary-value" dense flat solo hide-details readonly :rules="rules.required" prefix="$"></v-text-field>
+            </v-col>
+            <v-col  cols="2" class="d-flex justify-start">
+              <span class="summary-label pt-3">Feb:</span>
+              <v-text-field placeholder="Required"  :value="ccType.approvedFeeFeb" class="summary-value" dense flat solo hide-details readonly :rules="rules.required" prefix="$"></v-text-field>
+            </v-col>
+            <v-col  cols="2" class="d-flex justify-start">
+              <span class="summary-label pt-3">Mar:</span>
+              <v-text-field placeholder="Required"  :value="ccType.approvedFeeMar" class="summary-value" dense flat solo hide-details readonly :rules="rules.required" prefix="$"></v-text-field>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+      </div>
+      <v-row class="d-flex justify-start ml-0">
+        <v-col cols="12" lg="12" class="pb-2 pt-2">
+          <v-row no-gutters class="d-flex justify-start">
+            <v-col cols="12" class="d-flex justify-start">
+              <span class="summary-label">Do you charge parent fees at this facility to any closures on business days (other than statutory holidays)</span>
+              <v-text-field placeholder="Required"  :value="this.getClosureFees(this.ccfri.hasClosureFees)" class="summary-value" dense flat solo hide-details readonly :rules="rules.required" ></v-text-field>
+            </v-col>
+          </v-row>
 
+            
               <v-row v-if="this.ccfri.hasClosureFees == 100000000">
+                <v-col class="col-md-3 col-12"><span class="summary-label">Closure Start Date</span></v-col>
+                <v-col class="col-md-3 col-12"><span class="summary-label">End Date</span></v-col>
+                <v-col class="col-md-3 col-12"><span class="summary-label">Reason</span></v-col>
+                <v-col class="col-md-3 col-12"><span class="summary-label">Did parents pay for this closure?</span></v-col>
                 <v-row v-for="(obj, index) in   this.ccfri.dates  " :key="index">
                   <v-col class="col-md-3 col-12">
                     <v-menu v-model="obj.calendarMenu1" :close-on-content-click="false" :nudge-right="40"
                       transition="scale-transition" offset-y min-width="auto">
                       <template v-slot:activator="{ on, attrs }">
-                        <v-text-field placeholder="Required" outlined :rules="rules" v-model="obj.formattedStartDate"
+                        <v-text-field placeholder="Required" dense flat solo hide-details :rules="rules.required" v-model="obj.formattedStartDate"
                           label="Select Start Date (YYYY-MM-DD)" readonly v-bind="attrs" v-on="on">
                         </v-text-field>
                       </template>
@@ -130,35 +112,25 @@
                     <v-menu v-model="obj.calendarMenu2" :close-on-content-click="false" :nudge-right="40"
                       transition="scale-transition" offset-y min-width="auto">
                       <template v-slot:activator="{ on, attrs }">
-                        <v-text-field placeholder="Required" outlined required readonly v-model="obj.formattedEndDate"
-                          label="Select End Date (YYYY-MM-DD)" :rules="rules" v-bind="attrs" v-on="on">
+                        <v-text-field placeholder="Required" outlidense flat solo hide-detailsned required readonly v-model="obj.formattedEndDate"
+                          label="Select End Date (YYYY-MM-DD)" :rules="rules.required" v-bind="attrs" v-on="on">
                         </v-text-field>
                       </template>
                     </v-menu>
                   </v-col>
 
                   <v-col class="col-md-3 col-12 ">
-                    <v-text-field placeholder="Required" readonly v-model="obj.closureReason" label="Closure Reason"
-                      outlined clearable :rules="rules"></v-text-field>
+                    <v-text-field placeholder="Required" readonly v-model="obj.closureReason"
+                    dense flat solo hide-details :rules="rules.required"></v-text-field>
                   </v-col>
 
                   <v-col class="col-md-3 col-12">
-                    <v-text-field placeholder="Required" readonly row label="Did parents pay for this closure?"
-                      :value="obj.feesPaidWhileClosed === 1 ? 'Yes' : 'No'" :rules="dateRules">{{ obj.feesPaidWhileClosed
-                        === 1 ?
-                        'Yes' : 'No' }}</v-text-field>
+                    <v-text-field placeholder="Required" readonly dense flat solo hide-details
+                      :value="obj.feesPaidWhileClosed === 1 ? 'Yes' : 'No'" :rules="rules.required"></v-text-field>
                   </v-col>
 
                 </v-row> <!-- end v for-->
               </v-row> <!-- end v if -->
-            </v-col>
-          </v-row>
-
-          <v-row no-gutters class="d-flex justify-start">
-            <v-col cols="12" class="d-flex justify-start">
-              <span class="summary-label">CCFRI Opt-In/Opt-Out Status:</span>
-              <v-text-field placeholder="Required" :value="this.getOptInOptOut(this.ccfri.ccfriOptInStatus)"
-                class="summary-value" dense flat solo hide-details readonly :rules="rules.required"></v-text-field>
             </v-col>
           </v-row>
           <v-col cols="12" class="pb-2 pt-2">
