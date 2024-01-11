f273c61aebeeaceeecd4739ab6a137d74ca73c1d
Jen Beckett <115109120+jenbeckett@users.noreply.github.com>
2024-01-11 10:21:03 -0800

    Wording change addNewFees

--- frontend/src/components/ccfriApplication/group/AddNewFees.vue
+++ frontend/src/components/ccfriApplication/group/AddNewFees.vue
@@ -13,7 +13,7 @@
     <FacilityHeader :facilityAccountNumber="currentFacility?.facilityAccountNumber" :facilityName="currentFacility.facilityName" :licenseNumber="currentFacility?.licenseNumber"></FacilityHeader>
     <br><br>
     <p>
-      Enter the fees you charged a new parent for full-time care at this facility for the months below. <br><br>
+      Enter the fees you would charge a new parent for full-time care at this facility for the months below. <br><br>
       If you have more than one fee for the same category, <strong> enter the highest fee. </strong><br><br>
       <strong>Enter the fee before CCFRI is applied. </strong>  <br><br>
       <span v-if="languageYearLabel != programYearTypes.HISTORICAL"> <strong>New for 2024/25:</strong>  CCFRI regions align with the BCSSA's grouping of school districts into 6 regional chapters. Use the <a href="https://bcmcf.ca1.qualtrics.com/jfe/form/SV_eVcEWJC8HTelRCS"  target="_blank">BCSSA region lookup</a> to find your region.</span> <br><br>
