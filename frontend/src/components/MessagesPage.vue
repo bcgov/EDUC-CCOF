f02d80d420ab25041a4b095842da0325b7f3b921
Le <vietdung.le@cgi.com>
2024-12-06 10:39:20 -0800

    ccfri-4049 - fix Vue3 bugs for Message UI

--- frontend/src/components/MessagesPage.vue
+++ frontend/src/components/MessagesPage.vue
@@ -1,72 +1,72 @@
 <template>
-  <v-container id="messages" fluid class="pa-0 ma-0" height="100%">
-    <v-row v-if="loading" class="ma-0">
-      <Spinner style="width: 100%"></Spinner>
-    </v-row>
-    <v-row fluid class="mx-4" v-else>
-      <v-row>
-        <v-col id="messages-summary" fluid class="pa-0" :cols="4">
-          <v-card tile style="border-right: 1px solid lightgrey" :height="fitScreenHeight()" class="pa-0 elevation-0">
-            <v-data-table
-              :headers="columns"
-              :items="messages"
-              :mobile-breakpoint="960"
-              :height="fitScreenHeight()"
-              fixed-header
-              :item-value="messageId"
-              :items-per-page="-1"
-              hide-default-footer
-              @click:row="rowClickHandler"
-            >
-              <template #no-data>
-                <v-alert :value="true" type="info"> No Data </v-alert>
-              </template>
-              <template #item.isRead="{ item }">
-                {{ item.isRead ? 'Read' : 'Unread' }}
-              </template>
-              <template #item.programYearValue="{ item }">
-                {{ formatFiscalYearName(item.programYearValue) }}
-              </template>
-            </v-data-table>
-          </v-card>
-        </v-col>
-        <v-col v-if="message.sender" id="messages-content" fluid class="pa-0" :cols="8">
-          <v-card class="pa-4 overflow-auto elevation-0" fluid tile :height="fitScreenHeight()">
-            <v-card-title class="pa-0">
-              <v-row>
-                <v-col :cols="8">
-                  {{ message.sender }}
-                </v-col>
-                <v-col align="right" :cols="4">
-                  {{ message.dateReceived }}
-                </v-col>
-              </v-row>
-              <v-row>
-                <v-col>
-                  <strong>{{ message.subject }}</strong>
-                </v-col>
-              </v-row>
-            </v-card-title>
-            <v-divider />
-            <v-card-text v-html="message.messageContent"></v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
+  <v-container id="messages" fluid class="pa-0">
+    <Spinner v-if="loading" />
+    <v-row v-else no-gutters>
+      <v-col id="messages-summary" cols="12" md="5" :class="borderClass">
+        <v-card tile elevation="0">
+          <v-data-table
+            :headers="columns"
+            :items="messages"
+            :mobile="null"
+            mobile-breakpoint="lg"
+            :height="screenHeight"
+            fixed-header
+            hover
+            item-key="messageId"
+            item-value="messageId"
+            :items-per-page="-1"
+            hide-default-footer
+            :row-props="getRowProps"
+            @click:row="rowClickHandler"
+          >
+            <template #no-data>
+              <v-alert :value="true" type="info"> No Data </v-alert>
+            </template>
+            <template #item.isRead="{ item }">
+              {{ item.isRead ? 'Read' : 'Unread' }}
+            </template>
+            <template #item.programYearValue="{ item }">
+              {{ formatFiscalYearName(item.programYearValue) }}
+            </template>
+          </v-data-table>
+        </v-card>
+      </v-col>
+      <v-col v-if="message.sender" id="messages-content" cols="12" md="7" class="mt-6 mt-md-0">
+        <v-card class="px-6 overflow-auto elevation-0" :height="screenHeight">
+          <div>
+            <v-row>
+              <v-col cols="12" sm="8">
+                {{ message.sender }}
+              </v-col>
+              <v-col align="right" cols="12" sm="4">
+                {{ message.dateReceived }}
+              </v-col>
+            </v-row>
+            <div>
+              <strong>{{ message.subject }}</strong>
+            </div>
+          </div>
+          <v-divider class="my-4" />
+          <div v-html="message.messageContent"></div>
+        </v-card>
+      </v-col>
     </v-row>
     <v-divider />
     <v-row justify="center" class="pa-3">
-      <v-btn id="back-button" color="info" variant="outlined" :size="buttonSize" @click="goToHomePage()"> Back </v-btn>
+      <AppButton id="back-button" :primary="false" size="x-large" @click="goToHomePage()">Back</AppButton>
     </v-row>
   </v-container>
 </template>
 
 <script>
-import { useDisplay } from 'vuetify';
+import { mapState, mapActions } from 'pinia';
 
 import { useAuthStore } from '@/store/auth.js';
 import { useMessageStore } from '@/store/message.js';
-import { mapState, mapActions } from 'pinia';
+
 import Spinner from '@/components/common/Spinner.vue';
+import AppButton from '@/components/guiComponents/AppButton.vue';
+
 import { PATHS } from '@/utils/constants.js';
 import { formatFiscalYearName } from '@/utils/format';
 
@@ -74,10 +74,10 @@ export default {
   name: 'MessagesPage',
   components: {
     Spinner,
+    AppButton,
   },
   data() {
     return {
-      selectedId: -1,
       columns: [
         { title: 'Read/Unread', align: 'start', key: 'isRead' },
         { title: 'Subject', key: 'subject' },
@@ -91,8 +91,8 @@ export default {
         dateReceived: '',
         messageContent: '',
       },
-      displayName: '',
       loading: false,
+      selectedMessageId: undefined,
     };
   },
   computed: {
@@ -101,20 +101,28 @@ export default {
     messages() {
       return this.allMessages || [];
     },
-    buttonSize() {
-      const sizeMap = {
-        xs: 'large',
-        sm: 'large',
-        md: 'large',
-        lg: 'x-large',
-        xl: 'x-large',
-      };
-      return sizeMap[this.displayName] || 'medium';
+    borderClass() {
+      return this.$vuetify.display.mdAndUp ? 'border-right' : 'border-bottom';
+    },
+    screenHeight() {
+      switch (this.$vuetify.display.name) {
+        case 'xs':
+          return '67vh';
+        case 'sm':
+          return '82vh';
+        case 'md':
+          return '75vh';
+        case 'lg':
+          return '70vh';
+        case 'xl':
+          return '78vh';
+        default:
+          return '70vh';
+      }
     },
   },
-  mounted() {
-    this.displayName = useDisplay().name.value;
-    this.loadMessages();
+  async created() {
+    await this.loadMessages();
   },
   methods: {
     ...mapActions(useMessageStore, ['updateMessage', 'getAllMessages']),
@@ -133,7 +141,8 @@ export default {
       }
     },
 
-    rowClickHandler(event, { item }) {
+    rowClickHandler(_, { item }) {
+      this.selectedMessageId = item?.messageId;
       this.message = {
         subject: item.subject || 'No subject',
         dateReceived: item.dateReceived || 'Unknown date',
@@ -148,52 +157,25 @@ export default {
         console.error('No messageId found in item');
       }
     },
-    getMessageStyle(message) {
-      return message.isRead ? 'read' : 'unread';
+    getRowProps(item) {
+      const message = item?.item;
+      let rowClass = '';
+      if (!message?.isRead) rowClass += 'unread-message ';
+      if (this.selectedMessageId === message?.messageId) rowClass += 'highlighted-row';
+      return { class: rowClass };
     },
     goToHomePage() {
       this.$router.push(PATHS.ROOT.HOME);
     },
-    fitScreenHeight() {
-      switch (this.displayName) {
-        case 'xs':
-          return '67vh';
-        case 'sm':
-          return '82vh';
-        case 'md':
-          return '75vh';
-        case 'lg':
-          return '70vh';
-        case 'xl':
-          return '78vh';
-        default:
-          return '70vh';
-      }
-    },
   },
 };
 </script>
-
 <style scoped>
-:deep(html) {
-  overflow-y: auto;
-}
-:deep(.read) {
-  font-weight: normal;
-}
-:deep(.unread) {
+:deep(.unread-message) {
   font-weight: bold;
 }
-:deep(tr:hover) {
-  cursor: pointer;
-}
-:deep(tr.v-data-table__selected) {
-  background: #c2e0fa !important;
-}
-:deep(.v-data-table-header th) {
-  white-space: nowrap;
-}
-:deep(.v-data-table__wrapper) {
-  margin-bottom: 0px;
+
+:deep(.highlighted-row) {
+  background: #c2e0fa;
 }
 </style>
