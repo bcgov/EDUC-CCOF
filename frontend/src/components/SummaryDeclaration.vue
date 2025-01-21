26392703e1c841ad3057b83414f257b8933d45ca
Panika-cgi <panika.saxena@cgi.com>
2025-01-20 16:16:20 -0800

    a b c indentation

--- frontend/src/components/SummaryDeclaration.vue
+++ frontend/src/components/SummaryDeclaration.vue
@@ -359,7 +359,7 @@
                   and conditions. I further confirm that by clicking “I agree” below, I represent and warrant that:
                 </p>
 
-                <ol type="a" style="padding-top: 10px">
+                <ol type="a" style="padding-top: 10px; padding-left: 20px">
                   <li>
                     I am the authorized representative and signing authority of the Provider as named in the Funding
                     Agreement (the Provider);
