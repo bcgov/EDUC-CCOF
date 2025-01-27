ec338de9580c8aae04cbedd9fca45c35ae981624
Jen Beckett <115109120+jenbeckett@users.noreply.github.com>
2025-01-27 09:54:00 -0800

    PROD hotfix (#621)
    * PROD hotfix CCFRI 4447 update Declarations
    
    * updated comments

--- frontend/src/components/requestChanges/SummaryDeclarationChangeRequest.vue
+++ frontend/src/components/requestChanges/SummaryDeclarationChangeRequest.vue
@@ -173,12 +173,14 @@
           </v-row>
           <v-row v-if="!isProcessing">
             <v-col class="pb-0 px-8">
-              <div v-show="!isRenewal && !organizationAccountNumber">
+              <!-- Currently this component is only used for Report Other Changes or MTFI change Requests. Add New Facility uses PCF Summary Dec-->
+              <!-- Declaration A   should always been shown for Report Other Changes -->
+              <div v-if="isDeclarationADisplayed">
                 <p>
                   I hereby confirm that the information I have provided in this application is complete and accurate. I
                   certify that I have read and understand the following requirements:
                 </p>
-                <ul style="padding-top: 10px">
+                <ul class="ml-5 pt-5">
                   <li>Each facility must be licensed under the Community Care and Assisted Living Act;</li>
                   <li>
                     Each facility must be in compliance with the Community Care and Assisted Living Act and Child Care
@@ -207,19 +209,8 @@
                 </p>
               </div>
 
-              <!-- Declaration A -->
-              <div v-show="!isDeclarationBDisplayed">
-                <p>
-                  I do hereby certify that I am the <strong>authorized signing authority</strong> and that all of the
-                  information provided is true and complete to the best of my knowledge and belief.
-                </p>
-                <p>
-                  I consent to the Ministry contacting other branches within the Ministry and other Province ministries
-                  to validate the accuracy of any information that I have provided.
-                </p>
-              </div>
-              <!-- Declaration B -->
-              <div v-show="isDeclarationBDisplayed">
+              <!-- Declaration B should always been shown for MTFI-->
+              <div v-else-if="isDeclarationBDisplayed">
                 <p>
                   I do hereby certify that I am the <strong>authorized signing authority</strong> and that all of the
                   information provided is true and complete to the best of my knowledge and belief.
@@ -281,7 +272,7 @@
                   approval of enrolment, in writing, in the CCFRI or the ECE Wage Enhancement, the Provider is not
                   formally enrolled in these initiatives. The Province is not responsible for any pre-payments the
                   Provider may make in anticipation of enrolment in either of these initiatives and any pre-payments
-                  made are at the Provider’s own risk.
+                  made are at the Provider's own risk.
                 </p>
               </div>
             </v-col>
@@ -455,6 +446,9 @@ export default {
     },
     isDeclarationBDisplayed() {
       return this.model.enabledDeclarationB || this.hasChangeRequestType('MTFI');
+    },
+    isDeclarationADisplayed() {
+      return this.hasChangeRequestType('PDF_CHANGE') && !this.model.enabledDeclarationB;
     },
     pageTitle() {
       let changeRequestTypes = this.summaryModel?.changeRequestTypes;
