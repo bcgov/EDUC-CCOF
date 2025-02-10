3c02334339520dccdedec7649b203be00e7349e0
Jen Beckett <115109120+jenbeckett@users.noreply.github.com>
2025-02-10 13:42:20 -0800

    throw error right away if signature not found

--- backend/src/components/application.js
+++ backend/src/components/application.js
@@ -456,25 +456,14 @@ async function printPdf(req, numOfRetries = 0) {
     log.info('printPdf :: Waiting for signature field to appear');
     await page.waitForSelector('#signatureTextField', { visible: true });
 
-    let signatureContent = '';
-    let signatureRetryCount = 0;
-
-    while (signatureRetryCount < 5) {
-      signatureContent = await page.$eval('#signatureTextField', (el) => el.value.trim());
-
-      if (signatureContent) {
-        log.info(`printPdf :: Signature field has content: "${signatureContent}"`);
-        break;
-      }
-
-      signatureRetryCount++;
-      log.warn(`printPdf :: WARN: Signature field is empty (Attempt ${signatureRetryCount}/5). Retrying in 2s...`);
-      await sleep(2000);
-    }
+    const signatureContent = await page.$eval('#signatureTextField', (el) => el.value.trim());
 
     if (!signatureContent) {
-      throw new Error('printPdf :: ERROR: No signature found after 5 retries');
+      await sleep(2000);
+      throw new Error('printPdf :: ERROR: No signature found on Decleration.');
     }
+
+    log.info(`printPdf :: signatureTextField content is: ${signatureContent}`);
 
     log.info('printPdf :: Begin Generating PDF');
     const pdfStartTime = Date.now();
