1ed6242c3c8086c5170a95087a5a92dc7d55034c
Jonah <jonahcurl@gmail.com>
2025-03-10 15:35:26 -0700

    ccfri-4511 - Fixed formatting isues

--- backend/src/components/organization.js
+++ backend/src/components/organization.js
@@ -115,5 +115,5 @@ module.exports = {
   getOrganization,
   createOrganization,
   updateOrganization,
-  getOrganizationInGoodStanding: getOrganizationInGoodStandingCheck,
+  getOrganizationInGoodStandingCheck,
 };
