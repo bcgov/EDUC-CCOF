76b7a4dcbef6f054091df69f82f76ada16bb129c
Jonah <jonahcurl@gmail.com>
2025-03-19 09:00:08 -0700

    ccfri-4635 - Fixed styling for big and small screens on landing page

--- frontend/src/components/LandingPage.vue
+++ frontend/src/components/LandingPage.vue
@@ -234,22 +234,20 @@
     />
     <v-card v-else-if="navBarList?.length > 0" class="rounded-lg elevation-0 pa-4 mt-8" border>
       <v-row no-gutters>
-        <v-col class="col-12 col-md-6 ml-4">
+        <v-col class="col-12 col-lg-6">
           <h2>Fiscal Year: {{ programYearNameForFacilityCards }}</h2>
           <h2 v-if="getFundingAgreementNumberByYear">
             Funding Agreement Number: {{ getFundingAgreementNumberByYear }}
           </h2>
         </v-col>
-      </v-row>
-      <v-col class="col-12 col-md-6 ml-4">
-        <v-row class="justify-right align-center col-12 col-lg-7 ml-4">
+        <v-col class="col-12 col-lg-6 d-flex justify-lg-end">
           <v-btn @click="goToOrganizationClosures()" theme="dark" class="blueButton col-12 col-md-6 ml-4 mb-4">
             Organization Closures
           </v-btn>
-        </v-row>
-      </v-col>
-      <v-row no-gutters justify="space-between">
-        <v-col class="col-12 col-lg-7 ml-4">
+        </v-col>
+      </v-row>
+      <v-row no-gutters>
+        <v-col class="col-12 col-lg-4">
           <!--TODO: sezarch box only looks at facility name. Update it later to search for status and licence
             Update when data comes in from the API
             Filter by Facility Name, status, or licence: "
@@ -263,11 +261,11 @@
             :bind="input"
           />
         </v-col>
-        <v-col v-if="applicationIds?.length > 1" class="col-12 col-lg-4">
-          <v-row class="justify-right align-center mr-4">
-            <h3 class="mr-4">Select fiscal year:</h3>
-            <FiscalYearSlider @select-program-year="selectProgramYear" />
-          </v-row>
+        <v-col v-if="applicationIds?.length > 1" class="col-12 col-lg-8 d-flex justify-lg-end">
+          <!-- <v-row> -->
+          <h3 class="mr-4">Select fiscal year:</h3>
+          <FiscalYearSlider @select-program-year="selectProgramYear" />
+          <!-- </v-row> -->
         </v-col>
       </v-row>
       <v-row no-gutters class="justify-space-around">
