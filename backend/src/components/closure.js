5a3376f1cf34e08cabf05dcd3eb6b7f3ec1b6db6
Jonah <jonahcurl@gmail.com>
2025-03-25 10:31:43 -0700

    ccfri-4635 - condensed returnClosures and getClosures into one function in components/closure.js

---
+++ backend/src/components/closure.js
@@ -0,0 +1,35 @@
+'use strict';
+const { getOperation, postOperation, patchOperationWithObjectId, minify, getLabelFromValue, deleteOperationWithObjectId, getApplicationDocument, getHttpHeader } = require('./utils');
+const HttpStatus = require('http-status-codes');
+const axios = require('axios');
+const config = require('../config/index');
+const log = require('./logger');
+const { MappableObjectForFront, MappableObjectForBack, getMappingString } = require('../util/mapping/MappableObject');
+const { ClosureMappings } = require('../util/mapping/Mappings');
+
+function mapClosureObjectForFront(backendClosureObject) {
+  return {};
+}
+
+//a wrapper fn as getCCFRIClosureDates does not take in a req/res
+async function getOrganizationClosuresForFiscalYear(req, res) {
+  try {
+    const url = `ccof_application_ccfri_closures?$filter= _ccof_organization_value eq ${req.query.organizationId} and  _ccof_program_year_value eq ${req.query.programYearId} and ccof_closure_status ge 100000001`;
+    let data = await getOperation(url);
+    const frontendClosures = [];
+
+    for (const closureObject of data.value) {
+      frontendClosures.push(mapClosureObjectForFront(closureObject));
+    }
+
+    const closuresData = { closures: frontendClosures };
+    return res.status(HttpStatus.OK).json(closuresData);
+  } catch (e) {
+    log.error('failed with error', e);
+    return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(e.data ? e.data : e?.status);
+  }
+}
+
+module.exports = {
+  getOrganizationClosuresForFiscalYear,
+};
