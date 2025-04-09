a73d6e30844f2eea8ea23aa349daa55f3b3075bd
Jonah <jonahcurl@gmail.com>
2025-04-09 15:34:01 -0700

    ccfri 4640 - Added failure message

--- frontend/src/components/util/ClosureConfirmationDialog.vue
+++ frontend/src/components/util/ClosureConfirmationDialog.vue
@@ -5,21 +5,16 @@
         <v-col align="center">
           <!-- JonahCurlCGI todo: fill with wording once available -->
           <p class="pt-4 text-h6">Your message has been submitted.</p>
-          <p class="pt-4 text-h6">Reference: {{ referenceNumber }}</p>
+          <p class="pt-4 text-h6">Reference:</p>
           <p class="pt-4 text-h6">Typical response times are 3-5 business days.</p>
         </v-col>
       </v-row>
     </template>
     <template #button>
       <v-row justify="space-around">
-        <v-col cols="12" md="6" class="d-flex justify-center">
-          <AppButton id="return-button" :primary="false" size="large" min-width="250px" @click="returnPage()"
-            >Return to {{ returnTo }}</AppButton
-          >
-        </v-col>
-        <v-col cols="12" md="6" class="d-flex justify-center">
-          <AppButton id="view-messages-button" size="large" min-width="250px" @click="viewMessages()"
-            >View messages</AppButton
+        <v-col cols="12" class="d-flex justify-center">
+          <AppButton id="view-messages-button" size="large" min-width="250px" @click="closeDialog"
+            >Return to Closures</AppButton
           >
         </v-col>
       </v-row>
@@ -73,18 +68,6 @@ export default {
   methods: {
     closeDialog() {
       this.$emit('close');
-    },
-
-    viewMessages() {
-      this.closeDialog();
-      this.$router.push({ name: 'messaging' });
-    },
-
-    returnPage() {
-      this.closeDialog();
-      if (!this.$route.path?.includes(this.returnTo)) {
-        this.$router.push({ name: this.returnTo });
-      }
     },
   },
 };
