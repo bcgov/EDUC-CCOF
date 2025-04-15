fe050421f51bdb9d729540ceb838331f48db388e
Le <vietdung.le@cgi.com>
2025-04-15 09:46:52 -0700

    ccfri-4739 - minor UI change

--- frontend/src/components/ccofApplication/CcofApplicationTypeSelector.vue
+++ frontend/src/components/ccofApplication/CcofApplicationTypeSelector.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container max-width="1200">
+  <v-container max-width="1600">
     <p class="text-h4 text-center">Welcome to Child Care Operating Funding (CCOF)</p>
     <p class="px-4 mt-12 mb-4">
       Select the applicable provider type for your Organization. If you are unsure which type to select, you can preview
@@ -27,7 +27,7 @@
     </p>
     <v-row class="px-4 pb-8">
       <v-col cols="12" md="6" class="d-flex">
-        <v-card class="pa-4 d-flex flex-column flex-grow-1 elevation-4" height="250">
+        <v-card class="pa-4 d-flex flex-column flex-grow-1 elevation-4" min-height="220">
           <v-card-title>Group Provider</v-card-title>
           <v-card-text>
             You have a Group or Multi-Age Licence for eight or more children for a facility that is not your personal
@@ -39,7 +39,7 @@
         </v-card>
       </v-col>
       <v-col cols="12" md="6" class="d-flex">
-        <v-card class="pa-4 d-flex flex-column flex-grow-1 elevation-4">
+        <v-card class="pa-4 d-flex flex-column flex-grow-1 elevation-4" min-height="220">
           <v-card-title>Family Provider</v-card-title>
           <v-card-text>
             Family, In-Home or Multi-Age Licence for eight or fewer children in a personal residence.
