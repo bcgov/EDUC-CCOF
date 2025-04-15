1c1b6c068aef54e09247eb969022c807f2468f06
Jonah <jonahcurl@gmail.com>
2025-04-15 14:57:24 -0700

    ccfri 4640 - added v-form and vaildation

--- frontend/src/components/NewClosureRequestDialog.vue
+++ frontend/src/components/NewClosureRequestDialog.vue
@@ -1,203 +1,207 @@
 <template>
   <AppDialog v-model="isDisplayed" title="New Closure Request" :loading="isLoading" @close="closeDialog">
     <template #content>
-      <v-container width="80%" class="left-align text-primary pa-0">
-        <h1>Fiscal Year: {{ getProgramYearNameById(programYearId).slice(0, -3) }}</h1>
-        <p class="left-align mt-6 text-black">
-          Closures may impact your CCFRI payments. See the
-          <a :href="getFundingUrl(programYearId)" target="_blank" class="text-decoration-underline">
-            CCFRI Funding Guidelines</a
-          >
-          for information on the maximum closure period. Information about eligibility for CCOF Base Funding during
-          Emergency closures is available on the CCOF website.
-        </p>
-        <h3 class="mt-6">Select a Facility:</h3>
-        <v-select
-          v-model="selectedFacility"
-          :rules="rules.required"
-          :items="facilityList"
-          :loading="isLoading"
-          item-value="facilityId"
-          item-title="facilityName"
-          placeholder="Select a facility"
-          variant="outlined"
-          class="mt-2"
-          @update:model-value="handleFacilityChange"
-        >
-          <template #item="{ props, item, index }">
-            <v-list-item
-              :value="props.value"
-              :active="props.active"
-              :title="null"
-              class="text-primary"
-              @click="props.onClick"
+      <v-form v-model="isValidForm">
+        <v-container width="80%" class="left-align text-primary pa-0">
+          <h1>Fiscal Year: {{ getProgramYearNameById(programYearId).slice(0, -3) }}</h1>
+          <p class="left-align mt-6 text-black">
+            Closures may impact your CCFRI payments. See the
+            <a :href="getFundingUrl(programYearId)" target="_blank" class="text-decoration-underline">
+              CCFRI Funding Guidelines</a
             >
+            for information on the maximum closure period. Information about eligibility for CCOF Base Funding during
+            Emergency closures is available on the CCOF website.
+          </p>
+          <h3 class="mt-6">Select a Facility:</h3>
+          <v-select
+            v-model="selectedFacility"
+            :rules="rules.required"
+            :items="facilityList"
+            :loading="isLoading"
+            item-value="facilityId"
+            item-title="facilityName"
+            placeholder="Select a facility"
+            variant="outlined"
+            class="mt-2"
+            @update:model-value="handleFacilityChange"
+          >
+            <template #item="{ props, item, index }">
+              <v-list-item
+                :value="props.value"
+                :active="props.active"
+                :title="null"
+                class="text-primary"
+                @click="props.onClick"
+              >
+                <v-row>
+                  <v-col cols="12" md="8" align="start">
+                    <h4>{{ item.raw.facilityName }}</h4>
+                    <p>License #: {{ item.raw.licenseNumber }}</p>
+                  </v-col>
+                  <v-col cols="12" md="4" align="start">
+                    <p>Facility ID:</p>
+                    <p>{{ item.raw.facilityAccountNumber }}</p>
+                  </v-col>
+                </v-row>
+              </v-list-item>
+              <v-divider v-if="index < facilityList.length - 1" class="mx-4" />
+            </template>
+            <template #selection="{ item }">
+              <!-- JonahCurlCGI todo: fix formatting -->
               <v-row>
-                <v-col cols="12" md="8" align="start">
+                <v-col cols="12" md="8" align="start" class="ml-0">
                   <h4>{{ item.raw.facilityName }}</h4>
                   <p>License #: {{ item.raw.licenseNumber }}</p>
                 </v-col>
-                <v-col cols="12" md="4" align="start">
+                <v-col cols="12" md="4" align="end">
                   <p>Facility ID:</p>
                   <p>{{ item.raw.facilityAccountNumber }}</p>
                 </v-col>
               </v-row>
-            </v-list-item>
-            <v-divider v-if="index < facilityList.length - 1" class="mx-4" />
-          </template>
-          <template #selection="{ item }">
-            <!-- JonahCurlCGI todo: fix formatting -->
-            <v-row>
-              <v-col cols="12" md="8" align="start" class="ml-0">
-                <h4>{{ item.raw.facilityName }}</h4>
-                <p>License #: {{ item.raw.licenseNumber }}</p>
-              </v-col>
-              <v-col cols="12" md="4" align="end">
-                <p>Facility ID:</p>
-                <p>{{ item.raw.facilityAccountNumber }}</p>
-              </v-col>
-            </v-row>
-          </template>
-        </v-select>
-        <v-row>
-          <v-col cols="12" lg="9">
-            <h3 class="left-align mt-2">Will parents pay for this closure?</h3>
-          </v-col>
-          <v-col cols="12" lg="3" class="pl-0">
-            <v-radio-group v-model="parentsWillPayForClosure" :rules="rules.required">
-              <v-row justify="start">
-                <v-col cols="6">
-                  <v-radio label="Yes" :value="1" />
-                </v-col>
-                <v-col cols="6">
-                  <v-radio label="No" :value="0" />
-                </v-col>
-              </v-row>
-            </v-radio-group>
-          </v-col>
-        </v-row>
-        <v-container v-if="selectedFacility && parentsWillPayForClosure !== undefined" width="100%" class="pa-0">
+            </template>
+          </v-select>
           <v-row>
             <v-col cols="12" lg="9">
-              <h3 class="left-align mt-2">
-                Is this a full facility closure?
-                <AppTooltip tooltip-content="Select no if only some care categories will be affected by the closure." />
-              </h3>
+              <h3 class="left-align mt-2">Will parents pay for this closure?</h3>
             </v-col>
             <v-col cols="12" lg="3" class="pl-0">
-              <v-radio-group
-                v-model="fullFacilityClosure"
-                :rules="rules.required"
-                @update:model-value="handleFullFacilityClosureChange"
-              >
+              <v-radio-group v-model="parentsWillPayForClosure" :rules="rules.required">
                 <v-row justify="start">
                   <v-col cols="6">
-                    <v-radio label="Yes" :value="true" />
+                    <v-radio label="Yes" :value="1" />
                   </v-col>
                   <v-col cols="6">
-                    <v-radio label="No" :value="false" />
+                    <v-radio label="No" :value="0" />
                   </v-col>
                 </v-row>
               </v-radio-group>
             </v-col>
           </v-row>
-          <div v-if="fullFacilityClosure === false">
-            <v-row class="ml-0">
-              <h3 align="start" class="pr-2">Affected Care Categorie(s)</h3>
-              <p>(select all that apply):</p>
+          <v-container v-if="selectedFacility && parentsWillPayForClosure !== undefined" width="100%" class="pa-0">
+            <v-row>
+              <v-col cols="12" lg="9">
+                <h3 class="left-align mt-2">
+                  Is this a full facility closure?
+                  <AppTooltip
+                    tooltip-content="Select no if only some care categories will be affected by the closure."
+                  />
+                </h3>
+              </v-col>
+              <v-col cols="12" lg="3" class="pl-0">
+                <v-radio-group
+                  v-model="fullFacilityClosure"
+                  :rules="rules.required"
+                  @update:model-value="handleFullFacilityClosureChange"
+                >
+                  <v-row justify="start">
+                    <v-col cols="6">
+                      <v-radio label="Yes" :value="true" />
+                    </v-col>
+                    <v-col cols="6">
+                      <v-radio label="No" :value="false" />
+                    </v-col>
+                  </v-row>
+                </v-radio-group>
+              </v-col>
             </v-row>
-            <v-select
-              v-model.lazy="selectedAgeGroups"
-              :loading="isLoading"
-              :items="ageGroups"
-              item-title="label"
-              item-value="value"
-              label="Select affected care categories"
+            <div v-if="fullFacilityClosure === false">
+              <v-row class="ml-0">
+                <h3 align="start" class="pr-2">Affected Care Categorie(s)</h3>
+                <p>(select all that apply):</p>
+              </v-row>
+              <v-select
+                v-model.lazy="selectedAgeGroups"
+                :loading="isLoading"
+                :items="ageGroups"
+                item-title="label"
+                item-value="value"
+                label="Select affected care categories"
+                variant="outlined"
+                class="mt-2 pl-0 text-left"
+                multiple
+                chips
+                :rules="rulesAgeGroups"
+                clearable
+              >
+                <template #prepend-item>
+                  <v-list-item title="Select All" @click="toggleSelectAll">
+                    <template #prepend>
+                      <v-checkbox-btn
+                        :color="someAgeGroupsSelected ? '#003366' : undefined"
+                        :indeterminate="someAgeGroupsSelected && !allAgeGroupsSelected"
+                        :model-value="someAgeGroupsSelected"
+                      />
+                    </template>
+                  </v-list-item>
+                  <v-divider class="mt-2" />
+                </template>
+              </v-select>
+            </div>
+            <v-row>
+              <v-col cols="12" lg="3">
+                <h3 class="left-align mt-2">
+                  Dates:
+                  <AppTooltip tooltip-content="Select the estimated end date, if applicable." />
+                </h3>
+              </v-col>
+              <v-col cols="12" lg="4">
+                <AppDateInput
+                  v-model="formattedStartDate"
+                  :min="fiscalStartAndEndDates.startDate"
+                  :max="formattedEndDate ? formattedEndDate : fiscalStartAndEndDates.endDate"
+                  :rules="[
+                    ...rules.required,
+                    rules.min(fiscalStartAndEndDates.startDate, 'Must exceed fiscal year start date'),
+                    rules.max(fiscalStartAndEndDates.endDate, 'Must be before fiscal year end date'),
+                    rules.MMDDYYYY,
+                  ]"
+                  label="Start Date"
+                  clearable
+                />
+              </v-col>
+              <v-col cols="12" lg="1" class="pt-6" align="center">to</v-col>
+              <v-col cols="12" lg="4">
+                <AppDateInput
+                  v-model="formattedEndDate"
+                  :min="formattedStartDate ? formattedStartDate : fiscalStartAndEndDates.startDate"
+                  :max="fiscalStartAndEndDates.endDate"
+                  :rules="[
+                    ...rules.required,
+                    rules.min(fiscalStartAndEndDates.startDate, 'Must exceed fiscal year start date'),
+                    rules.max(fiscalStartAndEndDates.endDate, 'Must be before fiscal year end date'),
+                    rules.MMDDYYYY,
+                  ]"
+                  label="End Date"
+                  clearable
+                />
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="12" lg="3" align="start">
+                <h3>Reason:</h3>
+              </v-col>
+              <v-col cols="12" lg="9" align="start">
+                <v-text-field v-model="reason" variant="outlined" :rules="rules.required"></v-text-field>
+              </v-col>
+            </v-row>
+            <h3>Request Description:</h3>
+            <v-textarea
+              v-model="requestDescription"
               variant="outlined"
-              class="mt-2 pl-0 text-left"
-              multiple
-              chips
-              :rules="rulesAgeGroups"
-              clearable
-            >
-              <template #prepend-item>
-                <v-list-item title="Select All" @click="toggleSelectAll">
-                  <template #prepend>
-                    <v-checkbox-btn
-                      :color="someAgeGroupsSelected ? '#003366' : undefined"
-                      :indeterminate="someAgeGroupsSelected && !allAgeGroupsSelected"
-                      :model-value="someAgeGroupsSelected"
-                    />
-                  </template>
-                </v-list-item>
-                <v-divider class="mt-2" />
-              </template>
-            </v-select>
-          </div>
-          <v-row>
-            <v-col cols="12" lg="3">
-              <h3 class="left-align mt-2">
-                Dates:
-                <AppTooltip tooltip-content="Select the estimated end date, if applicable." />
-              </h3>
-            </v-col>
-            <v-col cols="12" lg="4">
-              <AppDateInput
-                v-model="formattedStartDate"
-                :min="fiscalStartAndEndDates.startDate"
-                :max="formattedEndDate ? formattedEndDate : fiscalStartAndEndDates.endDate"
-                :rules="[
-                  ...rules.required,
-                  rules.min(fiscalStartAndEndDates.startDate, 'Must exceed fiscal year start date'),
-                  rules.max(fiscalStartAndEndDates.endDate, 'Must be before fiscal year end date'),
-                  rules.MMDDYYYY,
-                ]"
-                label="Start Date"
-                clearable
-              />
-            </v-col>
-            <v-col cols="12" lg="1" class="pt-6" align="center">to</v-col>
-            <v-col cols="12" lg="4">
-              <AppDateInput
-                v-model="formattedEndDate"
-                :min="formattedStartDate ? formattedStartDate : fiscalStartAndEndDates.startDate"
-                :max="fiscalStartAndEndDates.endDate"
-                :rules="[
-                  ...rules.required,
-                  rules.min(fiscalStartAndEndDates.startDate, 'Must exceed fiscal year start date'),
-                  rules.max(fiscalStartAndEndDates.endDate, 'Must be before fiscal year end date'),
-                  rules.MMDDYYYY,
-                ]"
-                label="End Date"
-                clearable
-              />
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col cols="12" lg="3" align="start">
-              <h3>Reason:</h3>
-            </v-col>
-            <v-col cols="12" lg="9" align="start">
-              <v-text-field v-model="reason" variant="outlined" :rules="rules.required"></v-text-field>
-            </v-col>
-          </v-row>
-          <h3>Request Description:</h3>
-          <v-textarea
-            v-model="requestDescription"
-            variant="outlined"
-            label="Detailed description of request"
-            class="text-left"
-          />
-          <AppDocumentUpload
-            :loading="isLoading"
-            :document-type="DOCUMENT_TYPES.CLOSURE_REQUEST"
-            title="Supporting Documents"
-            class="left-align"
-            :required="false"
-            @update-documents-to-upload="updateDocuments"
-          />
+              label="Detailed description of request"
+              class="text-left"
+            />
+            <AppDocumentUpload
+              :loading="isLoading"
+              :document-type="DOCUMENT_TYPES.CLOSURE_REQUEST"
+              title="Supporting Documents"
+              class="left-align"
+              :required="false"
+              @update-documents-to-upload="updateDocuments"
+            />
+          </v-container>
         </v-container>
-      </v-container>
+      </v-form>
     </template>
     <template #button>
       <v-container width="80%">
@@ -206,7 +210,7 @@
             <AppButton :primary="false" :disabled="isLoading" @click="closeDialog">Cancel</AppButton>
           </v-col>
           <v-col cols="6" md="6" align="right">
-            <AppButton :disabled="!formComplete" @click="submit">Submit</AppButton>
+            <AppButton :disabled="!isValidForm" @click="submit">Submit</AppButton>
           </v-col>
         </v-row>
       </v-container>
@@ -252,6 +256,7 @@ export default {
   emits: ['close', 'submitted'],
   data() {
     return {
+      isValidForm: false,
       isDisplayed: false,
       isLoading: false,
       selectedFacility: undefined,
@@ -276,25 +281,13 @@ export default {
       return this.getFacilityListForPCFByProgramYearId(this.programYearId);
     },
     rulesAgeGroups() {
-      return this.fullFacilityClosure ? rules.required : [];
+      return this.fullFacilityClosure === false ? rules.required : [];
     },
     allAgeGroupsSelected() {
       return this.selectedAgeGroups?.length === this.ageGroups?.length;
     },
     someAgeGroupsSelected() {
       return this.selectedAgeGroups?.length > 0;
-    },
-    formComplete() {
-      return (
-        !this.isLoading &&
-        this.selectedFacility &&
-        this.parentsWillPayForClosure !== undefined &&
-        this.fullFacilityClosure !== undefined &&
-        (this.fullFacilityClosure || this.selectedAgeGroups.length > 0) &&
-        this.formattedStartDate &&
-        this.formattedEndDate &&
-        this.reason
-      );
     },
     applicationId() {
       const application = this.userInfo.applications?.find(
