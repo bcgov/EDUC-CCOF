50a86f6c6f009270e333d141941e0d1b0cb969c9
Jonah <jonahcurl@gmail.com>
2025-04-22 13:20:16 -0700

    ccfri 4764 - Fixed issue with auth.js import

--- frontend/src/components/NewClosureRequestDialog.vue
+++ frontend/src/components/NewClosureRequestDialog.vue
@@ -238,7 +238,7 @@ import FacilityService from '@/services/facilityService';
 import alertMixin from '@/mixins/alertMixin';
 import { useAppStore } from '@/store/app.js';
 import { useApplicationStore } from '@/store/application.js';
-import { useAuthStore } from '@/store/Auth.js';
+import { useAuthStore } from '@/store/auth.js';
 
 export default {
   name: 'NewClosureRequestDialog',
