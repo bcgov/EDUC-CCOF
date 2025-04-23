aea5fa25c95369b30d7114d25710ad20a417fd5a
weskubo-cgi <Wesley.Kubo@gov.bc.ca>
2025-04-23 15:02:51 -0700

    Added stubbed components.

---
+++ frontend/src/components/accountMgmt/ContactInfo.vue
@@ -0,0 +1,10 @@
+<template>
+  <h2>Contact Information</h2>
+  <v-card></v-card>
+</template>
+<script>
+export default {
+  name: 'ContactInfo',
+};
+</script>
+<style scoped></style>
---
+++ frontend/src/components/accountMgmt/OrganizationInfo.vue
@@ -0,0 +1,10 @@
+<template>
+  <h2>Organization Information</h2>
+  <v-card></v-card>
+</template>
+<script>
+export default {
+  name: 'OrganizationInfo',
+};
+</script>
+<style scoped></style>
