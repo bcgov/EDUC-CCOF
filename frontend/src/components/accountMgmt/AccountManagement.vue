b67ad4fc89cbe5d353eae5daf37427c465a0df34
Trevor Richards <trev@trevdev.ca>
2025-05-02 14:49:35 -0700

    fix: wrap card in a row/column to create space with nav

--- frontend/src/components/accountMgmt/AccountManagement.vue
+++ frontend/src/components/accountMgmt/AccountManagement.vue
@@ -1,20 +1,24 @@
 <template>
   <v-container fluid class="pa-12">
     <h1 class="mb-6">Organization and Facilities</h1>
-    <v-card>
-      <v-tabs v-model="tab" bg-color="#ffffff" density="compact" color="#003366" show-arrows>
-        <v-tab value="organization-tab"> Organization and Contact Information </v-tab>
-        <v-tab value="funding-agreement-tab"> Funding Agreement </v-tab>
-        <v-tab value="facilities-tab"> Facilities </v-tab>
-      </v-tabs>
-      <v-card-text>
-        <v-window v-model="tab">
-          <v-window-item value="organization-tab"> <ManageOrganization /></v-window-item>
-          <v-window-item value="funding-agreement-tab"><h2>Funding Agreement</h2> </v-window-item>
-          <v-window-item value="facilities-tab"> <h2>Facility(ies)</h2></v-window-item>
-        </v-window>
-      </v-card-text>
-    </v-card>
+    <v-row>
+      <v-col>
+        <v-card>
+          <v-tabs v-model="tab" bg-color="#ffffff" density="compact" color="#003366" show-arrows>
+            <v-tab value="organization-tab"> Organization and Contact Information </v-tab>
+            <v-tab value="funding-agreement-tab"> Funding Agreement </v-tab>
+            <v-tab value="facilities-tab"> Facilities </v-tab>
+          </v-tabs>
+          <v-card-text>
+            <v-window v-model="tab">
+              <v-window-item value="organization-tab"> <ManageOrganization /></v-window-item>
+              <v-window-item value="funding-agreement-tab"><h2>Funding Agreement</h2> </v-window-item>
+              <v-window-item value="facilities-tab"> <h2>Facility(ies)</h2></v-window-item>
+            </v-window>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
     <v-row>
       <v-col>
         <NavButton
