39238a0103c4c2fd310f674bc22852b1c9138358
Le <vietdung.le@cgi.com>
2025-06-16 17:56:38 -0700

    ccfri-5028 - rfi code clean up and UI improvement

--- frontend/src/components/RFI/RFIDocumentUpload.vue
+++ frontend/src/components/RFI/RFIDocumentUpload.vue
@@ -1,78 +1,72 @@
 <template>
   <v-form ref="form" v-model="isValidForm">
-    <div class="pa-0">
-      <div class="pa-2 pa-md-4 ma-0">
-        <p class="text-h5 text--primary px-5 py-0 my-0">Documentation Required</p>
-      </div>
-      <div class="px-md-12 px-7">
-        <v-row class="px-6 text-body-1">
-          Upload supporting documents (for example, receipts, quotes, invoices, and/or budget/finance documents)
-        </v-row>
-        <v-row class="pa-6 pt-2 text-body-2">
-          {{ FILE_REQUIREMENTS_TEXT }}
-        </v-row>
-      </div>
-      <div class="px-md-12 px-7 pb-10">
-        <v-data-table
-          v-if="!isLoading"
-          :headers="headers"
-          :items="uploadedRFITypeDocuments"
-          class="data-table-style"
-          hide-default-header
-          hide-default-footer
-          hide-border
-          :items-per-page="-1"
-          no-data-text=""
-        >
-          <template #top>
-            <v-col flex>
-              <AppButton v-if="!isLocked" id="add-new-file" :primary="false" size="large" class="mb-2" @click="addNew">
-                Add File
-              </AppButton>
-            </v-col>
-          </template>
-          <template #item.document="{ item }">
-            <div v-if="item?.annotationid">
-              <span> {{ item?.filename }} </span>
-            </div>
-            <v-file-input
-              v-else
-              :id="String(item.id)"
-              color="#003366"
-              :rules="rules.fileRules"
-              prepend-icon="mdi-file-upload"
-              :clearable="false"
-              class="mt-4"
-              :accept="FILE_TYPES_ACCEPT"
-              :disabled="false"
-              placeholder="Select your file"
-              required
-              @click:clear="deleteItem(item)"
-              @click="uploadDocumentClicked($event)"
-              @update:model-value="selectFile"
-            />
-          </template>
-          <template #item.description="{ item }">
-            <div v-if="item?.annotationid">
-              <span> {{ item?.description }} </span>
-            </div>
-            <v-text-field
-              v-else
-              v-model="item.description"
-              placeholder="Enter a description (Optional)"
-              clearable
-              :rules="[rules.maxLength(255)]"
-              max-length="255"
-              class="mt-4"
-              @change="descriptionChanged(item)"
-            />
-          </template>
-          <template #item.actions="{ item }">
-            <v-icon v-if="!isLocked" size="small" @click="deleteItem(item)"> mdi-delete </v-icon>
-          </template>
-        </v-data-table>
-      </div>
+    <p class="text-h5 text-primary mb-2">Documentation Required</p>
+    <div>
+      <p class="text-body-1 mb-1">
+        Upload supporting documents (for example, receipts, quotes, invoices, and/or budget/finance documents)
+      </p>
+      <p class="text-body-2">
+        {{ FILE_REQUIREMENTS_TEXT }}
+      </p>
     </div>
+    <v-data-table
+      v-if="!isLoading"
+      :headers="headers"
+      :items="uploadedRFITypeDocuments"
+      class="data-table-style"
+      hide-default-header
+      hide-default-footer
+      hide-border
+      :items-per-page="-1"
+      no-data-text=""
+    >
+      <template #top>
+        <v-col flex>
+          <AppButton v-if="!isLocked" id="add-new-file" :primary="false" size="large" class="mb-2" @click="addNew">
+            Add File
+          </AppButton>
+        </v-col>
+      </template>
+      <template #item.document="{ item }">
+        <div v-if="item?.annotationid">
+          <span> {{ item?.filename }} </span>
+        </div>
+        <v-file-input
+          v-else
+          :id="String(item.id)"
+          color="#003366"
+          :rules="rules.fileRules"
+          prepend-icon="mdi-file-upload"
+          :clearable="false"
+          class="mt-4"
+          :accept="FILE_TYPES_ACCEPT"
+          :disabled="false"
+          placeholder="Select your file"
+          required
+          @click:clear="deleteItem(item)"
+          @click="uploadDocumentClicked($event)"
+          @update:model-value="selectFile"
+        />
+      </template>
+      <template #item.description="{ item }">
+        <div v-if="item?.annotationid">
+          <span> {{ item?.description }} </span>
+        </div>
+        <v-text-field
+          v-else
+          v-model="item.description"
+          placeholder="Enter a description (Optional)"
+          clearable
+          :rules="[rules.maxLength(255)]"
+          max-length="255"
+          class="mt-4"
+          @change="descriptionChanged(item)"
+        />
+      </template>
+      <template #item.actions="{ item }">
+        <v-icon v-if="!isLocked" size="small" @click="deleteItem(item)"> mdi-delete </v-icon>
+      </template>
+    </v-data-table>
   </v-form>
 </template>
 <script>
@@ -111,29 +105,27 @@ export default {
   data() {
     return {
       isLoading: false,
-      rules,
       isValidForm: false,
       currentrow: null,
       headers: [
         {
           text: 'Document',
-          align: 'left',
-          sortable: false,
           value: 'document',
+          sortable: false,
+          width: '35%',
           class: 'table-header',
         },
         {
           text: 'Description',
-          align: 'left',
-          sortable: false,
           value: 'description',
+          sortable: false,
+          width: '55%',
           class: 'table-header',
         },
         {
           text: 'Actions',
-          align: 'left',
-          sortable: false,
           value: 'actions',
+          sortable: false,
           class: 'table-header',
         },
       ],
@@ -281,5 +273,9 @@ export default {
 .data-table-style {
   border-collapse: collapse;
   border-bottom: none;
+}
+:deep(.v-field__input) {
+  overflow: hidden;
+  text-overflow: ellipsis;
 }
 </style>
