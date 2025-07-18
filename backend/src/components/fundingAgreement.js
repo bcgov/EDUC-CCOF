cb222563bbe34ee44909f35908f3bbf9951e303e
Bhat <poornima.bhat@cgi.com>
2025-07-18 11:30:17 -0700

    ccfri 5477 add the status code filtering on the server side

--- backend/src/components/fundingAgreement.js
+++ backend/src/components/fundingAgreement.js
@@ -8,8 +8,9 @@ const { MappableObjectForFront } = require('../util/mapping/MappableObject');
 
 async function getFundingAgreements(req, res) {
   try {
-    const response = await getOperation(`ccof_funding_agreements?${buildFilterQuery(req.query, FundingAgreementMappings)}`);
-    const fundingAgreements = response?.value?.map((item) => new MappableObjectForFront(item, FundingAgreementMappings).toJSON()).filter((agreement) => agreement.fundingAgreementStatus !== 'Drafted');
+    const query = buildFilterQuery(req.query, FundingAgreementMappings).concat(' and statuscode ne 101510002'); // 101510002 = 'Drafted'
+    const response = await getOperation(`ccof_funding_agreements?${query}`);
+    const fundingAgreements = response?.value?.map((item) => new MappableObjectForFront(item, FundingAgreementMappings).toJSON());
     return res.status(HttpStatus.OK).json(fundingAgreements);
   } catch (e) {
     log.error(e);
