7eb9a471d4f36f81b4aac00baf0fb0d7f7510e73
Trevor Richards <trev@trevdev.ca>
2025-07-24 13:59:50 -0700

    refactor: review notes

--- backend/src/routes/contact.js
+++ backend/src/routes/contact.js
@@ -3,7 +3,7 @@ const passport = require('passport');
 const router = express.Router();
 const auth = require('../components/auth');
 const isValidBackendToken = auth.isValidBackendToken();
-const { disableContact, getActiveContactsInOrganization } = require('../components/contact');
+const { deactivateContact, getActiveContactsInOrganization } = require('../components/contact');
 const { param, validationResult } = require('express-validator');
 
 module.exports = router;
@@ -23,11 +23,11 @@ router.get(
 );
 
 /**
- * Soft delete (disable) a contact.
+ * Soft delete (deactivate) a contact.
  */
 router.delete('/:contactId', passport.authenticate('jwt', { session: false }), isValidBackendToken, [param('contactId', 'URL param: [contactId] is required').not().isEmpty()], (req, res) => {
   validationResult(req).throw();
-  return disableContact(req, res);
+  return deactivateContact(req, res);
 });
 
 module.exports = router;
