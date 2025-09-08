cba5dd975bf032c620c86916e0d12f2fe2dee07a
Le <vietdung.le@cgi.com>
2025-09-08 11:33:36 -0700

    ccfri-5403 - update ER declaration

--- frontend/src/components/enrolmentReports/EnrolmentReportDeclaration.vue
+++ frontend/src/components/enrolmentReports/EnrolmentReportDeclaration.vue
@@ -5,36 +5,32 @@
     </div>
     <template v-else>
       <EnrolmentReportHeader :enrolment-report="enrolmentReport" />
-      <v-card variant="outlined" class="px-8 px-md-12 py-4 py-lg-8 mt-8">
-        <h2 class="text-center mb-4 mb-lg-8">Declaration and Submission</h2>
+      <v-card variant="outlined" class="px-8 px-md-12 py-6 mt-4">
+        <h2 class="text-center mb-6">Declaration and Submission</h2>
         <div class="declaration-content px-md-4 px-xl-12 pb-2">
-          <p>
-            I have read and understand the Funding Agreement, have direct knowledge of the operation of the child care
-            facilities, and am authorized to submit enrolment reports for these facilities.
-          </p>
-          <p>
-            The Enrolment Reports being submitted have been completed with care and accurately represent the services
-            provided by these facilities.
-          </p>
-          <p>
-            I understand that the Ministry will be relying on the content of these Reports for its decision to disburse
-            funds to this Organization.
-          </p>
-          <p>I confirm that:</p>
-          <ul class="ml-12">
+          <p>By submitting this Enrolment Report, I confirm that:</p>
+          <ul class="ml-8">
+            <li>The information provided in this report is true, accurate and complete to the best of my knowledge.</li>
+            <li>I am authorized to submit enrolment reports for these facilities.</li>
             <li>
-              these facilities have not accepted registered or drop-in children, in numbers which would violate the
-              Community Care Facility Act licence;
+              I understand that the Ministry will be relying on the content of these Reports for its decision to
+              disburse funds to this Organization.
             </li>
             <li>
-              these facilities have not claimed a contribution from the province with respect to a drop-in child who was
-              occupying a space that was temporarily available due to the absence of an enrolled child; these facilities
-              are maintaining records that support the enrolment numbers reported and will provide them upon request to
-              parties authorized by the Ministry for review or audit purposes;
+              These facilities have not accepted registered or drop-in children in numbers which would violate the
+              Community Care and Assisted Living Act and Child Care Licensing Regulation.
             </li>
             <li>
-              these facilities have not claimed a contribution from the province with respect to any children living in
-              the licencee's own home (Family Providers Only)
+              These facilities have not claimed a contribution from the province for a drop-in child who was temporarily
+              occupying a space that was made available due to the absence of an enrolled child;
+            </li>
+            <li>
+              These facilities are maintaining records that support the enrolment numbers reported and will provide them
+              upon request to parties authorized by the Ministry for review or audit purposes;
+            </li>
+            <li>
+              These facilities have not claimed a contribution from the province with respect to any children living in
+              the licensee's own home (Family Providers Only).
             </li>
           </ul>
         </div>
@@ -109,6 +105,10 @@ export default {
 </script>
 <style scoped>
 .declaration-content p {
+  margin-bottom: 16px;
+}
+
+.declaration-content li {
   margin-bottom: 16px;
 }
 
