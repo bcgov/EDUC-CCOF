a1ec92bfaa7a220b6340085fc805514cd757cafb
Mahmoudian <hedie.mahmoudian@cgi.com>
2025-09-11 17:58:07 -0700

    Created new branch matching correct associated task number

--- test/Cypressautomation/cypress/e2e/2-group-application/02-organization-info-group-application-CCFRI.cy.js
+++ test/Cypressautomation/cypress/e2e/2-group-application/02-organization-info-group-application-CCFRI.cy.js
@@ -30,25 +30,15 @@ describe('Group Application-CCFRI', () => {
     cy.clickByText('Save')
     cy.clickByText('Next')
     cy.contains('p', 'Enter the fees you would charge a new parent for full-time care at this facility for the months below.', { timeout: 10000 }).should('be.visible')
-    // cy.get('p.px-6.py-3.card-title.font-weight-bold', { timeout: 10000 })
-    //     .eq(0)
-    //     .should('be.visible')
-    //     .and('have.text', ' Parent Fees 2024-25 FY: Full-Time 0 to 18 months')
     
-    cy.getByLabel(parentFees.frequency).click()
-    cy.log('parent fee frequency selected')
+    // cy.getByLabel(parentFees.frequency).click()
     const parentFeeFrequency = parentFees.frequency;
-    cy.log(`this is the frequency: ${parentFeeFrequency}`)
     if (parentFeeFrequency == "monthly")
+        cy.get('p')
         Object.entries(parentFees.parentFeeDates).forEach(([category, value]) => {
-            cy.log(`filling info out for ${category}`)
-            // cy.get('pa-8.my-2').debug()
-
-            // cy.get('pa-8.my-2.py-2', { timeout: 10000 }).contains('my-2.py-2', 'Enter your highest monthly parent fee before CCFRI is applied in every month below. If there is a month where you do not charge a parent fee, enter zero.', { timeout: 10000 }).should('be.visible')
-            
+            cy.get('p').should('contain', `${category}`).selectByLabel(parentFeeFrequency)
             Object.entries(value).forEach(([month, fee]) => {
                 cy.log(`current month is: ${month}`)
-                // cy.getByLabel(month).should('be.visible').and('have.text', `${month}`)
                 cy.getByLabel(month).typeAndAssert(fee, {force: true})
             });
         });
