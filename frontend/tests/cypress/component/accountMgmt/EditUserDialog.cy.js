5a1eeb72cded29bbc86f78fd114eb118df7fbd88
saibannur-cgi <sai.bannur@cgi.com>
2025-09-25 11:09:18 -0700

    added prop passing

--- frontend/tests/cypress/component/accountMgmt/EditUserDialog.cy.js
+++ frontend/tests/cypress/component/accountMgmt/EditUserDialog.cy.js
@@ -1,9 +1,21 @@
 import EditUserDialog from '@/components/accountMgmt/EditUserDialog.vue';
 import vuetify from '@/plugins/vuetify';
 
+const mockUser = {
+  contactId: 123,
+  firstName: 'Test',
+  lastName: 'User',
+  bceid: 'testuser',
+  email: 'test@example.com',
+  telephone: '123-456-7890',
+  role: { roleNumber: 1 },
+  facilities: [],
+};
+
 function mountWithPinia(initialState = {}, dataOverride = {}) {
   cy.setupPinia({ initialState, stubActions: false }).then((pinia) => {
     const pushStub = cy.stub();
+
     cy.mount(EditUserDialog, {
       global: {
         plugins: [pinia, vuetify],
@@ -13,13 +25,24 @@ function mountWithPinia(initialState = {}, dataOverride = {}) {
           ...dataOverride,
         };
       },
+      props: {
+        show: true,
+        user: mockUser,
+      },
+    }).then(({ component }) => {
+      component.loadEditUserData();
     });
+
     cy.wrap(pushStub).as('routerPush');
   });
 }
 
 describe('<EditUserDialog />', () => {
+  beforeEach(() => {
+    cy.viewport(800, 1000);
+  });
+
   it('mounts', () => {
-    mountWithPinia();
+    mountWithPinia({}, { dialogOpen: true });
   });
 });
