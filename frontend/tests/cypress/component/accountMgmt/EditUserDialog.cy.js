205c849200c2efd5f2f768bda76812825351428d
saibannur-cgi <sai.bannur@cgi.com>
2025-09-26 08:05:58 -0700

    added remove and edit user tests

--- frontend/tests/cypress/component/accountMgmt/EditUserDialog.cy.js
+++ frontend/tests/cypress/component/accountMgmt/EditUserDialog.cy.js
@@ -1,6 +1,10 @@
 import EditUserDialog from '@/components/accountMgmt/EditUserDialog.vue';
 import vuetify from '@/plugins/vuetify';
+import { useAuthStore } from '@/store/auth';
+import { ApiRoutes, ROLES } from '@/utils/constants.js';
+import { PERMISSIONS } from '@/utils/constants/permissions.js';
 
+const organizationId = '1234';
 const mockUser = {
   contactId: 123,
   firstName: 'Test',
@@ -11,10 +15,36 @@ const mockUser = {
   role: { roleNumber: 1 },
   facilities: [],
 };
+const adminUser = {
+  contactId: 123,
+  firstName: 'Test',
+  lastName: 'User',
+  bceid: 'testuser',
+  email: 'test@example.com',
+  telephone: '123-456-7890',
+  role: { roleNumber: ROLES.FAC_ADMIN_ADVANCED },
+  facilities: [],
+};
+const facilityResponse = [
+  {
+    facilityName: 'Test-Facility1',
+    facilityId: '777',
+  },
+  {
+    facilityName: 'Test-Facility2',
+    facilityId: '888',
+  },
+  {
+    facilityName: 'Test-Facility3',
+    facilityId: '999',
+  },
+];
 
-function mountWithPinia(initialState = {}, dataOverride = {}) {
+function mountWithPinia(initialState = {}, user) {
   cy.setupPinia({ initialState, stubActions: false }).then((pinia) => {
     const pushStub = cy.stub();
+    const onContactUpdated = cy.spy().as('contactUpdatedSpy');
+    const onCloseEditDialog = cy.spy().as('closeEditDialogSpy');
 
     cy.mount(EditUserDialog, {
       global: {
@@ -22,12 +52,15 @@ function mountWithPinia(initialState = {}, dataOverride = {}) {
       },
       data() {
         return {
-          ...dataOverride,
+          dialogOpen: true,
         };
       },
+      attrs: {
+        'onContact-updated': onContactUpdated,
+        'onClose-edit-dialog': onCloseEditDialog,
+      },
       props: {
-        show: true,
-        user: mockUser,
+        user: user ?? mockUser,
       },
     }).then(({ component }) => {
       component.loadEditUserData();
@@ -39,10 +72,111 @@ function mountWithPinia(initialState = {}, dataOverride = {}) {
 
 describe('<EditUserDialog />', () => {
   beforeEach(() => {
-    cy.viewport(800, 1000);
+    globalThis.localStorage.setItem('jwtToken', 'mocked-jwt-token');
+    cy.viewport(1200, 1500);
   });
 
-  it('mounts', () => {
-    mountWithPinia({}, { dialogOpen: true });
+  afterEach(() => {
+    globalThis.localStorage.removeItem('jwtToken');
+  });
+
+  it('should mount the dialog and display inputs for user without edit permissions', () => {
+    mountWithPinia({}, mockUser);
+    cy.contains('h3', 'Edit User');
+
+    cy.get('form input').should('have.length', 5);
+
+    cy.get('form input').eq(0).prev('label').should('have.text', 'First Name');
+    cy.get('form input').eq(1).prev('label').should('have.text', 'Last Name');
+    cy.get('form input').eq(2).prev('label').should('have.text', 'Business BCeID');
+    cy.get('form input').eq(3).prev('label').should('have.text', 'Phone Number');
+    cy.get('form input').eq(4).prev('label').should('have.text', 'Email Address');
+  });
+
+  it('should load the users prior data', () => {
+    mountWithPinia({}, mockUser);
+    cy.get('form input').eq(0).should('have.value', mockUser.firstName);
+    cy.get('form input').eq(1).should('have.value', mockUser.lastName);
+    cy.get('form input').eq(2).should('have.value', mockUser.bceid).shou;
+    cy.get('form input').eq(3).should('have.value', mockUser.telephone);
+    cy.get('form input').eq(4).should('have.value', mockUser.email);
+  });
+
+  it('should disable bceid input', () => {
+    mountWithPinia({}, mockUser);
+    cy.get('form input').eq(2).should('have.value', mockUser.bceid).should('have.css', 'pointer-events', 'none');
+  });
+
+  it('should render user role input for users with edit permission', () => {
+    mountWithPinia({
+      auth: {
+        isAuthenticated: true,
+        userInfo: {
+          serverTime: new Date(),
+        },
+      },
+    });
+
+    cy.then(() => {
+      const authStore = useAuthStore();
+      authStore.permissions = [PERMISSIONS.EDIT_USERS];
+    });
+
+    cy.contains('label', 'User Role');
+    cy.get('form input').eq(5).should('have.value', mockUser.role.roleNumber);
+  });
+
+  it('should render facilities options', () => {
+    cy.intercept('GET', `${ApiRoutes.ORGANIZATION}/${organizationId}/facilities`, {
+      statusCode: 200,
+      body: facilityResponse,
+    });
+
+    mountWithPinia(
+      {
+        auth: {
+          isAuthenticated: true,
+          userInfo: {
+            serverTime: new Date(),
+          },
+        },
+        organization: {
+          organizationId,
+        },
+      },
+      adminUser,
+    );
+
+    cy.then(() => {
+      const authStore = useAuthStore();
+      authStore.permissions = [PERMISSIONS.EDIT_USERS];
+    });
+
+    cy.contains('label', 'Facilities');
+    // TODO: assert size of results
+  });
+
+  it('should update user on clicking `Update` button', () => {
+    cy.intercept('PATCH', `${ApiRoutes.CONTACTS}/${mockUser.contactId}`, {
+      statusCode: 200,
+      body: {},
+    });
+
+    mountWithPinia({});
+
+    cy.contains('button', 'Update').click();
+    cy.get('@contactUpdatedSpy').should('have.been.calledOnce');
+  });
+
+  it('should disable `Update` button when inputs are empty', () => {
+    mountWithPinia({}, { firstName: 'John' });
+    cy.contains('button', 'Update').should('have.css', 'pointer-events', 'none');
+  });
+
+  it('should close dialog', () => {
+    mountWithPinia({}, mockUser);
+    cy.contains('button', 'Cancel').click();
+    cy.contains('Edit User').should('not.be.visible');
+    cy.get('@closeEditDialogSpy').should('have.been.calledOnce');
   });
 });
---
+++ frontend/tests/cypress/component/accountMgmt/RemoveUserDialog.cy.js
@@ -0,0 +1,70 @@
+import RemoveUserDialog from '@/components/accountMgmt/RemoveUserDialog.vue';
+import vuetify from '@/plugins/vuetify';
+import { ApiRoutes } from '@/utils/constants.js';
+
+const mockUser = {
+  contactId: 123,
+  firstName: 'Test',
+  lastName: 'User',
+  bceid: 'testuser',
+  email: 'test@example.com',
+  telephone: '123-456-7890',
+  role: { roleNumber: 1 },
+  facilities: [],
+};
+
+function mountWithPinia(initialState = {}, user) {
+  cy.setupPinia({ initialState, stubActions: false }).then((pinia) => {
+    const pushStub = cy.stub();
+    const onRemoveDialog = cy.spy().as('closeRemoveDialogSpy');
+    const onContactDeactivated = cy.spy().as('contactDeactivated');
+
+    cy.mount(RemoveUserDialog, {
+      global: {
+        plugins: [pinia, vuetify],
+      },
+      data() {
+        return {
+          dialogOpen: true,
+        };
+      },
+      attrs: {
+        'onClose-remove-dialog': onRemoveDialog,
+        'onContact-deactivated': onContactDeactivated,
+      },
+      props: {
+        user: mockUser,
+      },
+    });
+    cy.wrap(pushStub).as('routerPush');
+  });
+}
+
+describe('<RemoveUserDialog />', () => {
+  beforeEach(() => {
+    cy.viewport(1200, 1200);
+  });
+
+  it('should render remove user dialog', () => {
+    mountWithPinia();
+    cy.contains('Remove User');
+    cy.contains(`Are you sure you want to remove ${mockUser.firstName} ${mockUser.lastName}? You can't undo this.`);
+  });
+
+  it('should cancel removal of user', () => {
+    mountWithPinia();
+    cy.contains('button', 'Cancel').click();
+    cy.get('@closeRemoveDialogSpy').should('have.been.calledOnce');
+  });
+
+  it('should delete user', () => {
+    cy.intercept('DELETE', `${ApiRoutes.CONTACTS}/${mockUser.contactId}`, {
+      statusCode: 204,
+    });
+
+    mountWithPinia();
+    cy.contains('button', 'Yes, remove the user').click();
+    cy.get('@contactDeactivated').should('have.been.calledOnce');
+    cy.get('@closeRemoveDialogSpy').should('have.been.calledOnce');
+  });
+});
