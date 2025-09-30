a90ab8e774ada37ccec9647a998568deb32133b5
saibannur-cgi <sai.bannur@cgi.com>
2025-09-30 08:28:06 -0700

    dummy component render

--- frontend/tests/cypress/component/00.cy.js
+++
@@ -1,8 +0,0 @@
-import Footer from '@/components/Footer.vue';
-
-describe('Warmup', () => {
-  it('mounts a component to warm up Cypress and Vite', () => {
-    cy.mount(Footer);
-    cy.wait(1000);
-  });
-});
