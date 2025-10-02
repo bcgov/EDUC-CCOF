3d0b7475fb5f0dfdf3f1bf18140a016734693b6b
saibannur-cgi <sai.bannur@cgi.com>
2025-10-02 16:37:43 -0700

    added manage facilities tests

--- frontend/tests/cypress/component/orgFacilities/ManageFacilities.cy.js
+++ frontend/tests/cypress/component/orgFacilities/ManageFacilities.cy.js
@@ -1,7 +1,31 @@
 import ManageFacilities from '@/components/orgFacilities/ManageFacilities.vue';
 import vuetify from '@/plugins/vuetify';
+import { ApiRoutes, FUNDING_AGREEMENTS_STATUS, ORGANIZATION_FACILITY_STATUS_CODES } from '@/utils/constants';
 
-function mountWithPinia(initialState = {}) {
+const organizationId = '1234';
+
+const activeFacility = {
+  facilityName: 'Pineview Medical Clinic',
+  facilityAccountNumber: 'FAC-104839',
+  facilityIsActive: false,
+  licenseNumber: 'LIC-BC-99821',
+  action: 'Renew License',
+  addressLineOne: '123 Pine Street',
+  city: 'Victoria',
+  statusCode: ORGANIZATION_FACILITY_STATUS_CODES.ACTIVE,
+};
+
+const inActiveFacility = {
+  facilityName: 'Rainbow Clinic',
+  facilityAccountNumber: 'GRL-135612',
+  facilityIsActive: true,
+  licenseNumber: 'LIC-BC-31121',
+  action: 'Renew License',
+  addressLineOne: '543 Rain Street',
+  city: 'Vancouver',
+};
+
+function mountWithPinia(initialState = {}, dataOverrides = {}) {
   cy.setupPinia({ initialState, stubActions: false }).then((pinia) => {
     const pushStub = cy.stub();
 
@@ -19,8 +43,94 @@ function mountWithPinia(initialState = {}) {
   });
 }
 
+function interceptRequests(response = []) {
+  cy.intercept('GET', `${ApiRoutes.ORGANIZATION}/${organizationId}/facilities`, {
+    statusCode: 200,
+    body: response,
+  }).as('getFacilities');
+}
+
 describe('<ManageFaciltiies />', () => {
-  it('mounts', () => {
-    mountWithPinia();
+  beforeEach(() => {
+    globalThis.localStorage.setItem('jwtToken', 'mocked-jwt-token');
+  });
+
+  afterEach(() => {
+    globalThis.localStorage.removeItem('jwtToken');
+  });
+
+  it('should render main page text', () => {
+    interceptRequests();
+    mountWithPinia({
+      organization: {
+        organizationId,
+      },
+    });
+    cy.wait('@getFacilities');
+    cy.contains('p', 'View and manage the facilities of your organization.');
+    cy.contains('h3', 'Active Facilities');
+    cy.contains('h3', 'Inactive Facilities');
+    cy.get('table').should('have.length', 2);
+  });
+
+  it('should render in active facilities', () => {
+    interceptRequests([inActiveFacility]);
+
+    mountWithPinia({
+      organization: {
+        organizationId,
+      },
+    });
+
+    cy.wait('@getFacilities');
+    cy.get('table')
+      .eq(1)
+      .within(() => {
+        cy.get('td').eq(0).should('contain', 'Facility Name').and('contain', inActiveFacility.facilityName);
+        cy.get('td').eq(1).should('contain', 'Facility Address').and('contain', inActiveFacility.addressLineOne);
+        cy.get('td').eq(2).should('contain', 'Facility ID').and('contain', inActiveFacility.facilityAccountNumber);
+        cy.get('td').eq(3).should('contain', 'Licence Number').and('contain', inActiveFacility.licenseNumber);
+      });
+  });
+
+  it('should render active facilities', () => {
+    const programYearId = '1222';
+
+    interceptRequests([activeFacility]);
+
+    mountWithPinia({
+      organization: {
+        organizationId,
+      },
+      application: {
+        programYearId,
+        applicationMap: new Map([
+          [programYearId, { fundingAgreementNumber: '###', internalStatus: FUNDING_AGREEMENTS_STATUS.ACTIVE }],
+        ]),
+      },
+    });
+
+    cy.wait('@getFacilities');
+
+    cy.get('table')
+      .eq(0)
+      .within(() => {
+        cy.get('td').eq(0).should('contain', 'Facility Name').and('contain', activeFacility.facilityName);
+        cy.get('td').eq(1).should('contain', 'Facility Address').and('contain', activeFacility.addressLineOne);
+        cy.get('td').eq(2).should('contain', 'Facility ID').and('contain', activeFacility.facilityAccountNumber);
+        cy.get('td').eq(3).should('contain', 'Licence Number').and('contain', activeFacility.licenseNumber);
+      });
+  });
+
+  it('should display no data available', () => {
+    interceptRequests();
+    mountWithPinia({
+      organization: {
+        organizationId,
+      },
+    });
+
+    cy.wait('@getFacilities');
+    cy.contains('No data available');
   });
 });
