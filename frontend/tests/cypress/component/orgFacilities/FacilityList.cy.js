990a40db4e4eed96f9e0ee867d4da64ede042d59
saibannur-cgi <sai.bannur@cgi.com>
2025-10-03 12:57:20 -0700

    added FacilityList tests

--- frontend/tests/cypress/component/orgFacilities/FacilityList.cy.js
+++ frontend/tests/cypress/component/orgFacilities/FacilityList.cy.js
@@ -1,10 +1,9 @@
-import ManageFacilities from '@/components/orgFacilities/ManageFacilities.vue';
+import FacilityList from '@/components/orgFacilities/FacilityList.vue';
 import vuetify from '@/plugins/vuetify';
-import { ApiRoutes, FUNDING_AGREEMENTS_STATUS, ORGANIZATION_FACILITY_STATUS_CODES } from '@/utils/constants';
+import { PATHS } from '@/utils/constants';
 
-const organizationId = '1234';
-
-const activeFacility = {
+const facility1 = {
+  facilityId: 1,
   facilityName: 'Pineview Medical Clinic',
   facilityAccountNumber: 'FAC-104839',
   facilityIsActive: false,
@@ -12,24 +11,35 @@ const activeFacility = {
   action: '',
   addressLineOne: '123 Pine Street',
   city: 'Victoria',
-  statusCode: ORGANIZATION_FACILITY_STATUS_CODES.ACTIVE,
 };
 
-const inActiveFacility = {
-  facilityName: 'Rainbow Clinic',
-  facilityAccountNumber: 'GRL-135612',
+const facility2 = {
+  facilityId: 2,
+  facilityName: 'Oakview Health Centre',
+  facilityAccountNumber: 'FAC-205734',
   facilityIsActive: true,
-  licenseNumber: 'LIC-BC-31121',
+  licenseNumber: 'LIC-BC-77411',
   action: '',
-  addressLineOne: '543 Rain Street',
+  addressLineOne: '456 Ocean Drive',
   city: 'Vancouver',
 };
 
-function mountWithPinia(initialState = {}) {
+const facility3 = {
+  facilityId: 3,
+  facilityName: 'Uptown Family Practice',
+  facilityAccountNumber: 'FAC-319876',
+  facilityIsActive: false,
+  licenseNumber: 'LIC-BC-66123',
+  action: '',
+  addressLineOne: '789 Alpine Way',
+  city: 'Kelowna',
+};
+
+function mountWithPinia(initialState = {}, overrideProps = {}) {
   cy.setupPinia({ initialState, stubActions: false }).then((pinia) => {
     const pushStub = cy.stub();
 
-    cy.mount(ManageFacilities, {
+    cy.mount(FacilityList, {
       global: {
         plugins: [pinia, vuetify],
         mocks: {
@@ -38,99 +48,48 @@ function mountWithPinia(initialState = {}) {
           },
         },
       },
+      props: {
+        ...overrideProps,
+      },
     });
+
     cy.wrap(pushStub).as('routerPush');
   });
 }
 
-function interceptRequests(response = []) {
-  cy.intercept('GET', `${ApiRoutes.ORGANIZATION}/${organizationId}/facilities`, {
-    statusCode: 200,
-    body: response,
-  }).as('getFacilities');
-}
-
-describe('<ManageFaciltiies />', () => {
-  beforeEach(() => {
-    globalThis.localStorage.setItem('jwtToken', 'mocked-jwt-token');
-  });
-
-  afterEach(() => {
-    globalThis.localStorage.removeItem('jwtToken');
-  });
-
-  it('should render main page text', () => {
-    interceptRequests();
-    mountWithPinia({
-      organization: {
-        organizationId,
-      },
-    });
-    cy.wait('@getFacilities');
-    cy.contains('p', 'View and manage the facilities of your organization.');
-    cy.contains('h3', 'Active Facilities');
-    cy.contains('h3', 'Inactive Facilities');
-    cy.get('table').should('have.length', 2);
-  });
-
-  it('should render in active facilities', () => {
-    interceptRequests([inActiveFacility]);
-
-    mountWithPinia({
-      organization: {
-        organizationId,
-      },
-    });
-
-    cy.wait('@getFacilities');
-    cy.get('table')
-      .eq(1)
-      .within(() => {
-        cy.get('td').eq(0).should('contain', 'Facility Name').and('contain', inActiveFacility.facilityName);
-        cy.get('td').eq(1).should('contain', 'Facility Address').and('contain', inActiveFacility.addressLineOne);
-        cy.get('td').eq(2).should('contain', 'Facility ID').and('contain', inActiveFacility.facilityAccountNumber);
-        cy.get('td').eq(3).should('contain', 'Licence Number').and('contain', inActiveFacility.licenseNumber);
-      });
-  });
-
-  it('should render active facilities', () => {
-    const programYearId = '1222';
-
-    interceptRequests([activeFacility]);
-
-    mountWithPinia({
-      organization: {
-        organizationId,
-      },
-      application: {
-        programYearId,
-        applicationMap: new Map([
-          [programYearId, { fundingAgreementNumber: '###', internalStatus: FUNDING_AGREEMENTS_STATUS.ACTIVE }],
-        ]),
-      },
-    });
-
-    cy.wait('@getFacilities');
-
-    cy.get('table')
-      .eq(0)
-      .within(() => {
-        cy.get('td').eq(0).should('contain', 'Facility Name').and('contain', activeFacility.facilityName);
-        cy.get('td').eq(1).should('contain', 'Facility Address').and('contain', activeFacility.addressLineOne);
-        cy.get('td').eq(2).should('contain', 'Facility ID').and('contain', activeFacility.facilityAccountNumber);
-        cy.get('td').eq(3).should('contain', 'Licence Number').and('contain', activeFacility.licenseNumber);
-      });
-  });
-
-  it('should display no data available', () => {
-    interceptRequests();
-    mountWithPinia({
-      organization: {
-        organizationId,
-      },
-    });
-
-    cy.wait('@getFacilities');
+describe('<FacilityList />', () => {
+  it('should render no data', () => {
+    mountWithPinia();
     cy.contains('No data available');
+  });
+
+  it('should render `Filter by Facility` button', () => {
+    mountWithPinia();
+    cy.contains('button', 'Filter by Facility');
+  });
+
+  it('should render facility data in table', () => {
+    mountWithPinia({}, { facilities: [facility1] });
+
+    cy.get('tbody').find('tr').should('have.length', 1);
+    cy.get('td').eq(0).contains('div', 'Facility Name').next('div').should('have.text', facility1.facilityName);
+    cy.get('td')
+      .eq(1)
+      .contains('div', 'Facility Address')
+      .next('div')
+      .should('have.text', `${facility1.addressLineOne}, ${facility1.city}`);
+    cy.get('td').eq(2).contains('div', 'Facility ID').next('div').should('have.text', facility1.facilityAccountNumber);
+    cy.get('td').eq(3).contains('div', 'Licence Number').next('div').should('have.text', facility1.licenseNumber);
+  });
+
+  it('should navigate to facility page', () => {
+    mountWithPinia({}, { facilities: [facility1] });
+    cy.contains('button', 'Open').click();
+    cy.get('@routerPush').should('have.been.calledWith', `${PATHS.ROOT.MANAGE_FACILITY}/${facility1.facilityId}`);
+  });
+
+  it('should render many facilities in table', () => {
+    mountWithPinia({}, { facilities: [facility1, facility2, facility3] });
+    cy.get('tbody').find('tr').should('have.length', 3);
   });
 });
