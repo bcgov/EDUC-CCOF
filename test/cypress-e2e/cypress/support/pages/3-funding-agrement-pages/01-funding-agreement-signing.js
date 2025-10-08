eb9e466fa0feb375e0542c8f456fd8ce69b975f4
Hedie-cgi <hedie.mahmoudian@cgi.com>
2025-10-08 10:25:38 -0700

    CCFRI-6083 - portal cypress funding agreement signing (#842)
    * CCFRI-5516
    
    * update
    
    * Group application info and selenium setup
    
    * update selenium
    
    * Holding for adding attributes
    
    * CCFRI-5516 : Group application happy path till Licence Upload
    
    * CCFRI-5516 : Removed Data-cy from frontend folder
    
    * Created new branch matching correct associated task number
    
    * Fixed bugs related to Parent Fee portion of CCFRI, now fills out correctly but needs to be optimized
    
    * CCFRI-Parent Fees section completed, ECE-WE 1st successfully automated for all user paths
    
    * Untrack Cypress generated files and add to .gitignore; naming convention; sample config file
    
    * sonarqubeccloud suggestion
    
    * Refactored CCFRI automation to be more readable and include all possible happy path selections, updated cypress to newest available version
    
    * Completed all paths of group app from ccfri - submit, few bugs left to resolve
    
    * file deletion
    
    Group Application E2E running fully without errors, edited config file to increase default timeout to account for browser speed variability
    
    Preparing for merging with Main
    
    handled PR comments
    
    * Final changes made to E2E, marked areas for future addition including adding multiple Facilities, Closures, and adding Partial Closures
    
    * Uncommented section of CCFRI automation, originally commented out for efficiency of bug resolution
    
    * Updated README with details of unfinished Group Application components, finalized e2e script
    
    * Small edits to READ.me
    
    * Addressed PR comments and restructured/refactored code to reduce code duplication across e2e tests
    
    * Removed unecessary files from gitignore
    
    * Updated file path for portal login to correct path names
    
    * Began creating new page for Funding Agreement Signing and it's respective e2e test file
    
    * Completed FA base functionality
    
    * Ticket 6175 resolved, cy.wait() no longer needed
    
    ---------
    
    Co-authored-by: Singla <neha.singla@cgi.com>

---
+++ test/cypress-e2e/cypress/support/pages/3-funding-agrement-pages/01-funding-agreement-signing.js
@@ -0,0 +1,27 @@
+class FundingAgreement {
+    signFundingAgreement() {
+        // Navigate to FA pending signing
+        cy.contains('div', 'Manage Organization and Facilities')
+            .should('be.visible')
+            .clickByText('Manage Organization and Facilities')
+        cy.url().should('contain', '/manage-org-facilities')
+        cy.clickByText('Funding Agreement')
+        cy.contains('Drafted – Provider Action Required').should('be.visible')
+        cy.clickByText('View')
+        // TODO (Hedie-cgi) - Validation of FA PDF requires additional npm package pdf-parse (pending download due to Shai-Hulud worm)
+
+        // Validate PDF & submit
+        cy.contains('Carefully review your funding agreement.').should('be.visible')
+        cy.get('.vue-pdf-embed').should('be.visible')
+        cy.getByLabel('I agree, consent and certify').click({force: true})
+        cy.getByLabel('Your Organization\'s Authorized Signing Authority').typeAndAssert('Luffy', {force: true})
+        cy.clickByText('Submit').click({force: true})
+        cy.contains('Submission Complete').should('be.visible')
+        cy.contains('Your funding agreement has been signed. Refer to the Funding Agreements in Account Management for updates to your agreement.')
+        cy.clickByText('Return to Funding Agreements')
+        cy.url().should('contain', 'funding-agreement-tab')
+        cy.contains('Drafted - with Ministry').should('be.visible')
+    }
+}
+
+export const fundingAgreement = new FundingAgreement()
