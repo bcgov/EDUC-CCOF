bd8874adde59cc876d628582745be6d4fd2a75ef
Bhat <poornima.bhat@cgi.com>
2025-10-15 10:57:43 -0700

    remove redundant 'font-weight-bold' from <strong> elements

--- frontend/src/components/licences/ServiceDetails.vue
+++ frontend/src/components/licences/ServiceDetails.vue
@@ -3,7 +3,7 @@
     <div class="box-container">
       <v-row class="mb-2">
         <v-col cols="12" md="6" class="d-flex flex-wrap align-center">
-          <strong class="font-weight-bold mr-2">Licence Number:</strong>
+          <strong class="mr-2">Licence Number:</strong>
           <span>{{ licence.licenceNumber }}</span>
           <span class="ml-2" :class="getStatusClass(licence.licenceStatus)">
             {{ licence.licenceStatus }}
@@ -12,15 +12,15 @@
       </v-row>
       <v-row class="mb-2">
         <v-col cols="12" md="4" class="d-flex flex-wrap align-center">
-          <strong class="font-weight-bold mr-2">Licence Effective Date:</strong>
+          <strong class="mr-2">Licence Effective Date:</strong>
           <span>{{ formatUTCDate(licence.licenceStartDate) }}</span>
         </v-col>
         <v-col cols="12" md="4" class="d-flex flex-wrap align-center">
-          <strong class="font-weight-bold mr-2">Record Start Date:</strong>
+          <strong class="mr-2">Record Start Date:</strong>
           <span>{{ licence.recordStartDate }}</span>
         </v-col>
         <v-col cols="12" md="4" class="d-flex flex-wrap align-center">
-          <strong class="font-weight-bold mr-2">Record End Date:</strong>
+          <strong class="mr-2">Record End Date:</strong>
           <span>{{ licence.recordEndDate ?? EMPTY_PLACEHOLDER }}</span>
         </v-col>
       </v-row>
@@ -31,17 +31,17 @@
       <div class="box-container">
         <v-row class="mb-2">
           <v-col cols="12" md="6" class="d-flex flex-wrap align-center">
-            <strong class="font-weight-bold mr-2">Maximum Capacity:</strong>
+            <strong class="mr-2">Maximum Capacity:</strong>
             <span>{{ licence.licenceMaxCapacity ?? EMPTY_PLACEHOLDER }}</span>
           </v-col>
         </v-row>
         <v-row v-for="(serviceDetail, index) in licence.serviceDeliveryDetails" :key="index" class="mb-2">
           <v-col cols="12" md="6" class="d-flex flex-wrap align-center">
-            <strong class="font-weight-bold mr-2">Licence Category:</strong>
+            <strong class="mr-2">Licence Category:</strong>
             <span>{{ capitalize(serviceDetail.licenceCategory) }}</span>
           </v-col>
           <v-col cols="12" md="6" class="d-flex flex-wrap align-center">
-            <strong class="font-weight-bold mr-2">Maximum Capacity by Care Type:</strong>
+            <strong class="mr-2">Maximum Capacity by Care Type:</strong>
             <span>{{ serviceDetail.licencedSpaces }}</span>
           </v-col>
         </v-row>
@@ -51,38 +51,38 @@
       <div class="box-container">
         <v-row class="mb-2">
           <v-col cols="12" md="6" class="d-flex flex-wrap align-center">
-            <strong class="font-weight-bold mr-2">Max number of days per week you provide child care:</strong>
+            <strong class="mr-2">Max number of days per week you provide child care:</strong>
             <span>{{ licence.licenceMaxDaysPerWeek ?? EMPTY_PLACEHOLDER }}</span>
           </v-col>
           <v-col cols="12" md="6" class="d-flex flex-wrap align-center">
-            <strong class="font-weight-bold mr-2">Max number of weeks per year you provide child care:</strong>
+            <strong class="mr-2">Max number of weeks per year you provide child care:</strong>
             <span>{{ licence.licenceMaxWeeksPerYear ?? EMPTY_PLACEHOLDER }}</span>
           </v-col>
         </v-row>
         <v-row v-if="Object.keys(preschoolDetail).length" class="mb-2">
           <v-col cols="12" md="6" class="d-flex flex-wrap align-center">
-            <strong class="font-weight-bold mr-2">Number of preschool sessions per week:</strong>
+            <strong class="mr-2">Number of preschool sessions per week:</strong>
             <span>{{ preschoolDetail.preschoolSessions ?? EMPTY_PLACEHOLDER }}</span>
           </v-col>
         </v-row>
         <template v-if="Object.keys(schoolAgeCareDetail).length">
           <v-row class="mb-2">
             <v-col cols="12" md="6" class="d-flex flex-wrap align-center">
-              <strong class="font-weight-bold mr-2">Before school care offered:</strong>
+              <strong class="mr-2">Before school care offered:</strong>
               <span>{{ getYesNoValue(schoolAgeCareDetail.beforeSchoolCare) }}</span>
             </v-col>
             <v-col cols="12" md="6" class="d-flex flex-wrap align-center">
-              <strong class="font-weight-bold mr-2">After school care offered:</strong>
+              <strong class="mr-2">After school care offered:</strong>
               <span>{{ getYesNoValue(schoolAgeCareDetail.afterSchoolCare) }}</span>
             </v-col>
           </v-row>
           <v-row class="mb-2">
             <v-col cols="12" md="6" class="d-flex flex-wrap align-center">
-              <strong class="font-weight-bold mr-2">Before Kindergarten care offered:</strong>
+              <strong class="mr-2">Before Kindergarten care offered:</strong>
               <span>{{ getYesNoValue(schoolAgeCareDetail.morningKinderCare) }}</span>
             </v-col>
             <v-col cols="12" md="6" class="d-flex flex-wrap align-center">
-              <strong class="font-weight-bold mr-2">After Kindergarten care offered:</strong>
+              <strong class="mr-2">After Kindergarten care offered:</strong>
               <span>{{ getYesNoValue(schoolAgeCareDetail.afterKinderCare) }}</span>
             </v-col>
           </v-row>
@@ -93,9 +93,7 @@
       <div class="box-container">
         <v-row class="mb-2">
           <v-col cols="12" class="d-flex flex-wrap align-center">
-            <strong class="font-weight-bold mr-2">
-              Care before 6:00 AM, after 7:00 PM or overnight service regularly offered:
-            </strong>
+            <strong class="mr-2"> Care before 6:00 AM, after 7:00 PM or overnight service regularly offered: </strong>
             <span>{{ getYesNoValue(licence.licenceExtendedHours) }}</span>
           </v-col>
         </v-row>
