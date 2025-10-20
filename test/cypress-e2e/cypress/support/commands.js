ca6d1f331f302187de7212ba206fa236b3681166
Hedie-cgi <hedie.mahmoudian@cgi.com>
2025-10-20 16:53:01 -0700

    startNewApp pauses to check for correct URLs before continuing with start of app

--- test/cypress-e2e/cypress/support/commands.js
+++ test/cypress-e2e/cypress/support/commands.js
@@ -144,12 +144,13 @@ Cypress.Commands.add('clickByText', (text, selector = 'button') => {
 
 
 Cypress.Commands.add('startNewApp', (input) => {
-  cy.url().should('eq', Cypress.env('PORTAL_BASE_URL')).then(()=> {
-      cy.contains('What would you like to do?').should('be.visible').clickByText('Start Application')
-      cy.contains('p', 'Welcome to Child Care Operating Funding (CCOF)').should('be.visible').clickByText('Start Application')
-      cy.contains('.v-card', `${input}`).should('be.visible').within(()=> {
-        cy.contains('Start Application').click({force: true})
-      })
+  cy.url().should('eq', Cypress.env('PORTAL_BASE_URL'))
+  cy.contains('What would you like to do?').should('be.visible').clickByText('Start Application')
+  cy.url().should('include', '/new-application', {timeout: 10000})
+  cy.contains('p', 'Welcome to Child Care Operating Funding (CCOF)').should('be.visible').clickByText('Start Application')
+  cy.url().should('include', '/select-application-type', {timeout: 10000})
+  cy.contains('.v-card', `${input}`).should('be.visible').within(()=> {
+    cy.contains('Start Application').click({force: true})
   })
 });
 
