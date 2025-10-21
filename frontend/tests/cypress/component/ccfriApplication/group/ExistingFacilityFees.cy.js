6d69ca89738ba7252b41327972389c41b9a86e18
saibannur-cgi <sai.bannur@cgi.com>
2025-10-21 12:05:42 -0700

    refactor existing facility fees tests

--- frontend/tests/cypress/component/ccfriApplication/group/ExistingFacilityFees.cy.js
+++ frontend/tests/cypress/component/ccfriApplication/group/ExistingFacilityFees.cy.js
@@ -2,6 +2,22 @@ import ExistingFacilityFees from '@/components/ccfriApplication/group/ExistingFa
 import vuetify from '@/plugins/vuetify';
 
 const ccfriApplicationId = 1234;
+const curProgramYearId = 91919;
+const previousYearId = 2024;
+const facilityAccountNumber = 434343;
+const facilityId = 59831;
+
+const facilityName = 'TEST_FAC_NAME';
+const licenseNumber = 'LIC-99993';
+
+const getCCFRIStoreState = (overrides = {}) => ({
+  ccfriApp: {
+    ccfriStore: {
+      [ccfriApplicationId]: { facilityId },
+    },
+    ...overrides,
+  },
+});
 
 function mountWithPinia(
   initialState = {},
@@ -40,30 +56,21 @@ describe('<ExistingFacilityFees />', () => {
   });
 
   it('should render facility header', () => {
-    const facilityName = 'TEST_FAC_NAME';
-    const licenseNumber = 'LIC-99993';
-    const facilityAccountNumber = '434343';
     mountWithPinia({
-      ccfriApp: {
-        ccfriStore: {
-          1234: {
-            facilityId: '444',
-          },
-          facilityId: '4444',
-        },
+      ...getCCFRIStoreState({
         previousFeeStore: {
-          '444-2024': 'hello',
+          [`${facilityId}-${previousYearId}`]: 'test123',
         },
-      },
+      }),
       application: {
-        programYearId: '91919',
+        programYearId: curProgramYearId,
       },
       app: {
         programYearList: {
           list: [
             {
-              programYearId: '91919',
-              previousYearId: '2024',
+              programYearId: curProgramYearId,
+              previousYearId,
               name: 'TEST_NAME',
             },
           ],
@@ -79,37 +86,28 @@ describe('<ExistingFacilityFees />', () => {
   });
 
   it('should render `No Fees` message', () => {
-    const facilityName = 'TEST_FAC_NAME';
-    const licenseNumber = 'LIC-99993';
-    const facilityAccountNumber = '434343';
     mountWithPinia({
-      ccfriApp: {
-        ccfriStore: {
-          1234: {
-            facilityId: '444',
-          },
-          facilityId: '4444',
-        },
+      ...getCCFRIStoreState({
         previousFeeStore: {
-          '444-2024': 'hello',
+          [`${facilityId}-${previousYearId}`]: 'test123',
         },
-      },
+      }),
       application: {
-        programYearId: '91919',
+        programYearId: curProgramYearId,
       },
       app: {
         programYearList: {
           list: [
             {
-              programYearId: '91919',
-              previousYearId: '2024',
-              name: '"Program Year 2024 - Overview"',
+              programYearId: curProgramYearId,
+              previousYearId,
+              name: 'Program Year 2024 - Overview',
             },
           ],
         },
       },
       navBar: {
-        userProfileList: [{ ccfriApplicationId, facilityName, licenseNumber, facilityAccountNumber }],
+        userProfileList: [{ ccfriApplicationId }],
       },
     });
     cy.contains('p', `Our records show this facility's parent fees for January 2024 to March 2024`);
@@ -117,22 +115,13 @@ describe('<ExistingFacilityFees />', () => {
   });
 
   it('should render fee list', () => {
-    const facilityName = 'TEST_FAC_NAME';
-    const licenseNumber = 'LIC-99993';
-    const facilityAccountNumber = '434343';
     mountWithPinia({
-      ccfriApp: {
-        ccfriStore: {
-          1234: {
-            facilityId: '444',
-          },
-          facilityId: '4444',
-        },
+      ...getCCFRIStoreState({
         previousFeeStore: {
-          '444-2024': {
+          [`${facilityId}-${previousYearId}`]: {
             childCareTypes: [
               {
-                programYearId: '2024',
+                programYearId: previousYearId,
                 orderNumber: '1',
                 approvedFeeJan: 21,
                 approvedFeeFeb: 44,
@@ -141,23 +130,24 @@ describe('<ExistingFacilityFees />', () => {
             ],
           },
         },
-      },
+      }),
+
       application: {
-        programYearId: '91919',
+        programYearId: curProgramYearId,
       },
       app: {
         programYearList: {
           list: [
             {
-              programYearId: '91919',
-              previousYearId: '2024',
-              name: '"Program Year 2024 - Overview"',
+              programYearId: curProgramYearId,
+              previousYearId,
+              name: 'Program Year 2024 - Overview',
             },
           ],
         },
       },
       navBar: {
-        userProfileList: [{ ccfriApplicationId, facilityName, licenseNumber, facilityAccountNumber }],
+        userProfileList: [{ ccfriApplicationId }],
       },
     });
     cy.contains('h2', 'We have no fees on record for this facility.').should('not.exist');
@@ -175,47 +165,34 @@ describe('<ExistingFacilityFees />', () => {
   });
 
   it('should render radio group', () => {
-    const facilityName = 'TEST_FAC_NAME';
-    const licenseNumber = 'LIC-99993';
-    const facilityAccountNumber = '434343';
     mountWithPinia({
-      ccfriApp: {
-        ccfriStore: {
-          1234: {
-            facilityId: '444',
-          },
-          facilityId: '4444',
-        },
+      ...getCCFRIStoreState({
         previousFeeStore: {
-          '444-2024': {
+          [`${facilityId}-${previousYearId}`]: {
             childCareTypes: [
               {
-                programYearId: '2024',
-                orderNumber: '1',
-                approvedFeeJan: 21,
-                approvedFeeFeb: 44,
-                approvedFeeMar: 88,
+                programYearId: previousYearId,
               },
             ],
           },
         },
-      },
+      }),
       application: {
-        programYearId: '91919',
+        programYearId: curProgramYearId,
       },
       app: {
         programYearList: {
           list: [
             {
-              programYearId: '91919',
-              previousYearId: '2024',
-              name: '"Program Year 2024 - Overview"',
+              programYearId: curProgramYearId,
+              previousYearId,
+              name: 'Program Year 2024 - Overview',
             },
           ],
         },
       },
       navBar: {
-        userProfileList: [{ ccfriApplicationId, facilityName, licenseNumber, facilityAccountNumber }],
+        userProfileList: [{ ccfriApplicationId }],
       },
     });
 
