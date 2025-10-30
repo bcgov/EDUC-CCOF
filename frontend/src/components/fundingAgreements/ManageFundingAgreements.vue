6108a1bef54ff848be9455914a717be627d5c02b
Trevor Richards <trev@trevdev.ca>
2025-10-30 11:38:55 -0700

    feat: improve breakpoints

--- frontend/src/components/fundingAgreements/ManageFundingAgreements.vue
+++ frontend/src/components/fundingAgreements/ManageFundingAgreements.vue
@@ -4,14 +4,14 @@
       <v-col cols="12">
         <p class="mb-4">View and manage the funding agreements for your organization.</p>
       </v-col>
-      <v-col cols="8" sm="7">
+      <v-col cols="12" sm="9">
         <p class="mb-2">
           You must notify the Child Care Operating Funding Program within <strong>two business days</strong> of any
           change to your Facility Licence or Child Care Services outlined in Schedule A of your Child Care Operating
           Funding Agreement.
         </p>
       </v-col>
-      <v-col cols="4" sm="5" class="d-flex justify-end align-end">
+      <v-col cols="12" sm="3" class="d-flex justify-end align-end">
         <div>
           <AppButton size="small" @click="goToChangeRequest"> Request a Change </AppButton>
         </div>
