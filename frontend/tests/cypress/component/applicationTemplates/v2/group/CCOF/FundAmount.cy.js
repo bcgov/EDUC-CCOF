5c210213fa0f88f71da09634324a82978c440069
saibannur-cgi <sai.bannur@cgi.com>
2025-11-10 10:19:08 -0800

    remove dup test code in fundAmount

--- frontend/tests/cypress/component/applicationTemplates/v2/group/CCOF/FundAmount.cy.js
+++ frontend/tests/cypress/component/applicationTemplates/v2/group/CCOF/FundAmount.cy.js
@@ -1,6 +1,17 @@
 import FundAmount from '@/components/applicationTemplates/v2/group/CCOF/FundAmount.vue';
 import vuetify from '@/plugins/vuetify';
 
+function selectExtendedOption() {
+  cy.contains(
+    'label',
+    'Do you regularly offer extended hours of child care (care before 6:00 AM, after 7:00 PM, or overnight service)?',
+  )
+    .closest('.v-radio-group')
+    .within(() => {
+      cy.get('.v-radio').eq(0).click();
+    });
+}
+
 function mountWithPinia(initialState = {}) {
   cy.setupPinia({ initialState, stubActions: false }).then((pinia) => {
     cy.mount(FundAmount, {
@@ -12,9 +23,11 @@ function mountWithPinia(initialState = {}) {
 }
 
 describe('<FundAmount /> -- V2', () => {
+  beforeEach(() => {
+    mountWithPinia();
+  });
   context('Closed Month', () => {
     it('should render full closed months checkboxes if `Yes` to closed month', () => {
-      mountWithPinia();
       cy.contains(
         'label',
         'Are there months when ALL of the programs at this facility are closed for the entire month?',
@@ -30,7 +43,6 @@ describe('<FundAmount /> -- V2', () => {
     });
 
     it('should not render full closed months checkboxes if `No` to closed month', () => {
-      mountWithPinia();
       cy.contains(
         'label',
         'Are there months when ALL of the programs at this facility are closed for the entire month?',
@@ -47,86 +59,61 @@ describe('<FundAmount /> -- V2', () => {
   });
 
   context('License Category', () => {
-    it('should render under 36 months input when checkbox selected', () => {
-      mountWithPinia();
-      cy.contains('label', 'Group Child Care (Under 36 Months)').closest('.v-checkbox').click();
-      cy.contains('label', 'Maximum Number for Group Child Care (Under 36 Months)');
-    });
+    const categories = [
+      {
+        label: 'Group Child Care (Under 36 Months)',
+        maxLabel: 'Maximum Number for Group Child Care (Under 36 Months)',
+      },
+      {
+        label: 'Group Child Care (30 Months to School Age)',
+        maxLabel: 'Maximum Number for Group Child Care (30 Months to School Age)',
+      },
+      {
+        label: 'Group Child Care (School Age)',
+        maxLabel: 'Maximum Number for Group Child Care (School Age)',
+      },
+      {
+        label: 'School Age Care on School Grounds',
+        maxLabel: 'Maximum Number for School Age Care on School Grounds',
+      },
+      {
+        selector: '#preschool-checkbox',
+        label: 'Preschool',
+        maxLabel: 'Maximum Number for Preschool',
+      },
+      {
+        selector: '#multi-age-checkbox',
+        label: 'Multi-Age Child Care',
+        maxLabel: 'Maximum Number for Multi-Age Child Care',
+      },
+    ];
 
-    it('should not render under 36 months input when checkbox unselected', () => {
-      mountWithPinia();
-      cy.contains('label', 'Maximum Number for Group Child Care (Under 36 Months)').should('not.exist');
-    });
+    categories.forEach(({ label, selector, maxLabel }) => {
+      const clickTarget = selector
+        ? () => cy.get(selector).click()
+        : () => cy.contains('label', label).closest('.v-checkbox').click();
 
-    it('should render 30 months to school age input when checkbox selected', () => {
-      mountWithPinia();
-      cy.contains('label', 'Group Child Care (30 Months to School Age)').closest('.v-checkbox').click();
-      cy.contains('label', 'Maximum Number for Group Child Care (30 Months to School Age)');
-    });
+      it(`should render input when "${label}" checkbox selected`, () => {
+        clickTarget();
+        cy.contains('label', maxLabel);
+      });
 
-    it('should not render 30 months to school age input when checkbox unselected', () => {
-      mountWithPinia();
-      cy.contains('label', 'Maximum Number for Group Child Care (30 Months to School Age)').should('not.exist');
-    });
-
-    it('should render school age input when checkbox selected', () => {
-      mountWithPinia();
-      cy.contains('label', 'Group Child Care (School Age)').closest('.v-checkbox').click();
-      cy.contains('label', 'Maximum Number for Group Child Care (School Age)');
-    });
-
-    it('should not render school age input when checkbox unselected', () => {
-      mountWithPinia();
-      cy.contains('label', 'Maximum Number for Group Child Care (School Age)').should('not.exist');
-    });
-
-    it('should render care on school grounds input when checkbox selected', () => {
-      mountWithPinia();
-      cy.contains('label', 'School Age Care on School Grounds').closest('.v-checkbox').click();
-      cy.contains('label', 'Maximum Number for School Age Care on School Grounds');
-    });
-
-    it('should not render care on school grounds input when checkbox unselected', () => {
-      mountWithPinia();
-      cy.contains('label', 'Maximum Number for School Age Care on School Grounds').should('not.exist');
-    });
-
-    it('should render preschool input when checkbox selected', () => {
-      mountWithPinia();
-      cy.get('#preschool-checkbox').click();
-      cy.contains('label', 'Maximum Number for Preschool');
-    });
-
-    it('should not render preschool input when checkbox unselected', () => {
-      mountWithPinia();
-      cy.contains('label', 'Maximum Number for Preschool').should('not.exist');
-    });
-
-    it('should render multi-age input when checkbox selected', () => {
-      mountWithPinia();
-      cy.get('#multi-age-checkbox').click();
-      cy.contains('label', 'Maximum Number for Multi-Age Child Care');
-    });
-
-    it('should not render multi-age input when checkbox unselected', () => {
-      mountWithPinia();
-      cy.contains('label', 'Maximum Number for Multi-Age Child Care').should('not.exist');
+      it(`should not render input when "${label}" checkbox unselected`, () => {
+        cy.contains('label', maxLabel).should('not.exist');
+      });
     });
   });
 
   it('should render licensed capcity input', () => {
-    mountWithPinia();
     cy.get('#multi-age-checkbox').click();
     cy.contains('label', 'Maximum Licensed Capacity');
   });
 
   it('should not render licensed capcity input when no license category', () => {
-    mountWithPinia();
     cy.contains('label', 'Maximum Licensed Capacity').should('not.exist');
   });
 
   it('should render additional preschool sessions when preschool selected as license', () => {
-    mountWithPinia();
     cy.get('#preschool-checkbox').click();
     cy.contains('Please indicate how many preschool sessions your facility offers per day')
       .closest('.v-card')
@@ -142,7 +129,6 @@ describe('<FundAmount /> -- V2', () => {
   });
 
   it('should render additional school age inputs when school age care selected as license', () => {
-    mountWithPinia();
     cy.get('#schoolage-care-checkbox').click();
     cy.contains('Please indicate each service that your facility offers');
     cy.contains('label', 'Before School');
@@ -153,8 +139,6 @@ describe('<FundAmount /> -- V2', () => {
 
   context('Extended Hours', () => {
     it('should render additional inputs if selecting `Yes` for extended hours offering', () => {
-      mountWithPinia();
-
       cy.contains(
         'label',
         'Do you regularly offer extended hours of child care (care before 6:00 AM, after 7:00 PM, or overnight service)?',
@@ -175,8 +159,6 @@ describe('<FundAmount /> -- V2', () => {
     });
 
     it('should not render additional inputs if selecting `No` for extended hours offering', () => {
-      mountWithPinia();
-
       cy.contains(
         'label',
         'Do you regularly offer extended hours of child care (care before 6:00 AM, after 7:00 PM, or overnight service)?',
@@ -192,19 +174,7 @@ describe('<FundAmount /> -- V2', () => {
     });
 
     context('Extended Hours Specific License Category', () => {
-      function selectExtendedOption() {
-        cy.contains(
-          'label',
-          'Do you regularly offer extended hours of child care (care before 6:00 AM, after 7:00 PM, or overnight service)?',
-        )
-          .closest('.v-radio-group')
-          .within(() => {
-            cy.get('.v-radio').eq(0).click();
-          });
-      }
-
       it('should render under 36 months extended inputs when selected', () => {
-        mountWithPinia();
         selectExtendedOption();
 
         cy.get('#under-36months-extendedCC-checkbox').click();
@@ -228,7 +198,6 @@ describe('<FundAmount /> -- V2', () => {
       });
 
       it('should render school age care extended inputs when selected', () => {
-        mountWithPinia();
         selectExtendedOption();
 
         cy.get('#schoolage-care-extendedCC-checkbox').click();
@@ -237,7 +206,6 @@ describe('<FundAmount /> -- V2', () => {
       });
 
       it('should render care on school grounds extended inputs when selected', () => {
-        mountWithPinia();
         selectExtendedOption();
 
         cy.get('#care-on-school-grounds-extendedCC-checkbox').click();
@@ -246,7 +214,6 @@ describe('<FundAmount /> -- V2', () => {
       });
 
       it('should render multi age extended inputs when selected', () => {
-        mountWithPinia();
         selectExtendedOption();
 
         cy.get('#multi-age-extendedCC-checkbox').click();
