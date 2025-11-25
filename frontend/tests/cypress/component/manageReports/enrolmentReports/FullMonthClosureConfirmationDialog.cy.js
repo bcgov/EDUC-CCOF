de42efd39d922ed3a9924b42c6a3c52531b2e2c8
saibannur-cgi <sai.bannur@cgi.com>
2025-11-25 13:38:46 -0800

    CCFRI-6649 - Enrolment Reports Component Tests (#895)
    * added backConfirmationDialog tests
    
    * added tests to SubmitConfirmationDialog
    
    * added FullMonthClosureConfirmationDialog tests
    
    * added enrolment report declaration tests
    
    * add missing test info
    
    * added enrolment report header tests
    
    * added viewEnrolmentReports tests
    
    * initial enrolmentReportForm tests
    
    * finish enrolmentReportForm tests
    
    * fix sonar issues
    
    * decrease viewport
    
    * fix names
    
    * attempt to remove duplication
    
    * address PR comments

---
+++ frontend/tests/cypress/component/manageReports/enrolmentReports/FullMonthClosureConfirmationDialog.cy.js
@@ -0,0 +1,55 @@
+import FullMonthClosureConfirmationDialog from '@/components/manageReports/enrolmentReports/FullMonthClosureConfirmationDialog.vue';
+import vuetify from '@/plugins/vuetify';
+
+function mountWithPinia({ initialState = {}, propOverride = {} } = {}) {
+  cy.setupPinia({ initialState, stubActions: false }).then((pinia) => {
+    const onClose = cy.spy().as('closeDialogSpy');
+    const onProceed = cy.spy().as('proceedDialogSpy');
+    cy.mount(FullMonthClosureConfirmationDialog, {
+      global: {
+        plugins: [pinia, vuetify],
+      },
+      props: {
+        show: true,
+        ...propOverride,
+      },
+      attrs: {
+        onClose,
+        onProceed,
+      },
+    });
+  });
+}
+
+describe('<FullMonthClosureConfirmationDialog />', () => {
+  it('should display default dialog information', () => {
+    mountWithPinia();
+    cy.contains('Confirm Full Month Closure or No Enrolment');
+    cy.contains('You will be redirected to a declaration page to ');
+    cy.contains('If your facility opens or enrolment begins during this month, you must');
+    cy.contains('If your facility is permanently closed, or remains closed');
+  });
+
+  it('should not display dialog', () => {
+    mountWithPinia({ propOverride: { show: false } });
+    cy.contains('Confirm Full Month Closure or No Enrolment').should('not.exist');
+  });
+
+  it('should render `Cancel` and `Proceed` buttons', () => {
+    mountWithPinia();
+    cy.contains('button', 'Cancel');
+    cy.contains('button', 'Proceed');
+  });
+
+  it('should emit `proceed` event when clicking `Proceed`', () => {
+    mountWithPinia();
+    cy.contains('button', 'Proceed').click();
+    cy.get('@proceedDialogSpy').should('have.been.calledOnce');
+  });
+
+  it('should emit `close` event when clicking `Cancel`', () => {
+    mountWithPinia();
+    cy.contains('button', 'Cancel').click();
+    cy.get('@closeDialogSpy').should('have.been.calledOnce');
+  });
+});
