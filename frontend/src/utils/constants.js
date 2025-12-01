02f314e895cef6bbc7882821fa32e589a519107a
Le <vietdung.le@cgi.com>
2025-12-01 12:03:16 -0800

    ccfri-6752 - fix missing osscg ossck categories in closures and error router in CCFRI summar

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -482,8 +482,8 @@ export const CLOSURE_AFFECTED_AGE_GROUPS_VALUES_TO_TEXT = Object.freeze({
   100000000: '0 to 18 months',
   100000001: '18 to 36 months',
   100000002: '3 Years to Kindergarten',
-  100000003: 'Out of School Care - Kindergarten',
-  100000004: 'Out of School Care - Grade 1+',
+  100000003: 'Kindergarten',
+  100000004: 'Grade 1 to Age 12',
   100000005: 'Preschool',
 });
 
@@ -491,9 +491,14 @@ export const CLOSURE_AFFECTED_AGE_GROUPS = Object.freeze({
   '0 to 18 months': 100000000,
   '18 to 36 months': 100000001,
   '3 Years to Kindergarten': 100000002,
-  'Out of School Care - Kindergarten': 100000003,
-  'Out of School Care - Grade 1+': 100000004,
+  Kindergarten: 100000003,
+  'Grade 1 to Age 12': 100000004,
   Preschool: 100000005,
+});
+
+export const OLD_TO_NEW_CC_CATEGORY_LABEL_MAP = Object.freeze({
+  'Out of School Care - Kindergarten': 'Kindergarten',
+  'Out of School Care - Grade 1+': 'Grade 1 to Age 12',
 });
 
 export const CLOSURE_TYPES = Object.freeze({
