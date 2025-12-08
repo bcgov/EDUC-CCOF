3c93554e2e35a39acacd9e5752f7e058fde8daf0
Le <vietdung.le@cgi.com>
2025-12-08 11:30:00 -0800

    ccfri-6350 - filter out cancelled and draft licence

--- backend/src/components/licence.js
+++ backend/src/components/licence.js
@@ -1,6 +1,7 @@
 'use strict';
 const { getOperation, buildFilterQuery } = require('./utils');
 const { restrictFacilities } = require('../util/common');
+const { LICENCE_STATUS_CODES } = require('../util/constants');
 const { MappableObjectForFront } = require('../util/mapping/MappableObject');
 const { LicenceMappings, ServiceDeliveryMappings } = require('../util/mapping/Mappings');
 const HttpStatus = require('http-status-codes');
@@ -12,7 +13,7 @@ async function getLicencesByQuery(req, res) {
       'ccof_licenses?$select=_ccof_facility_value,ccof_facility_id,ccof_licenseid,ccof_name,ccof_organization,ccof_start_date,ccof_end_date,statuscode,ccof_record_start_date,ccof_record_end_date,ccof_maximum_capacity,ccof_maximum_days_per_week,ccof_maximum_weeks_per_year,ccof_extended_hours_offered,ccof_extended_days_per_week,ccof_extended_weeks_per_year' +
       '&$expand=ccof_service_delivery_details_license_ccof_license($select=ccof_after_school,ccof_before_school,ccof_afternoon_kindercare,ccof_morning_kindercare,' +
       'ccof_care_type,ccof_licenced_spaces,_ccof_license_categories_lookup_value,ccof_max_4_or_less,ccof_max_over_4,ccof_number_of_preschool_sessions)' +
-      `&${buildFilterQuery(req.query, LicenceMappings)}`;
+      `&${buildFilterQuery(req.query, LicenceMappings)} and statuscode ne ${LICENCE_STATUS_CODES.DRAFT} and statuscode ne ${LICENCE_STATUS_CODES.CANCELLED}`;
     const response = await getOperation(operation);
     let licences = response.value.map((item) => ({
       ...new MappableObjectForFront(item, LicenceMappings).toJSON(),
@@ -32,7 +33,7 @@ async function getLicencesByFacilityId(req, res) {
       'ccof_licenses?$select=ccof_end_date,_ccof_facility_value,ccof_licenseid,ccof_name,ccof_organization,ccof_start_date,statuscode,ccof_record_start_date,ccof_record_end_date,ccof_maximum_capacity,ccof_maximum_days_per_week,ccof_maximum_weeks_per_year,ccof_extended_hours_offered,ccof_extended_days_per_week,ccof_extended_weeks_per_year' +
       '&$expand=ccof_service_delivery_details_license_ccof_license($select=ccof_after_school,ccof_before_school,ccof_afternoon_kindercare,ccof_morning_kindercare,' +
       'ccof_care_type,ccof_licenced_spaces,_ccof_license_categories_lookup_value,ccof_max_4_or_less,ccof_max_over_4,ccof_number_of_preschool_sessions)' +
-      `&$filter=(_ccof_facility_value eq ${req.query.facilityId} and statuscode ne 100000001 and statuscode ne 101510003)`;
+      `&$filter=(_ccof_facility_value eq ${req.query.facilityId} and statuscode ne ${LICENCE_STATUS_CODES.DRAFT} and statuscode ne ${LICENCE_STATUS_CODES.CANCELLED})`;
     const response = await getOperation(operation);
     const licence = response.value.map((item) => ({
       ...new MappableObjectForFront(item, LicenceMappings).toJSON(),
@@ -52,7 +53,7 @@ async function getLicencesByFundingAgreementId(req, res) {
       '&$expand=ccof_Funding_Agreement_ccof_license_ccof_license' +
       '($select=ccof_end_date,ccof_extended_days_per_week,ccof_extended_hours_offered,ccof_extended_weeks_per_year,ccof_facility_id,ccof_maximum_days_per_week,ccof_maximum_weeks_per_year,' +
       'ccof_name,ccof_record_end_date,ccof_record_start_date,ccof_start_date,_ccof_facility_value,ccof_licenseid,ccof_maximum_capacity,ccof_organization,statuscode;' +
-      '$filter=(statecode eq 0 and statuscode ne 100000001))' +
+      `$filter=(statecode eq 0 and statuscode ne ${LICENCE_STATUS_CODES.DRAFT}))` +
       `&$filter=(ccof_funding_agreementid eq ${req.query.fundingAgreementId})`;
     const response = await getOperation(operation);
     let licences = [];
