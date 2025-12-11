89ea3a08d41c7b6244aaadceab9b5f687d7c4e51
Le <vietdung.le@cgi.com>
2025-12-10 16:10:55 -0800

    ccfri-6350 - update after code review

--- backend/src/components/licence.js
+++ backend/src/components/licence.js
@@ -27,25 +27,6 @@ async function getLicencesByQuery(req, res) {
   }
 }
 
-async function getLicencesByFacilityId(req, res) {
-  try {
-    const operation =
-      'ccof_licenses?$select=ccof_end_date,_ccof_facility_value,ccof_licenseid,ccof_name,ccof_organization,ccof_start_date,statuscode,ccof_record_start_date,ccof_record_end_date,ccof_maximum_capacity,ccof_maximum_days_per_week,ccof_maximum_weeks_per_year,ccof_extended_hours_offered,ccof_extended_days_per_week,ccof_extended_weeks_per_year' +
-      '&$expand=ccof_service_delivery_details_license_ccof_license($select=ccof_after_school,ccof_before_school,ccof_afternoon_kindercare,ccof_morning_kindercare,' +
-      'ccof_care_type,ccof_licenced_spaces,_ccof_license_categories_lookup_value,ccof_max_4_or_less,ccof_max_over_4,ccof_number_of_preschool_sessions)' +
-      `&$filter=(_ccof_facility_value eq ${req.query.facilityId} and statuscode ne ${LICENCE_STATUS_CODES.DRAFT} and statuscode ne ${LICENCE_STATUS_CODES.CANCELLED})`;
-    const response = await getOperation(operation);
-    const licence = response.value.map((item) => ({
-      ...new MappableObjectForFront(item, LicenceMappings).toJSON(),
-      serviceDeliveryDetails: item.ccof_service_delivery_details_license_ccof_license.map((detail) => new MappableObjectForFront(detail, ServiceDeliveryMappings).toJSON()),
-    }));
-    return res.status(HttpStatus.OK).json(licence);
-  } catch (e) {
-    log.error(e);
-    return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(e.data ? e.data : e?.status);
-  }
-}
-
 async function getLicencesByFundingAgreementId(req, res) {
   try {
     const operation =
@@ -90,15 +71,10 @@ async function getRawServiceDetails(licenceId) {
 
 async function getLicences(req, res) {
   try {
-    if (req.query.useCustomQuery) {
-      return await getLicencesByQuery(req, res);
-    }
-    const { facilityId, fundingAgreementId } = req.query;
-    if (facilityId) {
-      return await getLicencesByFacilityId(req, res);
-    } else if (fundingAgreementId) {
+    if (req.query.fundingAgreementId) {
       return await getLicencesByFundingAgreementId(req, res);
     }
+    return await getLicencesByQuery(req, res);
   } catch (e) {
     log.error(e);
     return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(e.data ? e.data : e?.status);
