fd8dab49d00eada733634368c4b084a0fdf79eba
Bhat <poornima.bhat@cgi.com>
2026-01-19 19:53:01 -0800

    CCFRI 7127 Add ECE Staff certificate status

--- frontend/src/components/eceStaff/ECEStaffCertificationDialog.vue
+++ frontend/src/components/eceStaff/ECEStaffCertificationDialog.vue
@@ -2,15 +2,24 @@
   <AppDialog v-model="dialogOpen" title="Staff Certification Details" max-width="800px" @close="closeDialog">
     <template #content>
       <v-row class="font-weight-bold mb-2">
-        <v-col cols="4">Certifications</v-col>
-        <v-col cols="4">Effective Start Date</v-col>
-        <v-col cols="4">Effective End Date</v-col>
+        <v-col cols="3">Certifications</v-col>
+        <v-col cols="3">Effective Start Date</v-col>
+        <v-col cols="3">Effective End Date</v-col>
+        <v-col cols="2">Status</v-col>
       </v-row>
 
       <v-row v-for="(cert, index) in staff?.certificates" :key="index" align="center">
-        <v-col cols="4">{{ cert.certificateLevel || EMPTY_PLACEHOLDER }}</v-col>
-        <v-col cols="4">{{ cert.effectiveStartDate || EMPTY_PLACEHOLDER }}</v-col>
-        <v-col cols="4">{{ cert.effectiveEndDate || EMPTY_PLACEHOLDER }}</v-col>
+        <v-col cols="3">{{ cert.certificateLevel || EMPTY_PLACEHOLDER }}</v-col>
+        <v-col cols="3">{{ cert.effectiveStartDate || EMPTY_PLACEHOLDER }}</v-col>
+        <v-col cols="3">{{ cert.effectiveEndDate || EMPTY_PLACEHOLDER }}</v-col>
+        <v-col cols="2">
+          <span
+            v-if="cert.certStatus === ECE_STAFF_CERT_STATUSES.EXPIRED"
+            :class="getECECertStatusClass(cert.certStatus)"
+          >
+            {{ cert.certStatus }}
+          </span>
+        </v-col>
       </v-row>
 
       <v-row v-if="!staff.certificates?.length" align="center">
@@ -22,7 +31,8 @@
 
 <script>
 import AppDialog from '@/components/guiComponents/AppDialog.vue';
-import { EMPTY_PLACEHOLDER } from '@/utils/constants.js';
+import { getECECertStatusClass } from '@/utils/common.js';
+import { ECE_STAFF_CERT_STATUSES, EMPTY_PLACEHOLDER } from '@/utils/constants.js';
 
 export default {
   name: 'ECEStaffCertificationDialog',
@@ -50,8 +60,10 @@ export default {
   },
   created() {
     this.EMPTY_PLACEHOLDER = EMPTY_PLACEHOLDER;
+    this.ECE_STAFF_CERT_STATUSES = ECE_STAFF_CERT_STATUSES;
   },
   methods: {
+    getECECertStatusClass,
     closeDialog() {
       this.dialogOpen = false;
     },
