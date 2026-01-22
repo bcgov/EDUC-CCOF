5fbc93803a7d9c893cc07b2c30cdd2cd878326bd
Trevor Richards <trev@trevdev.ca>
2026-01-22 09:27:05 -0800

    chore: use camel case for JS interface

--- backend/src/util/redis/estimator-cache-helper.js
+++ backend/src/util/redis/estimator-cache-helper.js
@@ -11,13 +11,13 @@ const { v4: uuidv4 } = require('uuid');
 const cacheHelper = {
   async getGuidE(guid) {
     if (Redis.isReady) {
-      const retVal = await Redis.client.HGET(GUID_E, guid);
+      const retVal = await Redis.client.hGet(GUID_E, guid);
       if (retVal) {
         return retVal;
       } else {
         const id = uuidv4();
-        await Redis.client.HSET(GUID_E, guid, id);
-        await Redis.client.HSET(GUID_D, id, guid);
+        await Redis.client.hSet(GUID_E, guid, id);
+        await Redis.client.hSet(GUID_D, id, guid);
         log.verbose(`GUIDE not found,setting for ${guid} returning ${id}`);
         return id;
       }
@@ -27,7 +27,7 @@ const cacheHelper = {
   },
   async getGuidD(guid) {
     if (Redis.isReady) {
-      const retVal = await Redis.client.HGET(GUID_D, guid);
+      const retVal = await Redis.client.hGet(GUID_D, guid);
       return retVal;
     } else {
       log.error('Redis client is not available, this should not have happened');
@@ -41,7 +41,7 @@ const cacheHelper = {
         await Redis.client.expire(FACILITY_D, facilityTTL, 'NX');
       }
 
-      const retVal = await Redis.client.HGET(FACILITY_D, guidd);
+      const retVal = await Redis.client.hGet(FACILITY_D, guidd);
       if (retVal) {
         log.verbose(`found facilityd for guidd[${guidd}], with data: ${retVal}`);
         return JSON.parse(retVal);
@@ -54,7 +54,7 @@ const cacheHelper = {
   },
   async setFacility(guidd, facility) {
     if (Redis.isReady) {
-      await Redis.client.HSET(FACILITY_D, guidd, JSON.stringify(facility));
+      await Redis.client.hSet(FACILITY_D, guidd, JSON.stringify(facility));
     } else {
       log.error('Redis client is not available, this should not have happened');
     }
