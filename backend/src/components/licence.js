f9b32f7f6ee7d56c7212bf5662a77fcf4e858ed6
Poornima-cgi <Poornima.Poornima@gov.bc.ca>
2026-01-28 15:54:17 -0800

    CCFRI 7248 : Replaced Licenced spaces with Max Capacity (#993)
    Co-authored-by: Bhat <poornima.bhat@cgi.com>

--- backend/src/components/licence.js
+++ backend/src/components/licence.js
@@ -12,7 +12,7 @@ async function getLicencesByQuery(req, res) {
     const operation =
       'ccof_licenses?$select=_ccof_facility_value,ccof_facility_id,ccof_licenseid,ccof_name,ccof_organization,ccof_start_date,ccof_end_date,statuscode,ccof_record_start_date,ccof_record_end_date,ccof_maximum_capacity,ccof_maximum_days_per_week,ccof_maximum_weeks_per_year,ccof_extended_hours_offered,ccof_extended_days_per_week,ccof_extended_weeks_per_year' +
       '&$expand=ccof_service_delivery_details_license_ccof_license($select=ccof_after_school,ccof_before_school,ccof_afternoon_kindercare,ccof_morning_kindercare,' +
-      'ccof_care_type,ccof_licenced_spaces,_ccof_license_categories_lookup_value,ccof_max_4_or_less,ccof_max_over_4,ccof_number_of_preschool_sessions)' +
+      'ccof_care_type,ccof_max_capacity,_ccof_license_categories_lookup_value,ccof_max_4_or_less,ccof_max_over_4,ccof_number_of_preschool_sessions)' +
       `&${buildFilterQuery(req.query, LicenceMappings)} and statuscode ne ${LICENCE_STATUS_CODES.DRAFT} and statuscode ne ${LICENCE_STATUS_CODES.CANCELLED}`;
     const response = await getOperation(operation);
     let licences = response.value.map((item) => ({
@@ -58,7 +58,7 @@ async function getRawServiceDetails(licenceId) {
   const operation =
     'ccof_service_delivery_detailses?' +
     '$select=ccof_after_school,ccof_afternoon_kindercare,ccof_before_school,ccof_extended_hours_offered,ccof_morning_kindercare,' +
-    'ccof_licenced_spaces,_ccof_license_categories_lookup_value,ccof_number_of_preschool_sessions,ccof_max_4_or_less,ccof_max_over_4,_ccof_license_value' +
+    'ccof_max_capacity,_ccof_license_categories_lookup_value,ccof_number_of_preschool_sessions,ccof_max_4_or_less,ccof_max_over_4,_ccof_license_value' +
     '&$expand=ccof_license($select=_ccof_facility_value,ccof_facility_id;' +
     '$expand=ccof_facility($select=accountid,name))' +
     `&$filter=(_ccof_license_value eq '${licenceId}')`;
