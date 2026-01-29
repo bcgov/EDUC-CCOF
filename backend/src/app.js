c0f172412888324caf84ba5fb990e0086d36d71e
Trevor Richards <trev@trevdev.ca>
2026-01-28 16:41:17 -0800

    fix: async with Redis.init & related configurations
    We're using CommonJS for the app.js file. I could either refactor this
    project to use ESM, or wrap the whole express app setup in the
    callback for when Redis is ready to go.
    
    It's not as if this app works without Redis at this point, anyway.

--- backend/src/app.js
+++ backend/src/app.js
@@ -64,261 +64,251 @@ const { isFacilityAdmin } = require('./util/common');
 //initialize app
 const app = express();
 
-app.set('trust proxy', 1);
-//sets security measures (headers, etc)
-app.use(cors());
-app.use(helmet());
-app.use(noCache());
-app.use(
-  promMid({
-    metricsPath: '/api/prometheus',
-    collectDefaultMetrics: true,
-    requestDurationBuckets: [0.1, 0.5, 1, 1.5],
-  }),
-);
-//tells the app to use json as means of transporting data
-app.use(bodyParser.json({ limit: '50mb', extended: true }));
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-    limit: '50mb',
-  }),
-);
-
-const logStream = {
-  write: (message) => {
-    log.info(message);
-  },
-};
-
-function getRedisDbSession() {
-  if (config.get('redis:use') == 'true') {
-    Redis.init();
-    const dbSession = new ConnectRedis({
-      client: Redis.client,
-      prefix: 'ccof-sess:',
-    });
-    return dbSession;
-  }
-  return undefined;
-}
-
-const dbSession = getRedisDbSession();
-const cookie = {
-  secure: true,
-  httpOnly: true,
-  maxAge: 1800000, //30 minutes in ms. this is same as session time. DO NOT MODIFY, IF MODIFIED, MAKE SURE SAME AS SESSION TIME OUT VALUE.
-};
-if ('local' === config.get('environment')) {
-  cookie.secure = false;
-}
-//sets cookies for security purposes (prevent cookie access, allow secure connections only, etc)
-app.use(
-  session({
-    name: 'ccof_cookie',
-    secret: config.get('oidc:clientSecret'),
-    resave: false,
-    saveUninitialized: true,
-    cookie: cookie,
-    store: dbSession,
-  }),
-);
-
-app.use(require('./routes/health-check').router);
-//initialize routing and session. Cookies are now only reachable via requests (not js)
-app.use(passport.initialize());
-app.use(passport.session());
-
-function addLoginPassportUse(discovery, strategyName, callbackURI, kc_idp_hint, clientId, clientSecret) {
-  passport.use(
-    strategyName,
-    new OidcStrategy(
-      {
-        issuer: discovery.issuer,
-        authorizationURL: discovery.authorization_endpoint,
-        tokenURL: discovery.token_endpoint,
-        userInfoURL: discovery.userinfo_endpoint,
-        clientID: config.get(clientId),
-        clientSecret: config.get(clientSecret),
-        callbackURL: callbackURI,
-        scope: 'openid',
-        kc_idp_hint: kc_idp_hint,
-      },
-      async (_issuer, profile, _context, idToken, accessToken, refreshToken, verified) => {
-        if (typeof accessToken === 'undefined' || accessToken === null || typeof refreshToken === 'undefined' || refreshToken === null) {
-          return verified('No access token', null);
-        }
-        //set access and refresh tokens
-        profile.jwtFrontend = auth.generateUiToken();
-        profile.jwt = accessToken;
-        profile._json = parseJwt(accessToken);
-        profile.refreshToken = refreshToken;
-        profile.idToken = idToken;
-
-        // Store additional information on the profile to enable role/permission/statecode validation
-        await populateUserInfo(profile);
-        return verified(null, profile);
-      },
-    ),
+Redis.init().then(() => {
+  app.set('trust proxy', 1);
+  //sets security measures (headers, etc)
+  app.use(cors());
+  app.use(helmet());
+  app.use(noCache());
+  app.use(
+    promMid({
+      metricsPath: '/api/prometheus',
+      collectDefaultMetrics: true,
+      requestDurationBuckets: [0.1, 0.5, 1, 1.5],
+    }),
   );
-}
+  //tells the app to use json as means of transporting data
+  app.use(bodyParser.json({ limit: '50mb', extended: true }));
+  app.use(
+    bodyParser.urlencoded({
+      extended: true,
+      limit: '50mb',
+    }),
+  );
 
-async function populateUserInfo(profile) {
-  const { guid, identity_provider } = profile._json;
+  const logStream = {
+    write: (message) => {
+      log.info(message);
+    },
+  };
 
-  // Get UserProfile for BCeID users
-  if (identity_provider === config.get('oidc:idpHintBceid')) {
-    // If the userGuid cannot be found in Dynamics, then Dynamics will check if the userName exists,
-    // If userName exists but has a null userGuid, the system will update the user record with the GUID and return that user profile.
-    // In CCOF this would only happen for new users added through the portal
-    const user = await getUserProfile(guid, profile._json.bceid_username);
+  const cookie = {
+    secure: true,
+    httpOnly: true,
+    maxAge: 1800000, //30 minutes in ms. this is same as session time. DO NOT MODIFY, IF MODIFIED, MAKE SURE SAME AS SESSION TIME OUT VALUE.
+  };
 
-    if (!isEmpty(user)) {
-      profile.contactId = user.contactid;
-      profile.organizationId = user.organization_accountid;
-      if (user.portalRole) {
-        profile.role = new MappableObjectForFront(user.portalRole, RoleMappings).data;
-      }
-      profile.statecode = user.statecode;
+  if ('local' === config.get('environment')) {
+    cookie.secure = false;
+  }
 
-      // Add facilities for Facility Admin users
-      if (isFacilityAdmin(profile)) {
-        const facilities = await getRawContactFacilities(profile.contactId);
-        profile.facilities = facilities.map((f) => ({ facilityId: f.facilityId }));
-      }
-    } else {
-      // If the user is not found in Dynamics at all, assign the default Organization Admin role
-      profile.role = {
-        roleNumber: ROLES.ORG_ADMIN,
+  //sets cookies for security purposes (prevent cookie access, allow secure connections only, etc)
+  app.use(
+    session({
+      name: 'ccof_cookie',
+      secret: config.get('oidc:clientSecret'),
+      resave: false,
+      saveUninitialized: true,
+      cookie: cookie,
+      store: new ConnectRedis({ client: Redis.client, prefix: 'ccof-sess:' }),
+    }),
+  );
+
+  const createRateLimitRedisConfig = () => {
+    if (Redis.clustered) {
+      return {
+        sendCommandCluster: ({ key, isReadOnly, command }) => Redis.client.sendCommand(key, isReadOnly, command),
       };
     }
-  }
-  // IDIR users get role information when impersonating, not on login
-}
+    return { sendCommand: (...args) => Redis.client.sendCommand(args) };
+  };
 
-const parseJwt = (token) => {
-  try {
-    return JSON.parse(atob(token.split('.')[1]));
-  } catch {
-    return null;
-  }
-};
-//initialize our authentication strategy
-utils.getOidcDiscovery().then((discovery) => {
-  //OIDC Strategy is used for authorization
-  addLoginPassportUse(discovery, 'oidcBceid', config.get('server:frontend') + '/api/auth/callback', config.get('oidc:idpHintBceid'), 'oidc:clientId', 'oidc:clientSecret');
-  addLoginPassportUse(discovery, 'oidcIdir', config.get('server:frontend') + '/api/auth/callback_idir', config.get('oidc:idpHintIdir'), 'oidc:clientId', 'oidc:clientSecret');
+  // Setup Rate limit for the number of frontend requests allowed per windowMs to avoid DDOS attack
+  const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    store: Redis.isReady ? new RateLimitRedis(createRateLimitRedisConfig()) : undefined,
+  });
 
-  //JWT strategy is used for authorization  keycloak_bcdevexchange_idir
-  passport.use(
-    'jwt',
-    new JWTStrategy(
-      {
-        algorithms: ['RS256'],
-        // Keycloak 7.3.0 no longer automatically supplies matching client_id audience.
-        // If audience checking is needed, check the following SO to update Keycloak first.
-        // Ref: https://stackoverflow.com/a/53627747
-        audience: config.get('server:frontend'),
-        issuer: config.get('tokenGenerate:issuer'),
-        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-        secretOrKey: config.get('tokenGenerate:publicKey'),
-        ignoreExpiration: true,
-      },
-      (jwtPayload, done) => {
-        if (typeof jwtPayload === 'undefined' || jwtPayload === null) {
-          return done('No JWT token', null);
+  app.use('/api/canadaPost', limiter);
+
+  app.use(require('./routes/health-check').router);
+  //initialize routing and session. Cookies are now only reachable via requests (not js)
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  function addLoginPassportUse(discovery, strategyName, callbackURI, kc_idp_hint, clientId, clientSecret) {
+    passport.use(
+      strategyName,
+      new OidcStrategy(
+        {
+          issuer: discovery.issuer,
+          authorizationURL: discovery.authorization_endpoint,
+          tokenURL: discovery.token_endpoint,
+          userInfoURL: discovery.userinfo_endpoint,
+          clientID: config.get(clientId),
+          clientSecret: config.get(clientSecret),
+          callbackURL: callbackURI,
+          scope: 'openid',
+          kc_idp_hint: kc_idp_hint,
+        },
+        async (_issuer, profile, _context, idToken, accessToken, refreshToken, verified) => {
+          if (typeof accessToken === 'undefined' || accessToken === null || typeof refreshToken === 'undefined' || refreshToken === null) {
+            return verified('No access token', null);
+          }
+          //set access and refresh tokens
+          profile.jwtFrontend = auth.generateUiToken();
+          profile.jwt = accessToken;
+          profile._json = parseJwt(accessToken);
+          profile.refreshToken = refreshToken;
+          profile.idToken = idToken;
+
+          // Store additional information on the profile to enable role/permission/statecode validation
+          await populateUserInfo(profile);
+          return verified(null, profile);
+        },
+      ),
+    );
+  }
+
+  async function populateUserInfo(profile) {
+    const { guid, identity_provider } = profile._json;
+
+    // Get UserProfile for BCeID users
+    if (identity_provider === config.get('oidc:idpHintBceid')) {
+      // If the userGuid cannot be found in Dynamics, then Dynamics will check if the userName exists,
+      // If userName exists but has a null userGuid, the system will update the user record with the GUID and return that user profile.
+      // In CCOF this would only happen for new users added through the portal
+      const user = await getUserProfile(guid, profile._json.bceid_username);
+
+      if (!isEmpty(user)) {
+        profile.contactId = user.contactid;
+        profile.organizationId = user.organization_accountid;
+        if (user.portalRole) {
+          profile.role = new MappableObjectForFront(user.portalRole, RoleMappings).data;
         }
+        profile.statecode = user.statecode;
 
-        done(null, {
-          email: jwtPayload.email,
-          familyName: jwtPayload.family_name,
-          givenName: jwtPayload.given_name,
-          jwt: jwtPayload,
-          name: jwtPayload.name,
-          user_guid: jwtPayload.user_guid,
-          realmRole: jwtPayload.realm_role,
-        });
-      },
-    ),
-  );
-});
-
-//functions for serializing/deserializing users
-passport.serializeUser((user, next) => next(null, user));
-passport.deserializeUser((obj, next) => next(null, obj));
-
-const createRateLimitRedisConfig = () => {
-  if (Redis.isReady) return;
-  if (Redis.clustered) {
-    return {
-      sendCommandCluster: ({ key, isReadOnly, command }) => Redis.client.sendCommand(key, isReadOnly, command),
-    };
+        // Add facilities for Facility Admin users
+        if (isFacilityAdmin(profile)) {
+          const facilities = await getRawContactFacilities(profile.contactId);
+          profile.facilities = facilities.map((f) => ({ facilityId: f.facilityId }));
+        }
+      } else {
+        // If the user is not found in Dynamics at all, assign the default Organization Admin role
+        profile.role = {
+          roleNumber: ROLES.ORG_ADMIN,
+        };
+      }
+    }
+    // IDIR users get role information when impersonating, not on login
   }
-  return { sendCommand: (...args) => Redis.client.sendCommand(args) };
-};
 
-// Setup Rate limit for the number of frontend requests allowed per windowMs to avoid DDOS attack
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-  store: new RateLimitRedis(createRateLimitRedisConfig()),
-});
+  const parseJwt = (token) => {
+    try {
+      return JSON.parse(atob(token.split('.')[1]));
+    } catch {
+      return null;
+    }
+  };
+  //initialize our authentication strategy
+  utils.getOidcDiscovery().then((discovery) => {
+    //OIDC Strategy is used for authorization
+    addLoginPassportUse(discovery, 'oidcBceid', config.get('server:frontend') + '/api/auth/callback', config.get('oidc:idpHintBceid'), 'oidc:clientId', 'oidc:clientSecret');
+    addLoginPassportUse(discovery, 'oidcIdir', config.get('server:frontend') + '/api/auth/callback_idir', config.get('oidc:idpHintIdir'), 'oidc:clientId', 'oidc:clientSecret');
 
-app.use('/api/canadaPost', limiter);
+    //JWT strategy is used for authorization  keycloak_bcdevexchange_idir
+    passport.use(
+      'jwt',
+      new JWTStrategy(
+        {
+          algorithms: ['RS256'],
+          // Keycloak 7.3.0 no longer automatically supplies matching client_id audience.
+          // If audience checking is needed, check the following SO to update Keycloak first.
+          // Ref: https://stackoverflow.com/a/53627747
+          audience: config.get('server:frontend'),
+          issuer: config.get('tokenGenerate:issuer'),
+          jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+          secretOrKey: config.get('tokenGenerate:publicKey'),
+          ignoreExpiration: true,
+        },
+        (jwtPayload, done) => {
+          if (typeof jwtPayload === 'undefined' || jwtPayload === null) {
+            return done('No JWT token', null);
+          }
 
-app.use(morgan(config.get('server:morganFormat'), { stream: logStream }));
-//set up routing to auth and main API
-app.use(/(\/api)?/, apiRouter);
+          done(null, {
+            email: jwtPayload.email,
+            familyName: jwtPayload.family_name,
+            givenName: jwtPayload.given_name,
+            jwt: jwtPayload,
+            name: jwtPayload.name,
+            user_guid: jwtPayload.user_guid,
+            realmRole: jwtPayload.realm_role,
+          });
+        },
+      ),
+    );
+  });
 
-apiRouter.use('/auth', authRouter);
-apiRouter.use('/user', userRouter);
-apiRouter.use('/facility', facilityRouter);
-apiRouter.use('/organization', organizationRouter);
-apiRouter.use('/public', publicRouter);
-apiRouter.use('/config', configRouter);
-apiRouter.use('/application', applicationRouter);
-apiRouter.use('/document', documentRouter);
-apiRouter.use('/group/funding', fundingRouter);
-apiRouter.use('/messages', messageRouter);
-apiRouter.use('/licenseUpload', licenseUploadRouter);
-apiRouter.use('/supportingDocument', supportingDocumentUploadRouter);
-apiRouter.use('/changeRequest', changeRequestRouter);
-apiRouter.use('/pdf', pdfRouter);
-apiRouter.use('/canadaPost', canadaPostRouter);
-apiRouter.use('/closures', closureRouter);
-apiRouter.use('/fundingAgreements', fundingAgreementRouter);
-apiRouter.use('/enrolmentReports', enrolmentReportRouter);
-apiRouter.use('/licences', licenceRouter);
-apiRouter.use('/contacts', contactRouter);
-apiRouter.use('/programsVacancies', programsVacanciesRouter);
-apiRouter.use('/payments', paymentRouter);
-apiRouter.use('/eceReports', eceReportRouter);
-apiRouter.use('/eceStaff', eceStaffRouter);
+  //functions for serializing/deserializing users
+  passport.serializeUser((user, next) => next(null, user));
+  passport.deserializeUser((obj, next) => next(null, obj));
 
-//Handle 500 error
-app.use((err, _req, res, next) => {
-  //This is from the ResultValidation
-  if (err.errors && err.mapped) {
-    return res.status(400).json({
-      errors: err.mapped(),
-    });
-  }
-  log.error(err?.stack);
-  res?.redirect(config?.get('server:frontend') + '/error?message=500_internal_error');
-  next();
-});
+  app.use(morgan(config.get('server:morganFormat'), { stream: logStream }));
+  //set up routing to auth and main API
+  app.use(/(\/api)?/, apiRouter);
 
-// Handle 404 error
-app.use((_req, res) => {
-  log.error('404 Error');
-  res.redirect(config?.get('server:frontend') + '/error?message=404_Page_Not_Found');
-});
+  apiRouter.use('/auth', authRouter);
+  apiRouter.use('/user', userRouter);
+  apiRouter.use('/facility', facilityRouter);
+  apiRouter.use('/organization', organizationRouter);
+  apiRouter.use('/public', publicRouter);
+  apiRouter.use('/config', configRouter);
+  apiRouter.use('/application', applicationRouter);
+  apiRouter.use('/document', documentRouter);
+  apiRouter.use('/group/funding', fundingRouter);
+  apiRouter.use('/messages', messageRouter);
+  apiRouter.use('/licenseUpload', licenseUploadRouter);
+  apiRouter.use('/supportingDocument', supportingDocumentUploadRouter);
+  apiRouter.use('/changeRequest', changeRequestRouter);
+  apiRouter.use('/pdf', pdfRouter);
+  apiRouter.use('/canadaPost', canadaPostRouter);
+  apiRouter.use('/closures', closureRouter);
+  apiRouter.use('/fundingAgreements', fundingAgreementRouter);
+  apiRouter.use('/enrolmentReports', enrolmentReportRouter);
+  apiRouter.use('/licences', licenceRouter);
+  apiRouter.use('/contacts', contactRouter);
+  apiRouter.use('/programsVacancies', programsVacanciesRouter);
+  apiRouter.use('/payments', paymentRouter);
+  apiRouter.use('/eceReports', eceReportRouter);
+  apiRouter.use('/eceStaff', eceStaffRouter);
 
-// Prevent unhandled errors from crashing application
-process.on('unhandledRejection', (err) => {
-  log.error('Unhandled Rejection at:', err?.stack || err);
+  //Handle 500 error
+  app.use((err, _req, res, next) => {
+    //This is from the ResultValidation
+    if (err.errors && err.mapped) {
+      return res.status(400).json({
+        errors: err.mapped(),
+      });
+    }
+    log.error(err?.stack);
+    res?.redirect(config?.get('server:frontend') + '/error?message=500_internal_error');
+    next();
+  });
+
+  // Handle 404 error
+  app.use((_req, res) => {
+    log.error('404 Error');
+    res.redirect(config?.get('server:frontend') + '/error?message=404_Page_Not_Found');
+  });
+
+  // Prevent unhandled errors from crashing application
+  process.on('unhandledRejection', (err) => {
+    log.error('Unhandled Rejection at:', err?.stack || err);
+  });
 });
 
 module.exports = app;
