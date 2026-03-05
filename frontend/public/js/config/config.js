59963afc75601ae9c89ea3ba3c32ccbc978bbe74
Trev <trev@trevdev.ca>
2026-03-05 10:47:58 -0800

    feat: update eslint to version 10 and add cypress eslint (#1021)
    * feat: update eslint to version 10 and add cypress eslint
    
    * fix: cypress lint for js styles
    
    * fix: package-lock.json
    
    Fastest way to correct it was to delete and re-generate it

--- frontend/public/js/config/config.js
+++ frontend/public/js/config/config.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line no-unused-vars
 var config = {
   VUE_APP_BCEID_REG_URL: 'https://www.test.bceid.ca/os/?7081&SkipTo=Basic#action',
   VUE_APP_META_DATA: [{ name: 'robots', content: 'noindex,nofollow' }],
