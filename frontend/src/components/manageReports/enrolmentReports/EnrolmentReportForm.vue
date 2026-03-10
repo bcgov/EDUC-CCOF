9d1e921a6f8a7d4f831036f510b6c6de3eff05e7
Panika Saxena <panika.saxena@cgi.com>
2026-03-10 13:54:34 -0700

    format fix

--- frontend/src/components/manageReports/enrolmentReports/EnrolmentReportForm.vue
+++ frontend/src/components/manageReports/enrolmentReports/EnrolmentReportForm.vue
@@ -1894,7 +1894,10 @@ export default {
       let paymentEligibleDaysChanged = false;
       let paymentEligibleDaysPayload;
       if (this.hasClosureDays) {
-        paymentEligibleDaysChanged = !isEqual(this.originalPaymentEligibleDaysCount,this.paymentEligibleDaysCount);
+        paymentEligibleDaysChanged = !isEqual(
+          this.originalPaymentEligibleDaysCount,
+          this.paymentEligibleDaysCount,
+        );
         if (paymentEligibleDaysChanged) { 
           paymentEligibleDaysPayload = this.buildPaymentEligibleDaysPayloadForBackend();
         }
