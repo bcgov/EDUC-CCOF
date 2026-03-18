f5b389833e8938be8000faca16c922cc0852a6bf
Panika-cgi <panika.saxena@cgi.com>
2026-03-18 15:12:03 -0700

    CCFRI 7591 Funding Agreement Declaration Updates (#1046)
    * CR bug fix, FA dec versions
    
    * cleanup
    
    * remove test for negative orderNumber
    
    * review fix

---
+++ frontend/src/components/fundingAgreements/fundingAgreementDeclarationTextVersions/FundingAgreementDeclarationTextV1.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="pa-lg-7 pa-5 overflow-y-auto grey-div-with-border">
+    <p>
+      I do hereby certify that I am the <strong>authorized signing authority</strong> and that all of the information
+      provided is true and complete to the best of my knowledge and belief.
+    </p>
+    <p>
+      I consent to the Ministry contacting other branches within the Ministry and other Province ministries to validate
+      the accuracy of any information that I have provided.
+    </p>
+    <p>
+      By completing and submitting this Program Confirmation Form (the Form) electronically, I hereby confirm that I
+      have carefully read this Form and the corresponding terms and conditions of the Child Care Operating Funding
+      Agreement (the Funding Agreement) and that I agree to be bound by such terms and conditions. I further confirm
+      that by clicking “I agree” below, I represent and warrant that:
+    </p>
+
+    <ol class="declarationBList" type="a">
+      <li>
+        I am the authorized representative and signing authority of the Provider as named in the Funding Agreement (the
+        Provider);
+      </li>
+      <li>
+        I have authority to submit the Form on behalf of the Provider and that by clicking “I agree”, I do hereby bind
+        the Provider to the terms and conditions of the Funding Agreement if the Province accepts this Form and enrols
+        the Provider in any or all of the Child Care Operating Funding Program, the CCFRI, or the ECE Wage Enhancement;
+      </li>
+      <li>
+        All information provided in the Form or otherwise in support of the Provider to receive funding under the
+        Funding Agreement is true and complete to the best of my knowledge and belief. I understand and acknowledge that
+        providing false or misleading information either on the Form or otherwise to the Province to obtain any funding
+        under the Funding Agreement or otherwise failing to comply with the Funding Agreement could result in certain
+        penalties or repayment obligations, or both, under any or all of the Early Learning and Child Care Act, any
+        successor legislation, or the Funding Agreement;
+      </li>
+      <li>
+        If I have applied for and been approved by the Province to enrol in the ECE Wage Enhancement, the Provider has
+        taken all actions required under any collective agreement to which it is a party to ensure it is:
+      </li>
+    </ol>
+
+    <v-row style="padding-left: 90px">
+      <v-col cols="12">
+        i. permitted to apply for the ECE Wage Enhancement for any of its unionized Early Childhood Educators (ECEs);
+        and
+      </v-col>
+    </v-row>
+
+    <v-row style="padding-left: 90px">
+      <v-col cols="12">
+        ii. able to comply with its ECE Wage Enhancement related obligations under the Funding Agreement.
+      </v-col>
+    </v-row>
+
+    <p style="padding-top: 10px">
+      I understand and acknowledge that until such time as the Province confirms approval or temporary approval of
+      enrolment, in writing, in the CCFRI or the ECE Wage Enhancement, the Provider is not formally enrolled in these
+      initiatives. The Province is not responsible for any pre-payments the Provider may make in anticipation of
+      enrolment in either of these initiatives and any pre-payments made are at the Provider's own risk.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FundingAgreementDeclarationTextV1',
+};
+</script>
+
+<style scoped>
+li {
+  padding-bottom: 12px;
+}
+</style>
