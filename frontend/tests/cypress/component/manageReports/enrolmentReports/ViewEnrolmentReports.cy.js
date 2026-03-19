a94d91585c0f0a290fd7b56332885c45d468e776
Panika Saxena <panika.saxena@cgi.com>
2026-03-19 14:39:38 -0700

    cleanup

--- frontend/tests/cypress/component/manageReports/enrolmentReports/ViewEnrolmentReports.cy.js
+++ frontend/tests/cypress/component/manageReports/enrolmentReports/ViewEnrolmentReports.cy.js
@@ -99,7 +99,6 @@ const createAppStore = () => {
             programYearId,
             financialYear: programYear.endYear,
             status: 'CURRENT',
-            order: 1,
           },
         ],
       },
