2d63ba425548a1855a2e86da5bad48ea59c5cb38
Rylan-cgi <rylan.evans@cgi.com>
2026-03-30 14:10:18 -0700

    feat: add org type passed in through script

--- test/cypress-e2e/cypress/e2e/3-template-2-applications/group-applications-v2/group-application-v2-multiplefac.cy.js
+++ test/cypress-e2e/cypress/e2e/3-template-2-applications/group-applications-v2/group-application-v2-multiplefac.cy.js
@@ -1,6 +1,6 @@
-import { submitApp } from '../../support/pages/2-portal-application-pages/04-portal-application-summary-declaration.js'
-import { loginPage } from "../../support/pages/1-portal-login-pages/01-portal-login.js";
-import { APP_TYPE, PROGRAM_YEAR} from '../../support/constants.js'
+import { submitApp } from '../../../support/pages/2-portal-application-pages/04-portal-application-summary-declaration.js'
+import { loginPage } from "../../../support/pages/1-portal-login-pages/01-portal-login.js";
+import { APP_TYPE, PROGRAM_YEAR} from '../../../support/constants.js'
 
 describe('Group Application Test Template 2', () => {
     it('Should run through Template 2 Group Application with multiple facilities, submit and logout', () => {
@@ -14,7 +14,7 @@ describe('Group Application Test Template 2', () => {
         // Currently Template 2 uses "Future" program year
         // On 2/15/2026 update "Current" year in Constants to 2026-27 and update parameter here to "PROGRAM_YEAR.CURRENT"
         cy.task('countFiles', 'cypress/fixtures/ccof-data/extra-facs-ccof').then((files)=> {
-            cy.runCcofApp(APP_TYPE.GROUP_V2, files)
+            cy.runCcofApp(APP_TYPE.GROUP_V2, "Registered Company", files)
         })
 
         cy.task('countFiles', 'cypress/fixtures/extra-facs-ccfri').then((files) => {
