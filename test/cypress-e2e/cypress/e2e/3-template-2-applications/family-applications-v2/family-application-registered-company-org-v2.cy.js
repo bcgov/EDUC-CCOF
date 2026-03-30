303b87218307e8aeef6975ada8cf91838f57b34d
Panika-cgi <panika.saxena@cgi.com>
2026-03-30 11:52:36 -0700

    CCFRI-6301 QA-Auto Group & Family Application alternative types (#1064)
    * Added paths for alternative org types
    
    * added tests for all org types for group & family applications

---
+++ test/cypress-e2e/cypress/e2e/3-template-2-applications/family-applications-v2/family-application-registered-company-org-v2.cy.js
@@ -0,0 +1,22 @@
+import { APP_TYPE, PROGRAM_YEAR } from "../../../support/constants.js";
+import { loginPage } from "../../../support/pages/1-portal-login-pages/01-portal-login.js";
+import { submitApp } from "../../../support/pages/2-portal-application-pages/04-portal-application-summary-declaration.js";
+
+describe("Family Application Test - Registered Company", () => {
+  it("Should run through Family Application for Registered Company org type, submit and logout", () => {
+    loginPage.visitLoginPage();
+    loginPage.clickLoginButton();
+    loginPage.loginThroughExternalProvider(
+      Cypress.env("PORTAL_USERNAME"),
+      Cypress.env("PORTAL_PASSWORD"),
+    );
+    cy.startNewApp(APP_TYPE.FAMILY_V2);
+
+    cy.then(() => {
+      cy.runCcofApp(APP_TYPE.FAMILY_V2);
+      cy.runCcfriApp(APP_TYPE.FAMILY_V2, PROGRAM_YEAR.CURRENT);
+      cy.runEceWeApp(APP_TYPE.FAMILY_V2, PROGRAM_YEAR.CURRENT);
+      submitApp.summaryAndDeclaration(APP_TYPE.FAMILY_V2);
+    });
+  });
+});
