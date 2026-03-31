1f06317510b3595072c8908fcff24f4957e8db00
Rylan-cgi <rylan.evans@cgi.com>
2026-03-31 16:26:04 -0700

    feat: removing unneeded logging

--- test/selenium/src/test/java/ca/bc/gov/ecc/ccof/testcases/TestValidLogin.java
+++ test/selenium/src/test/java/ca/bc/gov/ecc/ccof/testcases/TestValidLogin.java
@@ -24,36 +24,23 @@ public class TestValidLogin extends BaseTest {
 		CRMSignInCredentialPage objCRMSignInCredentialPage = new CRMSignInCredentialPage(driver);
 		Utilities utils = new Utilities(driver);
 		Thread.sleep(2000);
-        logger.info("Enter username");
 		objCRMSignInCredentialPage.enterUserId(CRM_USERNAME);
-        logger.info("Click next");
 		objCRMSignInCredentialPage.clickNext();
-        logger.info("Next clicked");
 		Thread.sleep(2000);
 		utils.waitForElement(objCRMSignInCredentialPage.waitBeforePasswordEntered());
-        logger.info("Enter password");
 		objCRMSignInCredentialPage.enterPassword(CRM_PASSWORD);
-        logger.info("Password entered");
 		Thread.sleep(2000);
 		utils.waitForElement(objCRMSignInCredentialPage.waitBeforeClickSignIn());
-        logger.info("Click sign in");
 		objCRMSignInCredentialPage.clickSignIn();
-        logger.info("Sign in clicked");
-        logger.info("Wait for YES");
         Thread.sleep(2000);
 		utils.waitForElement(objCRMSignInCredentialPage.waitBeforeClickYes());
-        logger.info("Yes found");
-        logger.info("Click yes");
 		objCRMSignInCredentialPage.clickYes();
-        logger.info("Yes clicked");
 		Thread.sleep(2000);
 		utils.waitForElement(objCRMSignInCredentialPage.waitBeforeClickSignInAgain());
-        logger.info("Click sign in again");
 		objCRMSignInCredentialPage.clickSignInAgain();
 		Thread.sleep(5000);
 		objCRMSignInCredentialPage.switchToAppsDashboardIFrame();
 		utils.waitForElement(objCRMSignInCredentialPage.waitBeforeClickOrgFacilities());
-        logger.info("Click org facilities");
 		objCRMSignInCredentialPage.clickOrgFacilities();
 	}
 
